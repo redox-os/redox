df6864fdf25c10560de70ead44f519ccbfae1347
Jeremy Soller <jackpot51@gmail.com>
2015-05-07 07:42:09 -0600

    Add rust logo

--- src/programs/viewer.rs
+++ src/programs/viewer.rs
@@ -50,7 +50,7 @@ impl Viewer {
         self.window.title = "Press a function key to load a file";
         self.background = BMP::new();
     }
-    
+
     unsafe fn load(&mut self, filename: &'static str){
         self.window.title = filename;
         let unfs = UnFS::new(Disk::new());
@@ -64,7 +64,7 @@ impl Viewer {
 impl Program for Viewer {
     unsafe fn draw(&self, display: &Display){
         self.window.draw(display);
-		
+
 		if ! self.window.shaded {
             // TODO: Improve speed!
             if ! self.window.shaded {
@@ -76,22 +76,23 @@ impl Program for Viewer {
             }
         }
     }
-    
+
     unsafe fn on_key(&mut self, key_event: KeyEvent){
         if key_event.pressed {
             match key_event.scancode {
                 0x3B => self.load("bmw.bmp"),
-                0x3C => self.load("schemabanner.bmp"),
+                0x3C => self.load("rust.bmp"),
+                0x3D => self.load("schemabanner.bmp"),
                 _ => ()
             }
-            
+
             match key_event.character {
                 '\x1B' => self.clear(),
                 _ => ()
             }
         }
     }
-    
+
     unsafe fn on_mouse(&mut self, mouse_point: Point, mouse_event: MouseEvent, allow_catch: bool) -> bool{
         return self.window.on_mouse(mouse_point, mouse_event, allow_catch);
     }
