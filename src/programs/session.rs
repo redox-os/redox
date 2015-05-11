7f04ea9abd4bd8a02c793c6c157b99276e6e94ae
Jeremy Soller <jackpot51@gmail.com>
2015-05-11 14:34:01 -0600

    Only redraw all when necessary Blit cursor on top of screen buffer

--- src/programs/session.rs
+++ src/programs/session.rs
@@ -1,3 +1,5 @@
+use core::cmp::max;
+use core::cmp::min;
 use core::result::Result;
 
 use common::memory::*;
@@ -18,11 +20,15 @@ pub trait SessionItem {
     unsafe fn on_mouse(&mut self, session: &mut Session, mouse_event: MouseEvent, alloc_catch: bool) -> bool;
 }
 
+pub const REDRAW_NONE: usize = 0;
+pub const REDRAW_CURSOR: usize = 1;
+pub const REDRAW_ALL: usize = 2;
+
 pub struct Session {
     pub display: Display,
     pub mouse_point: Point,
     pub items: Vector<Box<SessionItem>>,
-    pub redraw: bool
+    pub redraw: usize
 }
 
 impl Session {
@@ -31,7 +37,7 @@ impl Session {
             display: Display::new(),
             mouse_point: Point::new(0, 0),
             items: Vector::<Box<SessionItem>>::new(),
-            redraw: true
+            redraw: REDRAW_ALL
         }
     }
 
@@ -48,34 +54,23 @@ impl Session {
         let mut new_items = self.copy_items();
         new_items = Vector::<Box<SessionItem>>::from_value(item) + new_items;
         self.items = new_items;
-        self.redraw = true;
+        self.redraw = REDRAW_ALL;
     }
 
     pub unsafe fn on_key(&mut self, key_event: KeyEvent){
         let items = self.copy_items();
         for item in items.as_slice() {
             (*item).on_key(self, key_event);
-            self.redraw = true;
+            self.redraw = REDRAW_ALL;
             break;
         }
     }
 
     pub unsafe fn on_mouse(&mut self, mouse_event: MouseEvent){
-        self.mouse_point.x += mouse_event.x;
-        if self.mouse_point.x < 0 {
-            self.mouse_point.x = 0;
-        }
-        if self.mouse_point.x >= self.display.width as isize {
-            self.mouse_point.x = self.display.width as isize - 1;
-        }
+        self.mouse_point.x = max(0, min(self.display.width as isize - 1, self.mouse_point.x + mouse_event.x));
+        self.mouse_point.y = max(0, min(self.display.height as isize - 1, self.mouse_point.y + mouse_event.y));
 
-        self.mouse_point.y += mouse_event.y;
-        if self.mouse_point.y < 0 {
-            self.mouse_point.y = 0;
-        }
-        if self.mouse_point.y >= self.display.height as isize {
-            self.mouse_point.y = self.display.height as isize - 1;
-        }
+        self.redraw = max(self.redraw, REDRAW_CURSOR);
 
         let items = self.copy_items();
         let mut new_items = Vector::<Box<SessionItem>>::new();
@@ -84,40 +79,41 @@ impl Session {
             if (*item).on_mouse(self, mouse_event, allow_catch) {
                 new_items = Vector::<Box<SessionItem>>::from_ptr(item) + new_items;
                 allow_catch = false;
+                self.redraw = REDRAW_ALL;
             }else{
                 new_items = new_items + Vector::<Box<SessionItem>>::from_ptr(item);
             }
         }
         self.items = new_items;
-
-        self.redraw = true;
     }
 
     pub unsafe fn redraw(&mut self){
-        if self.redraw {
-            self.redraw = false;
+        if self.redraw > REDRAW_NONE {
+            if self.redraw >= REDRAW_ALL {
+                self.display.background();
 
-            self.display.background();
+                self.display.rect(Point::new(0, 0), Size::new(self.display.width, 18), Color::new(0, 0, 0));
 
-            self.display.rect(Point::new(0, 0), Size::new(self.display.width, 18), Color::new(0, 0, 0));
+                self.display.text(Point::new(self.display.width as isize/ 2 - 3*8, 1), &String::from_str("Redox"), Color::new(255, 255, 255));
 
-            self.display.text(Point::new(self.display.width as isize/ 2 - 3*8, 1), &String::from_str("Redox"), Color::new(255, 255, 255));
-
-            let items = self.copy_items();
-            let mut new_items = Vector::<Box<SessionItem>>::new();
-            for i in 0..items.len() {
-                match items.get(items.len() - 1 - i) {
-                    Result::Ok(item) => if (*item).draw(self){
-                        new_items = Vector::<Box<SessionItem>>::from_ptr(item) + new_items;
-                    },
-                    Result::Err(_) => ()
+                let items = self.copy_items();
+                let mut new_items = Vector::<Box<SessionItem>>::new();
+                for i in 0..items.len() {
+                    match items.get(items.len() - 1 - i) {
+                        Result::Ok(item) => if (*item).draw(self){
+                            new_items = Vector::<Box<SessionItem>>::from_ptr(item) + new_items;
+                        },
+                        Result::Err(_) => ()
+                    }
                 }
+                self.items = new_items;
             }
-            self.items = new_items;
+
+            self.display.flip();
 
             self.display.cursor(self.mouse_point);
 
-            self.display.flip();
+            self.redraw = REDRAW_NONE;
         }
     }
 }
