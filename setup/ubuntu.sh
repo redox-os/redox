fe41a1404a3e5572d8f6a778c955e3b93a014dfe
Jeremy Soller <jackpot51@gmail.com>
2015-05-14 22:23:34 +0000

    Add nasm to setup script

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install build-essential libc6-dev-i386 qemu-system-x86 qemu-kvm
+sudo apt-get install build-essential libc6-dev-i386 nasm qemu-system-x86 qemu-kvm
 git clone https://github.com/rust-lang/rust.git
 cd rust
 ./configure --target=i686-unknown-linux-gnu
