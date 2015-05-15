6e7309c4292584ef225636e51b163de65f1f4886
Jeremy Soller <jackpot51@gmail.com>
2015-05-15 16:27:44 -0600

    Readd HTTP support, with new stack

--- src/network/tcp.rs
+++ src/network/tcp.rs
@@ -3,9 +3,13 @@ use core::mem::size_of;
 use core::option::Option;
 
 use common::debug::*;
+use common::memory::*;
+use common::random::*;
+use common::string::*;
 use common::vector::*;
 
 use network::common::*;
+use network::http::*;
 
 #[derive(Copy, Clone)]
 pub struct TCPHeader {
@@ -13,7 +17,7 @@ pub struct TCPHeader {
     pub dst: n16,
     pub sequence: n32,
     pub ack_num: n32,
-    pub flags: u16,
+    pub flags: n16,
     pub window_size: n16,
     pub checksum: Checksum,
     pub urgent_pointer: n16
@@ -22,25 +26,9 @@ pub struct TCPHeader {
 pub struct TCP {
     header: TCPHeader,
     options: Vector<u8>,
-    data: Vector<u8>
-}
-
-impl FromBytes for TCP {
-    fn from_bytes(bytes: Vector<u8>) -> Option<TCP> {
-        if bytes.len() >= size_of::<TCPHeader>() {
-            unsafe {
-                let header = *(bytes.data as *const TCPHeader);
-                let header_len = ((header.flags & 0xF0) >> 2) as usize;
-
-                return Option::Some(TCP {
-                    header: header,
-                    options: bytes.sub(size_of::<TCPHeader>(), header_len - size_of::<TCPHeader>()),
-                    data: bytes.sub(header_len, bytes.len() - header_len)
-                });
-            }
-        }
-        return Option::None;
-    }
+    data: Vector<u8>,
+    src_ip: IPv4Addr,
+    dst_ip: IPv4Addr
 }
 
 impl ToBytes for TCP {
@@ -52,171 +40,135 @@ impl ToBytes for TCP {
     }
 }
 
+const TCP_FIN: u16 = 1;
+const TCP_SYN: u16 = 1 << 1;
+const TCP_RST: u16 = 1 << 2;
+const TCP_PSH: u16 = 1 << 3;
+const TCP_ACK: u16 = 1 << 4;
+
 impl Response for TCP {
     fn respond(&self) -> Vector<Vector<u8>> {
         d("            ");
         self.d();
         dl();
 
-        /*
-        if segment.dst.get() == 80 {
-            if cfg!(debug_network){
-                d("            HTTP Reply ");
-                dh(segment.flags as usize);
-                d(" ");
-                dh(segment.sequence.get() as usize);
-                d(" ");
-                dh(segment.ack_num.get() as usize);
-                dl();
-            }
+        let allow;
+        match self.header.dst.get() {
+            80 => allow = true,
+            _ => allow = false
+        }
 
-            frame.dst = frame.src;
-            frame.src = MAC_ADDR;
-            packet.dst = packet.src;
-            packet.src = IP_ADDR;
-            segment.dst.set(segment.src.get());
-            segment.src.set(80);
+        if allow {
+            if self.header.flags.get() & TCP_SYN != 0 {
+                d("            TCP SYN\n");
+                let mut ret = TCP {
+                    header: self.header,
+                    options: self.options.clone(),
+                    data: Vector::new(),
+                    src_ip: IP_ADDR,
+                    dst_ip: self.src_ip
+                };
 
-            let frame_addr: *const EthernetII = frame;
-            let packet_addr: *const IPv4 = packet;
+                ret.header.src = self.header.dst;
+                ret.header.dst = self.header.src;
+                ret.header.flags.set(self.header.flags.get() | TCP_ACK);
+                ret.header.ack_num.set(self.header.sequence.get() + 1);
+                ret.header.sequence.set(rand() as u32);
 
-            if segment.flags & (1 << 9) != 0 {
-                if cfg!(debug_network){
-                    d("            HTTP SYN\n");
-                }
-                let id = packet.id.get();
-                packet.id.set(id + 1);
+                unsafe{
+                    ret.header.checksum.data = 0;
 
-                segment.flags = segment.flags | (1 << 12);
-                segment.ack_num.set(segment.sequence.get() + 1);
-                segment.sequence.set(rand() as u32);
-
-
-                let proto = n16::new(packet.proto as u16);
-                let segment_hlen = segment.head_len();
-                let segment_len = n16::new(segment_hlen as u16);
-                segment.checksum.data = 0;
-                segment.checksum.data = Checksum::compile(
-                                            Checksum::sum((&packet.src as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                            Checksum::sum((&packet.dst as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                            Checksum::sum((&proto as *const n16) as usize, size_of::<n16>()) +
-                                            Checksum::sum((&segment_len as *const n16) as usize, size_of::<n16>()) +
-                                            Checksum::sum(segment_addr, segment_hlen)
-                                        );
-
-                let packet_hlen = packet.head_len();
-                packet.checksum.calculate(packet_addr as usize, packet_hlen);
-
-                device.send(frame_addr as usize, size_of::<EthernetII>() + packet.head_len() + segment.head_len());
-            }else if segment.flags & (1 << 11) != 0{
-                if cfg!(debug_network){
-                    d("            HTTP PSH\n");
+                    let proto = n16::new(0x06);
+                    let segment_len = n16::new((size_of::<TCPHeader>() + ret.options.len() + ret.data.len()) as u16);
+                    ret.header.checksum.data = Checksum::compile(
+                        Checksum::sum((&ret.src_ip as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
+                        Checksum::sum((&ret.dst_ip as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
+                        Checksum::sum((&proto as *const n16) as usize, size_of::<n16>()) +
+                        Checksum::sum((&segment_len as *const n16) as usize, size_of::<n16>()) +
+                        Checksum::sum((&ret.header as *const TCPHeader) as usize, size_of::<TCPHeader>()) +
+                        Checksum::sum(ret.options.data as usize, ret.options.len()) +
+                        Checksum::sum(ret.data.data as usize, ret.data.len())
+                    );
                 }
 
-                let request = String::from_c_slice(slice::from_raw_parts((segment_addr + segment.head_len()) as *const u8, packet.len.get() as usize - packet.head_len() - segment.head_len()));
-
+                return Vector::from_value(ret.to_bytes());
+            }else if self.header.flags.get() & TCP_PSH != 0{
+                d("            TCP PSH\n");
+                //Send TCP_ACK_PSH_FIN in one statement
                 {
-                    let id = packet.id.get();
-                    packet.id.set(id + 1);
+                    let mut ret = TCP {
+                        header: self.header,
+                        options: self.options.clone(),
+                        data: Vector::new(),
+                        src_ip: IP_ADDR,
+                        dst_ip: self.src_ip
+                    };
 
-                    segment.flags = segment.flags & (0xFFFF - (1 << 11));
-                    let sequence = segment.ack_num.get();
-                    let ack_num = segment.sequence.get() + (packet.len.get() as usize - packet.head_len() - segment.head_len()) as u32;
-                    segment.ack_num.set(ack_num);
-                    segment.sequence.set(sequence);
+                    ret.header.src = self.header.dst;
+                    ret.header.dst = self.header.src;
+                    ret.header.flags.set(self.header.flags.get() | TCP_FIN);
+                    ret.header.ack_num.set(self.header.sequence.get() + self.data.len() as u32);
+                    ret.header.sequence.set(self.header.ack_num.get());
 
-                    let packet_len = (packet.head_len() + segment.head_len()) as u16;
-                    packet.len.set(packet_len);
-
-                    let proto = n16::new(packet.proto as u16);
-                    let segment_hlen = segment.head_len();
-                    let segment_len = n16::new(segment_hlen as u16);
-                    segment.checksum.data = 0;
-                    segment.checksum.data = Checksum::compile(
-                                                Checksum::sum((&packet.src as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                                Checksum::sum((&packet.dst as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                                Checksum::sum((&proto as *const n16) as usize, size_of::<n16>()) +
-                                                Checksum::sum((&segment_len as *const n16) as usize, size_of::<n16>()) +
-                                                Checksum::sum(segment_addr, segment_hlen)
-                                            );
-
-                    let packet_hlen = packet.head_len();
-                    packet.checksum.calculate(packet_addr as usize, packet_hlen);
-
-                    device.send(frame_addr as usize, size_of::<EthernetII>() + packet.len.get() as usize);
-                }
-
-                {
-                    let html = http_response(request);
-
-                    let response_len = size_of::<EthernetII>() + packet.head_len() + segment.head_len() + html.len();
-                    let response_addr = alloc(response_len);
-
-                    let response_frame = &mut *(response_addr as *mut EthernetII);
-                    *response_frame = *frame;
-
-                    let response_packet = &mut *((response_addr + size_of::<EthernetII>()) as *mut IPv4);
-                    *response_packet = *packet;
-                    response_packet.id.set(packet.id.get() + 1);
-                    response_packet.len.set((packet.head_len() + segment.head_len() + html.len()) as u16);
-                    response_packet.checksum.calculate(response_addr + size_of::<EthernetII>(), packet.head_len());
-
-                    let response_segment = &mut *((response_addr + size_of::<EthernetII>() + packet.head_len()) as *mut TCP);
-                    *response_segment = *segment;
-                    response_segment.flags = segment.flags | (1 << 11) | (1 << 8);
-                    response_segment.checksum.data = 0;
-
-                    for i in 0..html.len() {
-                        *((response_addr + size_of::<EthernetII>() + packet.head_len() + segment.head_len() + i) as *mut u8) = html[i] as u8;
+                    match self.header.dst.get() {
+                        80 => {
+                            // TODO: More efficient method
+                            let html = http_response(String::from_c_slice(self.data.as_slice()));
+                            unsafe{
+                                let html_ptr = html.to_c_str();
+                                ret.data = Vector::from_raw(html_ptr, html.len());
+                                unalloc(html_ptr as usize);
+                            }
+                        },
+                        _ => ()
                     }
 
-                    let proto = n16::new(packet.proto as u16);
-                    let segment_len = n16::new(segment.head_len() as u16 + html.len() as u16);
-                    response_segment.checksum.data = Checksum::compile(
-                                                Checksum::sum((&packet.src as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                                Checksum::sum((&packet.dst as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                                Checksum::sum((&proto as *const n16) as usize, size_of::<n16>()) +
-                                                Checksum::sum((&segment_len as *const n16) as usize, size_of::<n16>()) +
-                                                Checksum::sum(response_addr + size_of::<EthernetII>() + packet.head_len(), segment.head_len()) +
-                                                Checksum::sum(response_addr + size_of::<EthernetII>() + packet.head_len() + segment.head_len(), html.len())
-                                            );
+                    unsafe{
+                        ret.header.checksum.data = 0;
 
-                    device.send(response_addr, response_len);
+                        let proto = n16::new(0x06);
+                        let segment_len = n16::new((size_of::<TCPHeader>() + ret.options.len() + ret.data.len()) as u16);
+                        ret.header.checksum.data = Checksum::compile(
+                            Checksum::sum((&ret.src_ip as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
+                            Checksum::sum((&ret.dst_ip as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
+                            Checksum::sum((&proto as *const n16) as usize, size_of::<n16>()) +
+                            Checksum::sum((&segment_len as *const n16) as usize, size_of::<n16>()) +
+                            Checksum::sum((&ret.header as *const TCPHeader) as usize, size_of::<TCPHeader>()) +
+                            Checksum::sum(ret.options.data as usize, ret.options.len()) +
+                            Checksum::sum(ret.data.data as usize, ret.data.len())
+                        );
+                    }
 
-                    unalloc(response_addr);
+                    return Vector::from_value(ret.to_bytes());
                 }
-            }else if segment.flags & (1 << 8) != 0 {
-                if cfg!(debug_network){
-                    d("            HTTP FIN\n");
-                }
-                let id = packet.id.get();
-                packet.id.set(id + 1);
-
-                let proto = n16::new(packet.proto as u16);
-                let segment_hlen = segment.head_len();
-                let segment_len = n16::new(segment_hlen as u16);
-                segment.checksum.data = 0;
-                segment.checksum.data = Checksum::compile(
-                                            Checksum::sum((&packet.src as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                            Checksum::sum((&packet.dst as *const IPv4Addr) as usize, size_of::<IPv4Addr>()) +
-                                            Checksum::sum((&proto as *const n16) as usize, size_of::<n16>()) +
-                                            Checksum::sum((&segment_len as *const n16) as usize, size_of::<n16>()) +
-                                            Checksum::sum(segment_addr, segment_hlen)
-                                        );
-
-                let packet_hlen = packet.head_len();
-                packet.checksum.calculate(packet_addr as usize, packet_hlen);
-
-                device.send(frame_addr as usize, size_of::<EthernetII>() + packet.len.get() as usize);
             }
+        }else{
+            d("            TCP RST TODO\n");
         }
-        */
 
         return Vector::new();
     }
 }
 
 impl TCP {
+    pub fn from_bytes_ipv4(bytes: Vector<u8>, src_ip: IPv4Addr, dst_ip: IPv4Addr) -> Option<TCP> {
+        if bytes.len() >= size_of::<TCPHeader>() {
+            unsafe {
+                let header = *(bytes.data as *const TCPHeader);
+                let header_len = ((header.flags.get() & 0xF000) >> 10) as usize;
+
+                return Option::Some(TCP {
+                    header: header,
+                    options: bytes.sub(size_of::<TCPHeader>(), header_len - size_of::<TCPHeader>()),
+                    data: bytes.sub(header_len, bytes.len() - header_len),
+                    src_ip: src_ip,
+                    dst_ip: dst_ip
+                });
+            }
+        }
+        return Option::None;
+    }
+
     pub fn d(&self){
         d("TCP from ");
         dd(self.header.src.get() as usize);
