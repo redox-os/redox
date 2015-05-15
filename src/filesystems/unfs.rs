6df95f2e90b5e55249fa676434831c7ef55c5030
Jeremy Soller <jackpot51@gmail.com>
2015-05-15 17:02:55 -0600

    WIP fix listing

--- src/filesystems/unfs.rs
+++ src/filesystems/unfs.rs
@@ -1,6 +1,4 @@
-use core::clone::Clone;
 use core::mem::size_of;
-use core::ptr;
 
 use common::memory::*;
 use common::string::*;
@@ -68,16 +66,16 @@ impl UnFS {
     }
 
     pub unsafe fn node(&self, filename: String) -> *const Node{
-        let mut ret: *const Node = ptr::null();
+        let mut ret: *const Node = 0 as *const Node;
         let mut node_matches = false;
 
+        let root_sector_list = alloc(size_of::<SectorList>()) as *const SectorList;
         let mut root_sector_list_address = self.header.root_sector_list.address;
         while root_sector_list_address > 0 {
-            self.disk.read(root_sector_list_address, 1, 0x200400);
-            let root_sector_list = &*(0x200400 as *const SectorList);
+            self.disk.read(root_sector_list_address, 1, root_sector_list as usize);
 
             for extent_i in 0..30 {
-                let extent = root_sector_list.extents[extent_i];
+                let extent = (*root_sector_list).extents[extent_i];
                 if extent.block.address > 0 {
                     for node_address in extent.block.address..extent.block.address + extent.length {
                         let node = alloc(size_of::<Node>()) as *const Node;
@@ -110,12 +108,14 @@ impl UnFS {
                 }
             }
 
-            root_sector_list_address = root_sector_list.next_fragment.address;
+            root_sector_list_address = (*root_sector_list).next_fragment.address;
 
             if node_matches{
                 break;
             }
         }
+
+        unalloc(root_sector_list as usize);
 
         ret
     }
@@ -124,77 +124,91 @@ impl UnFS {
         let mut ret = Vector::<String>::new();
 
         unsafe{
+            let root_sector_list = alloc(size_of::<SectorList>()) as *const SectorList;
             let mut root_sector_list_address = self.header.root_sector_list.address;
             while root_sector_list_address > 0 {
-                self.disk.read(root_sector_list_address, 1, 0x200400);
-                let root_sector_list = &*(0x200400 as *const SectorList);
+                self.disk.read(root_sector_list_address, 1, root_sector_list as usize);
 
                 for extent_i in 0..30 {
-                    let extent = root_sector_list.extents[extent_i];
+                    let extent = (*root_sector_list).extents[extent_i];
                     if extent.block.address > 0 {
                         for node_address in extent.block.address..extent.block.address + extent.length {
                             let node = alloc(size_of::<Node>()) as *const Node;
                             self.disk.read(node_address, 1, node as usize);
 
-                            let node_name = String::from_c_slice(&(*node).name);
-                            if node_name.starts_with(directory.clone()){
-                                ret = ret + node_name;
-                            }
+                            // TODO: Fix listing
+
                             unalloc(node as usize);
                         }
                     }
                 }
 
-                root_sector_list_address = root_sector_list.next_fragment.address;
+                root_sector_list_address = (*root_sector_list).next_fragment.address;
             }
+
+            unalloc(root_sector_list as usize);
         }
 
         ret
     }
 
     pub unsafe fn load(&self, filename: String) -> usize{
+        let mut destination = 0;
+
         let node = self.node(filename);
 
-        if node != ptr::null() && (*node).data_sector_list.address > 0 {
-            self.disk.read((*node).data_sector_list.address, 1, 0x200B00);
-            let sector_list = &*(0x200B00 as *const SectorList);
+        if node as usize > 0{
+            if (*node).data_sector_list.address > 0 {
+                let sector_list = alloc(size_of::<SectorList>()) as *const SectorList;
+                self.disk.read((*node).data_sector_list.address, 1, sector_list as usize);
 
-            //TODO: More than one extent, extent sector count > 64K
-            let mut size = 0;
-            for i in 0..1 {
-                if sector_list.extents[i].block.address > 0 && sector_list.extents[i].length > 0{
-                    size += sector_list.extents[i].length * 512;
-                }
-            }
-
-            let destination = alloc(size as usize);
-            if destination > 0 {
+                //TODO: More than one extent, extent sector count > 64K
+                let mut size = 0;
                 for i in 0..1 {
-                    if sector_list.extents[i].block.address > 0 && sector_list.extents[i].length > 0{
-                        self.disk.read(sector_list.extents[i].block.address, sector_list.extents[i].length as u16, destination);
+                    if (*sector_list).extents[i].block.address > 0 && (*sector_list).extents[i].length > 0{
+                        size += (*sector_list).extents[i].length * 512;
                     }
                 }
+
+                destination = alloc(size as usize);
+                if destination > 0 {
+                    for i in 0..1 {
+                        if (*sector_list).extents[i].block.address > 0 && (*sector_list).extents[i].length > 0{
+                            self.disk.read((*sector_list).extents[i].block.address, (*sector_list).extents[i].length as u16, destination);
+                        }
+                    }
+                }
+
+                unalloc(sector_list as usize);
             }
-            return destination;
+
+            unalloc(node as usize);
         }
-        return 0;
+
+        return destination;
     }
 
     // TODO: Support realloc of LBAs
     pub unsafe fn save(&self, filename: String, source: usize){
         let node = self.node(filename);
 
-        if node != ptr::null() && (*node).data_sector_list.address > 0 {
-            self.disk.read((*node).data_sector_list.address, 1, 0x200B00);
-            let sector_list = &*(0x200B00 as *const SectorList);
+        if node as usize > 0{
+            if (*node).data_sector_list.address > 0 {
+                let sector_list = alloc(size_of::<SectorList>()) as *const SectorList;
+                self.disk.read((*node).data_sector_list.address, 1, sector_list as usize);
 
-            if source > 0 {
-                for i in 0..1 {
-                    if sector_list.extents[i].block.address > 0 && sector_list.extents[i].length > 0{
-                        self.disk.write(sector_list.extents[i].block.address, sector_list.extents[i].length as u16, source);
+                if source > 0 {
+                    for i in 0..1 {
+                        if (*sector_list).extents[i].block.address > 0 && (*sector_list).extents[i].length > 0{
+                            self.disk.write((*sector_list).extents[i].block.address, (*sector_list).extents[i].length as u16, source);
+                        }
                     }
                 }
+
+                unalloc(sector_list as usize);
             }
+
+            unalloc(node as usize);
         }
     }
 }
