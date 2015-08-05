2e9d30a821a486331abf6da492416978fe7f5fea
Jeremy Soller <jackpot51@gmail.com>
2015-08-04 20:19:25 -0600

    WIP Serial terminal

--- src/drivers/serial.rs
+++ src/drivers/serial.rs
@@ -7,14 +7,18 @@ use programs::session::*;
 
 pub struct Serial {
     pub port: u16,
-    pub irq: u8
+    pub irq: u8,
+    pub escape: bool,
+    pub cursor_control: bool
 }
 
 impl Serial {
     pub fn new(port: u16, irq: u8) -> Serial{
         return Serial {
             port: port,
-            irq: irq
+            irq: irq,
+            escape: false,
+            cursor_control: false
         };
     }
 }
@@ -26,20 +30,50 @@ impl SessionDevice for Serial {
             unsafe{
                 while inb(self.port + 5) & 1 == 0 {}
                 let mut c = inb(self.port) as char;
+                let mut sc = 0;
 
-                dbh(c as u8);
-                dl();
+                if self.escape {
+                    self.escape = false;
 
-                if c == '\r' {
+                    if c == '['{
+                        self.cursor_control = true;
+                    }
+
+                    c = '\0';
+                }else if self.cursor_control {
+                    self.cursor_control = false;
+
+                    if c == 'A'{
+                        sc = 0x48;
+                    }else if c == 'B'{
+                        sc = 0x50;
+                    }else if c == 'C'{
+                        sc = 0x4D;
+                    }else if c == 'D'{
+                        sc = 0x4B;
+                    }
+
+                    c = '\0';
+                }else if c == '\x1B' {
+                    self.escape = true;
+                    c = '\0';
+                }else if c == '\r' {
                     c = '\n';
+                    dc(c);
                 }else if c == '\x7F' {
                     c = '\x08';
+                    dc(c);
+                }else{
+                    dc(c);
                 }
-                updates.key_events.push(KeyEvent {
-                    character: c,
-                    scancode: 0,
-                    pressed: true
-                });
+
+                if c != '\0' || sc != 0 {
+                    updates.key_events.push(KeyEvent {
+                        character: c,
+                        scancode: sc,
+                        pressed: true
+                    });
+                }
             }
         }
     }
