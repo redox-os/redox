5d5b80475d3c162e44da1d1c883a2f2f5be0abfc
Jeremy Soller <jackpot51@gmail.com>
2015-08-17 12:54:47 -0600

    Fix bootloader

--- src/loader.asm
+++ src/loader.asm
@@ -10,47 +10,142 @@ boot: ; dl comes with disk
     mov ss, ax
     ; initialize stack
     mov sp, 0x7bfe
+    
+    mov [disk], dl
 
     mov si, name
     call print
+    call print_line
+    
+    mov bh, 0
+    mov bl, [disk]
+    call print_num
+    call print_line
 
-    mov si, DAPACK      ; address of "disk address packet"
-    mov ah, 0x42        ; AL is unused
-    int 0x13
-    jc error
+    mov ax, (unfs_header - boot)/512
+    mov bx, unfs_header
+    mov cx, (kernel_file.end - unfs_header)/512
+    xor dx, dx
+    call load
 
     jmp startup
+    
+load:
+    cmp cx, 127
+    jbe .good_size
+
+    pusha
+    mov cx, 127
+    call load
+    popa
+    add ax, 127
+    add dx, 127*512/16
+    sub cx, 127
+    
+    jmp load
+.good_size:
+    mov [DAPACK.addr], ax
+    mov [DAPACK.buf], bx
+    mov [DAPACK.count], cx
+    mov [DAPACK.seg], dx
+    
+    mov si, .msg
+    call print
+    call print_line
+    
+    mov bx, [DAPACK.addr]
+    call print_num
+    call print_line
+    
+    mov bx, [DAPACK.buf]
+    call print_num
+    call print_line
+    
+    mov bx, [DAPACK.count]
+    call print_num
+    call print_line
+    
+    mov bx, [DAPACK.seg]
+    call print_num
+    call print_line
+
+    mov dl, [disk]
+    mov si, DAPACK
+    mov ah, 0x42
+    int 0x13
+    jc error
+    ret
+.msg: db "Loading",0
+    
+print_char:
+    mov ah, 0x0e
+    int 0x10
+    ret
+    
+print_num:
+    mov cx, 4
+.loop:
+    mov al, bh
+    shr al, 4
+    and al, 0xF
+    
+    cmp al, 0xA
+    jb .below_a
+    
+    add al, 'A' - '0' - 0xA
+.below_a:
+    add al, '0'
+    
+    push cx
+    push bx
+    call print_char
+    pop bx
+    pop cx
+    
+    shl bx, 4
+    loop .loop
+    
+    ret
+    
+print_line:
+    mov si, line
+    call print
+    ret
 
 print:
 .loop:
     lodsb
     or al, al
     jz .done
-    mov ah, 0x0e
-    int 0x10
+    call print_char
     jmp .loop
 .done:
     ret
 
 name: db "Redox Loader",0
 
+line: db 13,10,0
+
 error:
   mov si, .msg
   call print
+  call print_line
 .halt:
   cli
   hlt
   jmp .halt
-.msg db ": Could not read disk", 0
+.msg db "Could not read disk",13,10,0
+
+disk: db 0
 
 DAPACK:
-	db	0x10
-	db	0
-blkcnt:	dw	(kernel_file.end - unfs_header)/512		; int 13 resets this to # of blocks actually read/written
-db_add:	dw	unfs_header		; memory buffer destination address (0:7c00)
-	dw	0		; in memory page zero
-d_lba:	dd	(unfs_header - boot)/512	; put the lba to read in this spot
-	dd	0		; more storage bytes only for big lba's ( > 4 bytes )
+        db	0x10
+        db	0
+.count: dw	0	; int 13 resets this to # of blocks actually read/written
+.buf:   dw	0       ; memory buffer destination address (0:7c00)
+.seg:   dw	0	; in memory page zero
+.addr:  dd	0	; put the lba to read in this spot
+        dd	0	; more storage bytes only for big lba's ( > 4 bytes )
 
 times 510-($-$$) db 0
 db 0x55
