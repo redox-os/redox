b71457298a9b10220206ddc3985aed6efc087779
Jeremy Soller <jackpot51@gmail.com>
2015-08-23 20:20:32 -0600

    Scriptability

--- filesystem/example.sh
+++ filesystem/example.sh
@@ -1,3 +1,17 @@
-echo hello
-url random
-url memory
+if a == a
+  echo true a == a
+
+  if b != b
+    echo true b != b
+  else
+    echo false b != b
+
+    if 3 > 2
+      echo true 3 > 2
+    else
+      echo false 3 > 2
+    fi
+  fi
+else
+  echo false a == a
+fi
