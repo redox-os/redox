0f9a3675e665be03024975e0ae4ed3abe7b5950f
Jeremy Soller <jackpot51@gmail.com>
2015-09-09 09:29:53 -0600

    WIP remove debugging

--- src/network/icmp.rs
+++ src/network/icmp.rs
@@ -16,8 +16,8 @@ pub struct ICMPHeader {
 }
 
 pub struct ICMP {
-    header: ICMPHeader,
-    data: Vec<u8>
+    pub header: ICMPHeader,
+    pub data: Vec<u8>
 }
 
 impl FromBytes for ICMP {
@@ -44,45 +44,6 @@ impl ToBytes for ICMP {
         }
     }
 }
-/*
-impl Response for ICMP {
-    #[allow(unused_variables)]
-    fn respond(&self) -> Vec<Vec<u8>>{
-        if cfg!(debug_network){
-            d("        ");
-            self.d();
-            dl();
-        }
-
-        let mut ret: Vec<Vec<u8>> = Vec::new();
-        if self.header._type == 0x08 {
-            if cfg!(debug_network){
-                d("            Echo Reply\n");
-            }
-
-            let mut response = ICMP {
-                header: self.header,
-                data: self.data.clone()
-            };
-
-            response.header._type = 0x00;
-
-            unsafe{
-                response.header.checksum.data = 0;
-
-                let header_ptr: *const ICMPHeader = &response.header;
-                response.header.checksum.data = Checksum::compile(
-                    Checksum::sum(header_ptr as usize, size_of::<ICMPHeader>()) +
-                    Checksum::sum(response.data.as_ptr() as usize, response.data.len())
-                );
-            }
-
-            ret.push(response.to_bytes());
-        }
-        return ret;
-    }
-}
-*/
 
 impl ICMP {
     pub fn d(&self){
