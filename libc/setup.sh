ee50e67bd4eef1828874d08c1ab76cc5fbf2eded
Jeremy Soller <jackpot51@gmail.com>
2015-09-24 20:16:35 -0600

    Uncomment the lines

--- libc/setup.sh
+++ libc/setup.sh
@@ -6,75 +6,75 @@ mkdir -p build
 cd build
 
 PREFIX="${PWD}/prefix"
-# rm -rf "${PREFIX}"
-# mkdir -p "${PREFIX}"
-# mkdir -p "${PREFIX}/bin"
+rm -rf "${PREFIX}"
+mkdir -p "${PREFIX}"
+mkdir -p "${PREFIX}/bin"
 export PATH="${PREFIX}/bin:$PATH"
-#
+
 SYSROOT="${PWD}/sysroot"
-# rm -rf "${SYSROOT}"
-# mkdir -p "${SYSROOT}"
-#
-# ###################BINUTILS#########################
-# BINUTILS=binutils-2.24.90
-#
-# if [ ! -f "${BINUTILS}.tar.bz2" ]
-# then
-#     curl "ftp://sourceware.org/pub/binutils/snapshots/${BINUTILS}.tar.bz2" -o "${BINUTILS}.tar.bz2"
-# fi
-#
-# if [ ! -d "${BINUTILS}" ]
-# then
-#     tar xvf "${BINUTILS}.tar.bz2"
-# fi
-#
-# cp -r ../binutils-redox/* "${BINUTILS}"
-#
-# rm -rf "build-${BINUTILS}"
-# mkdir "build-${BINUTILS}"
-# pushd "build-${BINUTILS}"
-#     "../${BINUTILS}/configure" --target=i386-elf-redox --prefix="${PREFIX}" --with-sysroot="${SYSROOT}" --disable-nls --disable-werror
-#     make -j `nproc`
-#     make -j `nproc` install
-# popd
-#
-# read -p "BINUTILS SETUP"
-#
-# ##################GCC FREESTANDING##############################
+rm -rf "${SYSROOT}"
+mkdir -p "${SYSROOT}"
+
+###################BINUTILS#########################
+BINUTILS=binutils-2.24.90
+
+if [ ! -f "${BINUTILS}.tar.bz2" ]
+then
+    curl "ftp://sourceware.org/pub/binutils/snapshots/${BINUTILS}.tar.bz2" -o "${BINUTILS}.tar.bz2"
+fi
+
+if [ ! -d "${BINUTILS}" ]
+then
+    tar xvf "${BINUTILS}.tar.bz2"
+fi
+
+cp -r ../binutils-redox/* "${BINUTILS}"
+
+rm -rf "build-${BINUTILS}"
+mkdir "build-${BINUTILS}"
+pushd "build-${BINUTILS}"
+    "../${BINUTILS}/configure" --target=i386-elf-redox --prefix="${PREFIX}" --with-sysroot="${SYSROOT}" --disable-nls --disable-werror
+    make -j `nproc`
+    make -j `nproc` install
+popd
+
+read -p "BINUTILS SETUP"
+
+##################GCC FREESTANDING##############################
 GCC=gcc-4.6.4
-#
-# if [ ! -f "${GCC}.tar.bz2" ]
-# then
-#     curl "http://ftp.gnu.org/gnu/gcc/${GCC}/${GCC}.tar.bz2" -o "${GCC}.tar.bz2"
-# fi
-#
-# if [ ! -d "${GCC}" ]
-# then
-#     tar xvf "${GCC}.tar.bz2"
-#     pushd "${GCC}"
-#         ./contrib/download_prerequisites
-#     popd
-# fi
-#
-# cp -r ../gcc-redox/* "${GCC}"
-#
-# pushd "${GCC}/libstdc++-v3"
-#     autoconf2.64
-# popd
-#
-# rm -rf "build-freestanding-${GCC}"
-# mkdir "build-freestanding-${GCC}"
-# pushd "build-freestanding-${GCC}"
-#     "../${GCC}/configure" --target=i386-elf-redox --prefix="${PREFIX}" --disable-nls --enable-languages=c,c++ --without-headers
-#     make -j `nproc` all-gcc
-#     make -j `nproc` all-target-libgcc
-#     make -j `nproc` install-gcc
-#     make -j `nproc` install-target-libgcc
-# popd
-#
-# read -p "GCC FREESTANDING SETUP"
-#
-# ##################NEWLIB###########################
+
+if [ ! -f "${GCC}.tar.bz2" ]
+then
+    curl "http://ftp.gnu.org/gnu/gcc/${GCC}/${GCC}.tar.bz2" -o "${GCC}.tar.bz2"
+fi
+
+if [ ! -d "${GCC}" ]
+then
+    tar xvf "${GCC}.tar.bz2"
+    pushd "${GCC}"
+        ./contrib/download_prerequisites
+    popd
+fi
+
+cp -r ../gcc-redox/* "${GCC}"
+
+pushd "${GCC}/libstdc++-v3"
+    autoconf2.64
+popd
+
+rm -rf "build-freestanding-${GCC}"
+mkdir "build-freestanding-${GCC}"
+pushd "build-freestanding-${GCC}"
+    "../${GCC}/configure" --target=i386-elf-redox --prefix="${PREFIX}" --disable-nls --enable-languages=c,c++ --without-headers
+    make -j `nproc` all-gcc
+    make -j `nproc` all-target-libgcc
+    make -j `nproc` install-gcc
+    make -j `nproc` install-target-libgcc
+popd
+
+read -p "GCC FREESTANDING SETUP"
+
+##################NEWLIB###########################
 NEWLIB=newlib-2.2.0.20150824
 
 if [ ! -f "${NEWLIB}.tar.gz" ]
