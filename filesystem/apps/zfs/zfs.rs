9f9a28db122227ba236ef9a000141f9eff9f6548
Jeremy Soller <jackpot51@gmail.com>
2015-10-02 15:49:35 -0600

    Add OX

--- filesystem/apps/zfs/zfs.rs
+++ filesystem/apps/zfs/zfs.rs
@@ -1,5 +1,4 @@
 //To use this, please install zfs-fuse
-
 use redox::*;
 
 pub struct ZFS {
@@ -12,6 +11,19 @@ impl ZFS {
             disk: disk,
         }
     }
+
+    //TODO: Error handling
+    pub fn read(&mut self, block: usize) -> [u8; 512] {
+        let mut data: [u8; 512] = [0; 512];
+        self.disk.seek(Seek::Start(block * 512));
+        self.disk.read(&mut data);
+        return data;
+    }
+
+    pub fn write(&mut self, block: usize, data: &[u8; 512]) {
+        self.disk.seek(Seek::Start(block * 512));
+        self.disk.write(data);
+    }
 }
 
 //TODO: Find a way to remove all the to_string's
@@ -19,7 +31,7 @@ pub fn main() {
     console_title(&"ZFS".to_string());
 
     println!("Opening ZFS Image".to_string());
-    let zfs = ZFS::new(File::open(&"zfs.img".to_string()));
+    let mut zfs = ZFS::new(File::open(&"zfs.img".to_string()));
 
     while let Option::Some(line) = readln!() {
         let mut args: Vec<String> = Vec::new();
@@ -28,12 +40,29 @@ pub fn main() {
         }
 
         if let Option::Some(command) = args.get(0) {
-            print_color!(command.clone() + "\n", Color::new(255, 128, 128));
+            println!("# ".to_string() + line);
 
-            if *command == "ls".to_string() {
-                println!("List files".to_string());
-            }else{
-                println!("Command not found".to_string());
+            if *command == "list".to_string() {
+                print_color!("List volumes\n".to_string(), Color::new(127, 255, 127));
+            } else if *command == "dump".to_string() || *command == "d".to_string() {
+                match args.get(1) {
+                    Option::Some(arg) => {
+                        let block = arg.to_num();
+                        print_color!("Dump block:".to_string() + block, Color::new(127, 255, 127));
+
+                        let data = zfs.read(block);
+                        for i in 0..data.len() {
+                            if i % 32 == 0 {
+                                print!("\n".to_string() + String::from_num_radix(i, 16) + ":");
+                            }
+                            print!(" ".to_string() + String::from_num_radix(data[i] as usize, 16));
+                        }
+                        print!("\n".to_string());
+                    },
+                    Option::None => print_color!("No block specified!\n".to_string(), Color::new(255, 127, 127))
+                }
+            } else {
+                print_color!("Commands: list\n".to_string(), Color::new(127, 127, 255));
             }
         }
     }
