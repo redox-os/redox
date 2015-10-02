cdda4358b3b3151c02faba2fd5f5bf8f66fd5ec5
Jeremy Soller <jackpot51@gmail.com>
2015-10-02 14:32:02 -0600

    Add example of processing commands

--- filesystem/apps/zfs/zfs.rs
+++ filesystem/apps/zfs/zfs.rs
@@ -14,6 +14,30 @@ impl ZFS {
     }
 }
 
+//TODO: Find a way to remove all the to_string's
 pub fn main() {
-    let zfs = ZFS::new(File::open("zfs.img"));
+    console_title(&"ZFS".to_string());
+
+    println!("Opening ZFS Image".to_string());
+    let zfs = ZFS::new(File::open(&"zfs.img".to_string()));
+
+    while let Option::Some(line) = readln!() {
+        let mut args: Vec<String> = Vec::new();
+        for arg in line.split(" ".to_string()) {
+            args.push(arg);
+        }
+
+        if let Option::Some(command) = args.get(0) {
+            print_color!(command.clone() + "\n", Color::new(255, 128, 128));
+
+            if *command == "ls".to_string() {
+                println!("List files".to_string());
+            }else{
+                println!("Command not found".to_string());
+            }
+        }
+    }
+
+    println!("Closing ZFS Image".to_string());
+    drop(zfs);
 }
