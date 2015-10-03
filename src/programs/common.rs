57f23e9d58d1d8025bad51a38afe1f68170e3b16
stratact <stratact1@gmail.com>
2015-10-03 12:29:01 -0700

    Remove the other pub imports in programs::common while were at it

--- src/programs/common.rs
+++ src/programs/common.rs
@@ -1,21 +1,8 @@
-pub use alloc::boxed::*;
-
-pub use core::cmp::max;
-pub use core::cmp::min;
 pub use core::clone::Clone;
-pub use core::mem::size_of;
-pub use core::mem::size_of_val;
 pub use core::option::Option;
-pub use core::ptr;
 pub use core::sync::atomic::*;
 
-pub use graphics::color::*;
-pub use graphics::display::*;
-pub use graphics::point::*;
-pub use graphics::size::*;
-pub use graphics::window::*;
-
-pub use syscall::call::*;
+use alloc::boxed::*;
 
 use common::resource::{NoneResource, Resource, URL};
 use common::string::String;
