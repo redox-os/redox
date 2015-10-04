448b511a0a2103afd10876bee922bb398361a3c4
Ticki <Ticki@users.noreply.github.com>
2015-10-04 20:07:19 +0200

    Use cargo for `common` module

--- filesystem/apps/ox/ox.rs
+++ filesystem/apps/ox/ox.rs
@@ -1,6 +1,7 @@
 use redox::*;
 
 //TODO: Find a way to clean up the to_string's
+// Maybe make a Into trait, which is generic. Let rust's type interference do the work.
 pub fn main() {
     console_title(&"Ox".to_string());
 
@@ -19,7 +20,7 @@ pub fn main() {
                         print_color!("Install ".to_string() + package + "\n".to_string(), Color::new(127, 255, 127));
                     }
                 }
-            }else if *command == "uninstall".to_string() || *command == "u".to_string() {
+            } else if *command == "uninstall".to_string() || *command == "u".to_string() {
                 for i in 1..args.len() {
                     if let Option::Some(package) = args.get(i) {
                         print_color!("Uninstall ".to_string() + package + "\n".to_string(), Color::new(255, 127, 127));
