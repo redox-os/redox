e188fea4809902eb023fe47ef644375fcb37ef8e
Ticki <Ticki@users.noreply.github.com>
2015-10-04 20:07:19 +0200

    Use cargo for `common` module

---
+++ src/common/src/lib.rs
@@ -0,0 +1,17 @@
+
+/// Debug
+pub mod debug; // Not needed
+/// Event input
+pub mod event;
+/// A module for queues
+pub mod queue;
+/// A module for pseudorandom generator
+pub mod random; // Should remove
+/// A module for scheduling
+pub mod scheduler; // Should remove
+/// A module for owned strings
+pub mod string;
+/// A module for time
+pub mod time;
+/// A module for heap allocated, growable vectors
+pub mod vec;
