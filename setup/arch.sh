bcfcbc479da8d4d9bd594c1eea7bf42440ff7c94
Michael Gattozzi <mgattozzi@gmail.com>
2015-10-06 07:12:23 -0400

    Added setup script for Arch Linux to repo
    Changes to be committed:

---
+++ setup/arch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#Script designed to setup an arch linux computer to use Redox
+
+#Install necessary packages
+echo "Installing needed packages"
+sudo pacman -S --needed base-devel nasm lib32-glibc glibc
+
+#Install Virtual Box and Setup
+echo "Installing virtualbox and it's dependencies"
+sudo pacman -S --needed virtualbox virtualbox-host-modules virtualbox-guest-iso qt4
+
+#Load kernel modules and set for persistence
+echo "Setting up virtualbox for persistence across reboots"
+sudo modprobe vboxdrv
+sudo mkdir -p /etc/modules-load.d
+sudo touch /etc/modules-load.d/virtualbox.conf
+#This hackish priviledge elevation seems to be the only way to get this to work
+sudo su -c "echo vboxdrv > /etc/modules-load.d/virtualbox.conf"
+sudo su -c "echo vboxpci >> /etc/modules-load.d/virtualbox.conf"
+
+#Add user to vboxusers group
+sudo gpasswd -a $USER vboxusers
+
+echo "Script complete"
+
