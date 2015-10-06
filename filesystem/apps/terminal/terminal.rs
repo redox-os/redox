812befedfacbd65692919af3e7ac9dffcf64b20f
Michael Gattozzi <mgattozzi@gmail.com>
2015-10-06 14:15:49 -0400

    Fixed issue regarding terminal not compiling
    Changes to be committed:
    	modified:   filesystem/apps/terminal/terminal.rs

--- filesystem/apps/terminal/terminal.rs
+++ filesystem/apps/terminal/terminal.rs
@@ -1,6 +1,6 @@
 use core::ops::DerefMut;
 use redox::ion::command::{Application};
-
+static mut application: *mut Application = 0 as *mut Application;
 pub fn main() {
     unsafe {
         let mut app = box Application::new();
