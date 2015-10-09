4c9fbf13b8386588c8788aa7074af650c8a6334b
Ticki <Ticki@users.noreply.github.com>
2015-10-09 23:13:12 +0200

    More docs

--- src/drivers/src/lib.rs
+++ src/drivers/src/lib.rs
@@ -1,8 +1,16 @@
+/// Disk
 pub mod disk;
+/// MMIO
 pub mod mmio;
+/// PCI
 pub mod pci;
+/// PCI configuration
 pub mod pciconfig;
+/// PIO
 pub mod pio;
+/// PS2
 pub mod ps2;
+/// RTC
 pub mod rtc;
+/// Serial
 pub mod serial;
