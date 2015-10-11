1165b6077ef766fe846fb629e328c4fd84f3598c
Theodore DeRego <tderego94@gmail.com>
2015-10-10 21:00:26 -1000

    Read object directory zap object

--- filesystem/apps/zfs/zap.rs
+++ filesystem/apps/zfs/zap.rs
@@ -1,7 +1,12 @@
+use redox::*;
+
+use super::FromBytes;
+
 const MZAP_ENT_LEN: usize = 64;
 const MZAP_NAME_LEN: usize = MZAP_ENT_LEN - 8 - 4 - 2;
 
 #[repr(u64)]
+#[derive(Copy, Clone, Debug)]
 pub enum ZapObjectType {
     Micro = (1 << 63) + 3,
     Header = (1 << 63) + 1,
@@ -9,15 +14,40 @@ pub enum ZapObjectType {
 }
 
 /// Microzap
+#[repr(packed)]
 pub struct MZapPhys {
-    block_type: u64, // ZapObjectType::Micro
+    block_type: ZapObjectType, // ZapObjectType::Micro
     salt: u64,
-    normflags: u64,
+    norm_flags: u64,
     pad: [u64; 5],
     chunk: [MZapEntPhys; 1],
     // actually variable size depending on block size
 }
 
+impl FromBytes for MZapPhys {
+    fn from_bytes(data: &[u8]) -> Option<Self> {
+        if data.len() >= mem::size_of::<MZapPhys>() {
+            let mzap_phys = unsafe { ptr::read(data.as_ptr() as *const MZapPhys) };
+            Some(mzap_phys)
+        } else {
+            Option::None
+        }
+    }
+}
+
+impl fmt::Debug for MZapPhys {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "MZapPhys {{\nblock_type: {:?},\nsalt: {:X},\nnorm_flags: {:X},\nchunk: [\n",
+                    self.block_type, self.salt, self.norm_flags));
+        for chunk in &self.chunk {
+            try!(write!(f, "{:?}", chunk));
+        }
+        try!(write!(f, "] }}\n"));
+        Ok(())
+    }
+}
+
+#[repr(packed)]
 pub struct MZapEntPhys{
     pub value: u64,
     pub cd: u32,
@@ -25,9 +55,19 @@ pub struct MZapEntPhys{
     pub name: [u8; MZAP_NAME_LEN],
 }
 
+impl fmt::Debug for MZapEntPhys {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "MZapEntPhys {{\nvalue: {:X},\ncd: {:X},\n",
+                    self.value, self.cd));
+        try!(write!(f, "}}\n"));
+        Ok(())
+    }
+}
+
 /// Fatzap
+#[repr(packed)]
 pub struct ZapPhys {
-    pub block_type: u64, // ZapObjectType::Header
+    pub block_type: ZapObjectType, // ZapObjectType::Header
     pub magic: u64,
     pub ptr_table: ZapTablePhys,
     pub free_block: u64,
@@ -38,6 +78,7 @@ pub struct ZapPhys {
     pub leafs: [u64; 8192],
 }
 
+#[repr(packed)]
 pub struct ZapTablePhys {
      pub block: u64,
      pub num_blocks: u64,
@@ -63,8 +104,9 @@ const ZAP_LEAF_ARRAY_BYTES: usize = ZAP_LEAF_CHUNKSIZE - 3;
     } chunks[ZapLeafChunk; ZAP_LEAF_NUMCHUNKS],
 }*/
 
+#[repr(packed)]
 pub struct ZapLeafHeader {
-    pub block_type: u64, // ZapObjectType::Leaf
+    pub block_type: ZapObjectType, // ZapObjectType::Leaf
     pub next: u64,
     pub prefix: u64,
     pub magic: u32,
@@ -75,6 +117,7 @@ pub struct ZapLeafHeader {
     pad2: [u8; 12],
 }
 
+#[repr(packed)]
 struct ZapLeafEntry {
     leaf_type: u8,
     int_size: u8,
@@ -88,12 +131,14 @@ struct ZapLeafEntry {
     hash: u64,
 }
 
+#[repr(packed)]
 struct ZapLeafArray {
     leaf_type: u8,
     array: [u8; ZAP_LEAF_ARRAY_BYTES],
     next: u16,
 }
 
+#[repr(packed)]
 struct ZapLeafFree{
     free_type: u8,
     pad: [u8; ZAP_LEAF_ARRAY_BYTES],
