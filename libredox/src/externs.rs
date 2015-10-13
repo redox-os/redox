a8f189e975b84e37c4c8f2cdfcad72ad3317d555
Jeremy Soller <jackpot51@gmail.com>
2015-10-13 14:09:05 -0600

    Make sure that allocators in alloc are correct

--- libredox/src/externs.rs
+++ libredox/src/externs.rs
@@ -3,7 +3,7 @@ use core::fmt::Write;
 use core::ptr;
 use core::result;
 
-use syscall::{sys_debug, sys_exit};
+use syscall::*;
 
 pub struct DebugStream;
 
@@ -41,6 +41,38 @@ extern "C" fn stack_exhausted() {
 #[lang="eh_personality"]
 extern "C" fn eh_personality() {
 
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_allocate(size: usize, align: usize) -> *mut u8 {
+    unsafe { sys_alloc(size) as *mut u8 }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_deallocate(ptr: *mut u8, old_size: usize, align: usize) {
+    unsafe { sys_unalloc(ptr as usize) }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize,
+                                align: usize) -> *mut u8 {
+    unsafe { sys_realloc(ptr as usize, size) as *mut u8 }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_reallocate_inplace(ptr: *mut u8, old_size: usize,
+                                        size: usize, align: usize) -> usize {
+    unsafe { sys_realloc_inplace(ptr as usize, size) }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_usable_size(size: usize, align: usize) -> usize {
+    size
 }
 
 #[no_mangle]
