f5ec5ef644c103317c9363b322cc19bdcde18182
Jeremy Soller <jackpot51@gmail.com>
2015-10-13 14:09:05 -0600

    Make sure that allocators in alloc are correct

--- src/externs.rs
+++ src/externs.rs
@@ -3,6 +3,7 @@ use core::ptr;
 use core::result;
 
 use common::debug::*;
+use common::memory::*;
 
 use syscall::handle::do_sys_exit;
 
@@ -42,6 +43,38 @@ extern "C" fn stack_exhausted() {
 #[lang="eh_personality"]
 extern "C" fn eh_personality() {
 
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_allocate(size: usize, align: usize) -> *mut u8 {
+    unsafe { alloc(size) as *mut u8 }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_deallocate(ptr: *mut u8, old_size: usize, align: usize) {
+    unsafe { unalloc(ptr as usize) }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize,
+                                align: usize) -> *mut u8 {
+    unsafe { realloc(ptr as usize, size) as *mut u8 }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_reallocate_inplace(ptr: *mut u8, old_size: usize,
+                                        size: usize, align: usize) -> usize {
+    unsafe { realloc_inplace(ptr as usize, size) }
+}
+
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern fn __rust_usable_size(size: usize, align: usize) -> usize {
+    size
 }
 
 #[no_mangle]
