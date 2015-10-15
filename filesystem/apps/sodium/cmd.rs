6c6db40be9ec062df889fb8c54746b8faf7e8911
Ticki <Ticki@users.noreply.github.com>
2015-10-15 19:48:19 +0200

    More refactoring

--- filesystem/apps/sodium/cmd.rs
+++ filesystem/apps/sodium/cmd.rs
@@ -2,6 +2,10 @@ use collections::VecDeque;
 // Temporary hack until libredox get hashmaps
 use redox::*;
 
+#[derive(Clone)]
+/// An instruction
+pub Inst(i16, char)
+
 #[derive(Clone, PartialEq, Copy, Hash)]
 pub enum InsertMode {
     Append,
@@ -40,7 +44,7 @@ pub enum PrimitiveMode {
 
 #[derive(Clone, PartialEq, Hash)]
 /// The state of the editor
-pub struct Editor {
+pub struct Editor<I: Iterator<Item = char>> {
     /// The current cursor
     pub current_cursor: u8,
     /// The cursors
@@ -53,54 +57,61 @@ pub struct Editor {
     pub scroll_y: u32,
     /// Number of repeation entered
     pub n: u16,
+    /// The input iterator
+    pub iter: I,
 }
 
 
 impl Editor {
 
-    /// Execute a instruction n times
-    pub fn exec(&mut self, n: u16, cmd: char) {
+    /// Execute a instruction
+    pub fn exec(&mut self, inst: Inst) {
 
-        match cmd {
-
-        }
     }
-    
+
     /// Feed a char to the editor (as input)
-    fn feed(&mut self, c: char) {
+    pub fn feed(&mut self, c: char) {
         match self.cursors[self.current_cursor as usize].mode {
             Mode::Primitive(_) => {
-                self.exec(0, c);
+                self.exec(Inst(0, c));
             },
             Mode::Command(_) => {
                 self.n = match c {
                     '0' if self.n != 0 => self.n * 10,
-                    '1'           => self.n * 10 + 1,
-                    '2'           => self.n * 10 + 2,
-                    '3'           => self.n * 10 + 3,
-                    '4'           => self.n * 10 + 4,
-                    '5'           => self.n * 10 + 5,
-                    '6'           => self.n * 10 + 6,
-                    '7'           => self.n * 10 + 7,
-                    '8'           => self.n * 10 + 8,
-                    '9'           => self.n * 10 + 9,
-                    _             => {
-                        self.exec(if self.n == 0 { 1 } else { self.n }, c);
+                    '1'                => self.n * 10 + 1,
+                    '2'                => self.n * 10 + 2,
+                    '3'                => self.n * 10 + 3,
+                    '4'                => self.n * 10 + 4,
+                    '5'                => self.n * 10 + 5,
+                    '6'                => self.n * 10 + 6,
+                    '7'                => self.n * 10 + 7,
+                    '8'                => self.n * 10 + 8,
+                    '9'                => self.n * 10 + 9,
+                    _                  => {
+                        self.exec(Inst(if self.n == 0 { 1 } else { self.n },
+                                       c));
                         self.n
-                    }
+                    },
                 }
 
             }
         }
     }
 
-    /// Insert text
-    fn insert(&mut self, c: char) {
+    /// Initialize the editor
+    pub fn init(&mut self) {
+        for c in self.iter {
+            self.feed(c);
+        }
+    }
 
+    /// Insert text
+    pub fn insert(&mut self, c: char) {
+        
     }
 
     /// Create new default state editor
-    fn new() -> Editor {
+    pub fn new() -> Editor {
         Editor {
             current_cursor: 0,
             cursors: Vec::new(),
@@ -132,5 +143,5 @@ pub struct Cursor {
     /// The mode of the cursor
     pub mode: Mode,
     /// The history of the cursor
-    pub history: Vec<Unit>,
-
+    pub history: Vec<Inst>,
+}
