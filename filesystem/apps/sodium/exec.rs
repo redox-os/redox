8cd6386adfd883c7d790245728944eedd11dfbf6
Ticki <Ticki@users.noreply.github.com>
2015-10-16 16:34:03 +0200

    Refactor

--- filesystem/apps/sodium/exec.rs
+++ filesystem/apps/sodium/exec.rs
@@ -1,40 +1,63 @@
 use super::*;
 
+struct InstructionIterator<'a, I: 'a> {
+    editor: &'a mut Editor<I>,
+}
+
+impl<'a, I: Iterator<Item = char>> Iterator for InstructionIterator<'a, I> {
+    type Item = Inst;
+    
+    fn next(&mut self) -> Option<Inst> {
+        let mut n = 0;
+
+        match self.editor.iter {
+            Some(i) => {
+                let last;
+                for c in i {
+                    match self.editor.cursors[self.editor.current_cursor as usize].mode {
+                        Mode::Primitive(_) => {
+                            Inst(0, c);
+                        },
+                        Mode::Command(_) => {
+                            n = match c {
+                                '0' if n != 0 => n * 10,
+                                '1'           => n * 10 + 1,
+                                '2'           => n * 10 + 2,
+                                '3'           => n * 10 + 3,
+                                '4'           => n * 10 + 4,
+                                '5'           => n * 10 + 5,
+                                '6'           => n * 10 + 6,
+                                '7'           => n * 10 + 7,
+                                '8'           => n * 10 + 8,
+                                '9'           => n * 10 + 9,
+                                _             => {
+                                    last = c;
+                                    break;
+                                },
+                            }
+
+                        }
+                    }
+                }
+
+                Some(Inst(if n == 0 { 1 } else { n }, last))
+            },
+            None => None,
+        }
+    }
+}
+
+impl<I: Iterator<Item = char>> Editor<I> {
+    pub fn instr_iter<'a>(&'a mut self) -> InstructionIterator<'a, I> {
+        InstructionIterator {
+            editor: self,
+        }
+    }
+}
 impl<I: Iterator<Item = char>> Editor<I> {
     /// Execute a instruction
     pub fn exec(&mut self, inst: Inst) {
         
     }
 
-
-
-
-    /// Feed a char to the editor (as input)
-    pub fn feed(&mut self, c: char) {
-        match self.cursors[self.current_cursor as usize].mode {
-            Mode::Primitive(_) => {
-                self.exec(Inst(0, c));
-            },
-            Mode::Command(_) => {
-                self.n = match c {
-                    '0' if self.n != 0 => self.n * 10,
-                    '1'                => self.n * 10 + 1,
-                    '2'                => self.n * 10 + 2,
-                    '3'                => self.n * 10 + 3,
-                    '4'                => self.n * 10 + 4,
-                    '5'                => self.n * 10 + 5,
-                    '6'                => self.n * 10 + 6,
-                    '7'                => self.n * 10 + 7,
-                    '8'                => self.n * 10 + 8,
-                    '9'                => self.n * 10 + 9,
-                    _                  => {
-                        self.exec(Inst(if self.n == 0 { 1 } else { self.n },
-                                       c));
-                        self.n
-                    },
-                }
-
-            }
-        }
-    }
 }
