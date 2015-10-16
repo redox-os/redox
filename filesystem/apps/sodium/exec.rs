e48d02ae067d1ebe3757d080f4f67ceeeff80a0e
Ticki <Ticki@users.noreply.github.com>
2015-10-16 23:07:04 +0200

    Fix some type errors

--- filesystem/apps/sodium/exec.rs
+++ filesystem/apps/sodium/exec.rs
@@ -13,31 +13,34 @@ impl<'a, I: Iterator<Item = EventOption>> Iterator for InstructionIterator<'a, I
         let mut n = 0;
 
         let mut last = '\0';
-        for c in self.iter {
-            match self.editor.cursors[self.editor.current_cursor as usize].mode {
-                Mode::Primitive(_) => {
-                    Inst(0, c);
-                },
-                Mode::Command(_) => {
-                    n = match c {
-                        '0' if n != 0 => n * 10,
-                        '1'           => n * 10 + 1,
-                        '2'           => n * 10 + 2,
-                        '3'           => n * 10 + 3,
-                        '4'           => n * 10 + 4,
-                        '5'           => n * 10 + 5,
-                        '6'           => n * 10 + 6,
-                        '7'           => n * 10 + 7,
-                        '8'           => n * 10 + 8,
-                        '9'           => n * 10 + 9,
-                        _             => {
-                            last = c;
-                            break;
+        for e in self.iter {
+            match e {
+                EventOption::Key(k) if k.pressed => {
+                    let c = k.character;
+                    match self.editor.cursors[self.editor.current_cursor as usize].mode {
+                        Mode::Primitive(_) => {
+                            Inst(0, c);
                         },
-                    }
+                        Mode::Command(_) => {
+                            n = match c {
+                                '0' if n != 0 => n * 10,
+                                '1'           => n * 10 + 1,
+                                '2'           => n * 10 + 2,
+                                '3'           => n * 10 + 3,
+                                '4'           => n * 10 + 4,
+                                '5'           => n * 10 + 5,
+                                '6'           => n * 10 + 6,
+                                '7'           => n * 10 + 7,
+                                '8'           => n * 10 + 8,
+                                '9'           => n * 10 + 9,
+                                _             => {
+                                    last = c;
+                                    break;
+                                },
+                            }
 
-                }
-            }
+                        }
+                    }
         }
 
         Some(Inst(if n == 0 { 1 } else { n }, last))
