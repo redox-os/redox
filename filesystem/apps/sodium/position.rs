acaf183c91ca6622635e5c1c26010064d5ebdccb
Ticki <Ticki@users.noreply.github.com>
2015-10-17 19:44:50 +0200

    It now works like a very basic text editor

---
+++ filesystem/apps/sodium/position.rs
@@ -0,0 +1,19 @@
+use super::*;
+
+impl Editor {
+    /// Get the x coordinate of the current cursor
+    /// (if out of bound, it's the length which is given)
+    pub fn x(&self) -> usize {
+        let x = self.cursor().x;
+        let y = self.cursor().y;
+        if x > self.text[y].len() {
+            self.text[y].len()
+        } else {
+            x
+        }
+    }
+    /// Get y coordinate
+    pub fn y(&self) -> usize {
+        self.cursor().y
+    }
+}
