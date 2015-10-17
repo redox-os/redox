424a503c014da1f10c534771fa5f5e1199951dcb
stratact <stratact1@gmail.com>
2015-10-17 03:07:57 -0700

    Fixed error for filesystem/apps/sodium/exec.rs

--- filesystem/apps/sodium/exec.rs
+++ filesystem/apps/sodium/exec.rs
@@ -11,44 +11,39 @@ pub struct InstructionIterator<'a, I: 'a> {
 
 impl<'a, I: Iterator<Item = EventOption>> Iterator for InstructionIterator<'a, I> {
     type Item = Inst;
-    
+
     fn next(&mut self) -> Option<Inst> {
         let mut n = 0;
 
         let mut last = '\0';
-        for e in self.iter {
-            match e {
-                EventOption::Key(k) if k.pressed => {
-                    let c = k.character;
-                    match *self.mode {
-                        Mode::Primitive(_) => {
-                            Inst(0, c);
-                        },
-                        Mode::Command(_) => {
-                            n = match c {
-                                '0' if n != 0 => n * 10,
-                                '1'           => n * 10 + 1,
-                                '2'           => n * 10 + 2,
-                                '3'           => n * 10 + 3,
-                                '4'           => n * 10 + 4,
-                                '5'           => n * 10 + 5,
-                                '6'           => n * 10 + 6,
-                                '7'           => n * 10 + 7,
-                                '8'           => n * 10 + 8,
-                                '9'           => n * 10 + 9,
-                                _             => {
-                                    last = c;
-                                    break;
-                                },
+        while let Some(EventOption::Key(k)) = self.iter.next() {
+            if k.pressed {
+                let c = k.character;
+                match *self.mode {
+                    Mode::Primitive(_) => {
+                        Inst(0, c);
+                    },
+                    Mode::Command(_) => {
+                        n = match c {
+                            '0' if n != 0 => n * 10,
+                            '1'           => n * 10 + 1,
+                            '2'           => n * 10 + 2,
+                            '3'           => n * 10 + 3,
+                            '4'           => n * 10 + 4,
+                            '5'           => n * 10 + 5,
+                            '6'           => n * 10 + 6,
+                            '7'           => n * 10 + 7,
+                            '8'           => n * 10 + 8,
+                            '9'           => n * 10 + 9,
+                            _             => {
+                                last = c;
+                                break;
                             }
-
                         }
                     }
-                },
-                _ => {},
+                }
             }
         }
-
         Some(Inst(if n == 0 { 1 } else { n }, last))
     }
 }
