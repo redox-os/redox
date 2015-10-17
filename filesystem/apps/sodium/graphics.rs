3fef4d49405304d2c8de656b6e54c97b84382210
Ticki <Ticki@users.noreply.github.com>
2015-10-17 17:25:15 +0200

    Swap x and y

--- filesystem/apps/sodium/graphics.rs
+++ filesystem/apps/sodium/graphics.rs
@@ -10,18 +10,18 @@ impl Editor {
         for (y, row) in self.text.iter().enumerate() {
             for (x, c) in row.iter().enumerate() {
                 if self.cursor().x == x && self.cursor().y == y {
-                    self.window.rect(8 * (y - self.scroll_y) as isize,
-                                     16 * (x - self.scroll_x) as isize,
+                    self.window.rect(8 * (x - self.scroll_y) as isize,
+                                     16 * (y - self.scroll_x) as isize,
                                      8,
                                      16,
                                      [255, 255, 255, 255]);
-                    self.window.char(8 * (y - self.scroll_y) as isize,
-                                     16 * (x - self.scroll_x) as isize,
+                    self.window.char(8 * (x - self.scroll_y) as isize,
+                                     16 * (y - self.scroll_x) as isize,
                                      *c,
                                      [0, 0, 0, 255]);
                 } else {
-                    self.window.char(8 * (y - self.scroll_y) as isize,
-                                     16 * (x - self.scroll_x) as isize,
+                    self.window.char(8 * (x - self.scroll_y) as isize,
+                                     16 * (y - self.scroll_x) as isize,
                                      *c,
                                      [255, 255, 255, 255]);
                 }
