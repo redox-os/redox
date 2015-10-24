0addc3afdd9e1296635f2192690943f68b4bbcc1
Ticki <Ticki@users.noreply.github.com>
2015-10-24 16:54:33 +0200

    Refactor the methods of the vertical axis

--- filesystem/apps/sodium/motion.rs
+++ filesystem/apps/sodium/motion.rs
@@ -1,8 +1,13 @@
 use super::*;
 
 impl Editor {
-    pub fn to_motion(Inst(n, cmd): Inst) -> (usize, usize) {
-        // TODO
-        (0, 0)
+    pub fn to_motion(&self, Inst(n, cmd): Inst) -> (usize, usize) {
+        match cmd {
+            'h' => self.left_pos(n),
+            'l' => self.right_pos(n),
+            'j' => self.down_pos(n),
+            'k' => self.up_pos(n),
+            _ => (0, 0),
+        }
     }
 }
