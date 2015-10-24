f7cc35ff1df21e0cab5100dbc7fdc6bb1217ca16
Ticki <Ticki@users.noreply.github.com>
2015-10-24 15:16:06 +0200

    Fix out-of-bound bug
    Caused by missing checks in the .delete() function

--- filesystem/apps/sodium/delete.rs
+++ filesystem/apps/sodium/delete.rs
@@ -6,8 +6,10 @@ impl Editor {
         let y = self.y();
         let x = self.x();
         if self.text[y].len() == 0 {
-            self.text.remove(y);
-        } else {
+            if self.text.len() != 1 {
+                self.text.remove(y);
+            }
+        } else if x < self.text[y].len() {
             self.text[y].remove(x);
         }
     }
