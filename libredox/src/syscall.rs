fec922555192ff8125d6aec0cbdf1cb1c96f1cc8
Jeremy Soller <jackpot51@gmail.com>
2015-10-25 17:36:20 -0600

    Organize syscalls, add WIP link and unlink

--- libredox/src/syscall.rs
+++ libredox/src/syscall.rs
@@ -35,6 +35,26 @@ pub unsafe fn sys_debug(byte: u8) {
     syscall(SYS_DEBUG, byte as usize, 0, 0);
 }
 
+pub unsafe fn sys_brk(addr: usize) -> usize {
+    syscall(SYS_BRK, addr, 0, 0)
+}
+
+pub unsafe fn sys_chdir(path: *const u8) -> usize {
+    syscall(SYS_CHDIR, path as usize, 0, 0)
+}
+
+pub unsafe fn sys_close(fd: usize) -> usize {
+    syscall(SYS_CLOSE, fd, 0, 0)
+}
+
+pub unsafe fn sys_dup(fd: usize) -> usize {
+    syscall(SYS_DUP, fd, 0, 0)
+}
+
+pub unsafe fn sys_execve(path: *const u8) -> usize {
+    syscall(SYS_EXECVE, path as usize, 0, 0)
+}
+
 pub unsafe fn sys_exit(status: isize) {
     syscall(SYS_EXIT, status as usize, 0, 0);
 }
@@ -43,48 +63,49 @@ pub unsafe fn sys_fork() -> usize {
     syscall(SYS_FORK, 0, 0, 0)
 }
 
-pub unsafe fn sys_read(fd: usize, buf: *mut u8, count: usize) -> usize {
-    syscall(SYS_READ, fd, buf as usize, count)
-}
-
-pub unsafe fn sys_write(fd: usize, buf: *const u8, count: usize) -> usize {
-    syscall(SYS_WRITE, fd, buf as usize, count)
-}
-
-pub unsafe fn sys_open(path: *const u8, flags: usize, mode: usize) -> usize {
-    syscall(SYS_OPEN, path as usize, flags, mode)
-}
-
-pub unsafe fn sys_dup(fd: usize) -> usize {
-    syscall(SYS_DUP, fd, 0, 0)
-}
-
 pub unsafe fn sys_fpath(fd: usize, buf: *mut u8, len: usize) -> usize {
     syscall(SYS_FPATH, fd, buf as usize, len)
 }
 
-pub unsafe fn sys_close(fd: usize) -> usize {
-    syscall(SYS_CLOSE, fd, 0, 0)
+//TODO: FSTAT
+
+pub unsafe fn sys_fsync(fd: usize) -> usize {
+    syscall(SYS_FSYNC, fd, 0, 0)
 }
 
-pub unsafe fn sys_execve(path: *const u8) -> usize {
-    syscall(SYS_EXECVE, path as usize, 0, 0)
+#[repr(packed)]
+pub struct TV {
+    pub tv_sec: i64,
+    pub tv_usec: i32,
+}
+
+//TODO: gettimeofday
+pub unsafe fn sys_gettimeofday(tv: *mut TV) -> usize{
+    syscall(SYS_GETTIMEOFDAY, tv as usize, 0, 0)
+}
+
+pub unsafe fn sys_link(old: *const u8, new: *const u8) -> usize {
+    syscall(SYS_LINK, old as usize, new as usize, 0)
 }
 
 pub unsafe fn sys_lseek(fd: usize, offset: isize, whence: usize) -> usize {
     syscall(SYS_LSEEK, fd, offset as usize, whence)
 }
 
-pub unsafe fn sys_fsync(fd: usize) -> usize {
-    syscall(SYS_FSYNC, fd, 0, 0)
+pub unsafe fn sys_open(path: *const u8, flags: usize, mode: usize) -> usize {
+    syscall(SYS_OPEN, path as usize, flags, mode)
 }
 
-pub unsafe fn sys_time(time_ptr: *mut Duration, realtime: bool) {
-    syscall(SYS_TIME, time_ptr as usize, realtime as usize, 0);
+pub unsafe fn sys_read(fd: usize, buf: *mut u8, count: usize) -> usize {
+    syscall(SYS_READ, fd, buf as usize, count)
 }
 
-pub unsafe fn sys_brk(addr: usize) -> usize {
-    syscall(SYS_BRK, addr, 0, 0)
+pub unsafe fn sys_unlink(path: *const u8) -> usize {
+    syscall(SYS_UNLINK, path as usize, 0, 0)
+}
+
+pub unsafe fn sys_write(fd: usize, buf: *const u8, count: usize) -> usize {
+    syscall(SYS_WRITE, fd, buf as usize, count)
 }
 
 pub unsafe fn sys_yield() {
