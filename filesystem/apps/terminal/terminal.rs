ecd9f3486cbfd84c9968a04efcacfad9aedffeff
stratact <stratact1@gmail.com>
2015-10-25 14:06:32 -0700

    Add `exit` command to terminal

--- filesystem/apps/terminal/terminal.rs
+++ filesystem/apps/terminal/terminal.rs
@@ -7,6 +7,7 @@ use redox::io::*;
 use redox::console::*;
 use redox::env::*;
 use redox::to_num::*;
+use redox::syscall;
 
 /* Magic Macros { */
 static mut application: *mut Application = 0 as *mut Application;
@@ -185,7 +186,7 @@ impl Command {
             },
         });
 
-        let command_list = commands.iter().fold(String::new(), |l , c| l + " " + &c.name);
+        let command_list = commands.iter().fold(String::new(), |l , c| l + " " + &c.name) + " exit";
 
         commands.push(Command {
             name: "help".to_string(),
@@ -392,7 +393,9 @@ impl Application {
 
         while let Some(command) = readln!() {
             println!("# {}", command);
-            if command.len() > 0 {
+            if command == "exit" {
+                break;
+            } else if command.len() > 0 {
                 self.on_command(&command);
             }
         }
