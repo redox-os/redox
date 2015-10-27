9698215593dba94ee50b0b22178fe5ff3d9ea934
Ticki <Ticki@users.noreply.github.com>
2015-10-27 20:35:14 +0100

    Configuarationalizationise them all

--- filesystem/apps/sodium/prompt.rs
+++ filesystem/apps/sodium/prompt.rs
@@ -3,6 +3,21 @@ use redox::*;
 
 impl Editor {
     pub fn invoke(&mut self, cmd: String) {
+        let mut split = cmd.split(' ');
+        let base_cmd = split.nth(0).unwrap_or("");
+        let sec_cmd = split.nth(0).unwrap_or("");
 
+        match base_cmd {
+            "set" => {
+                self.options.set(sec_cmd);
+            },
+            "unset" => {
+                self.options.unset(sec_cmd);
+            },
+            "toggle" | "tog" => {
+                self.options.toggle(sec_cmd);
+            },
+            _ => {},
+        }
     }
 }
