13eacca4fb63a2c6e47ded3b9e2aafb988a8affd
Ticki <Ticki@users.noreply.github.com>
2015-10-27 12:03:39 +0100

    Fix highlighting

--- filesystem/apps/sodium/graphics.rs
+++ filesystem/apps/sodium/graphics.rs
@@ -16,18 +16,33 @@ impl Editor {
                          16,
                          Color::WHITE);
 
+        let mut string = false;
+
         for (y, row) in self.text.iter().enumerate() {
-            for (x, c) in row.iter().enumerate() {
+            for (x, &c) in row.iter().enumerate() {
+                let color = match c {
+                    '\'' | '"' => {
+                        string = !string;
+                        (226, 225, 167) //(167, 222, 156)
+                    },
+                    _ if string => (226, 225, 167), //(167, 222, 156)
+                    '!' | '@' | '#' | '$' | '%' | '^' | '&' | '|' | '*' | '+' | '-' | '/' | ':' | '=' | '<' | '>' => (198, 83, 83), //(228, 190, 175), //(194, 106, 71),
+                    '.' | ',' => (241, 213, 226),
+                    '(' | ')' | '[' | ']' | '{' | '}' => (164, 212, 125), //(195, 139, 75),
+                    '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => (209, 209, 177),
+                    _ => (255, 255, 255),
+                };
+
                 if self.x() == x && self.y() == y {
                     self.window.char(8 * (x - self.scroll_y) as isize,
                                      16 * (y - self.scroll_x) as isize,
-                                     *c,
-                                     Color::BLACK);
+                                     c,
+                                     Color::rgb(color.0 / 3, color.1 / 3, color.2 / 3));
                 } else {
                     self.window.char(8 * (x - self.scroll_y) as isize,
                                      16 * (y - self.scroll_x) as isize,
-                                     *c,
-                                     Color::WHITE);
+                                     c,
+                                     Color::rgb(color.0, color.1, color.2));
                 }
             }
         }
@@ -35,26 +50,13 @@ impl Editor {
         let w = self.window.width();
         self.window.rect(0, h as isize - 18, w, 18, Color::rgba(74, 74, 74, 255));
 
-        let mut string = false;
-
         for (n, c) in (if self.status_bar.mode.len() > w / (8 * 4) {
             self.status_bar.mode.chars().take(w / (8 * 4) - 5).chain(vec!['.', '.', '.']).collect::<Vec<_>>()
         } else {
             self.status_bar.mode.chars().collect()
         }).into_iter().enumerate() {
-            let color = match c {
-                _ if string => Color::BLUE,
-                '\'' | '"' => {
-                    string = true;
-                    Color::RED
-                },
-                '!' | '@' | '#' | '$' | '%' | '^' | '&' | '*' | '+' | '-' | '/' | ':' | '=' | '<' | '>' => Color::RED,
-                '.' | ',' => Color::BLUE,
-                '(' | ')' | '[' | ']' | '{' | '}' => Color::GREEN,
-                _ => Color::WHITE,
-            };
 
-            self.window.char(n as isize * 8, h as isize - 16 - 1, if c == '\t' { ' ' } else { c }, color);
+            self.window.char(n as isize * 8, h as isize - 16 - 1, if c == '\t' { ' ' } else { c }, Color::RED);
         }
 
         self.window.sync();
