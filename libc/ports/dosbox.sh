68f6478e5b06227673ee17569ab24ab37468c3b6
Jeremy Soller <jackpot51@gmail.com>
2015-10-28 17:43:41 -0600

    Dosbox WIP

---
+++ libc/ports/dosbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source environ.sh
+
+SRC=http://downloads.sourceforge.net/project/dosbox/dosbox/0.74/dosbox-0.74.tar.gz
+DIR=dosbox-0.74
+
+CONFIGURE_ARGS="--host=${HOST} --prefix=${PREFIX} --disable-shared"
+configure_template $*
