fa92b4f638c0b4a2275f8671feaa23393711f34c
Jeremy Soller <jackpot51@gmail.com>
2015-10-29 10:40:01 -0600

    WIP: nanosleep

--- libc/newlib-redox/newlib/libc/sys/redox/time.c
+++ libc/newlib-redox/newlib/libc/sys/redox/time.c
@@ -4,7 +4,6 @@ int gettimeofday(struct timeval *__restrict tv, void *__restrict tz){
     return (int)syscall(SYS_GETTIMEOFDAY, (uint)tv, (uint)tz, 0);
 }
 
-
 int nanosleep(const struct timespec * req, struct timespec * rem){
     return (int)syscall(SYS_NANOSLEEP, (uint)req, (uint)rem, 0);
 }
