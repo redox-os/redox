fd9121b5245990295fa294f0f898817a3424c063
Jeremy Soller <jackpot51@gmail.com>
2015-10-28 22:17:29 -0600

    Posix linkage in newlib

---
+++ libc/newlib-redox/newlib/libc/sys/redox/bits/dirent.h
@@ -0,0 +1,37 @@
+/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#ifndef _SYS_DIRENT_H
+# error "Never use <bits/dirent.h> directly; include <dirent.h> instead."
+#endif
+
+struct dirent
+  {
+    int d_ino;
+    off_t d_off;
+    unsigned short int d_reclen;
+    unsigned char d_type;
+    char d_name[256];		/* We must not include limits.h! */
+  };
+
+#define d_fileno	d_ino	/* Backwards compatibility.  */
+
+#undef  _DIRENT_HAVE_D_NAMLEN
+#define _DIRENT_HAVE_D_RECLEN
+#define _DIRENT_HAVE_D_OFF
+#define _DIRENT_HAVE_D_TYPE
--- libc/newlib-redox/newlib/libc/sys/redox/sys/dirent.h
+++ libc/newlib-redox/newlib/libc/sys/redox/sys/dirent.h
@@ -1,13 +1,26 @@
+/* libc/sys/linux/sys/dirent.h - Directory entry as returned by readdir */
+
+/* Written 2000 by Werner Almesberger */
+
+
 #ifndef _SYS_DIRENT_H
 #define _SYS_DIRENT_H
 
-struct dirent {
-    char d_name[4096];
-};
+#include <sys/types.h>
+#include <bits/dirent.h>
+
+#define HAVE_NO_D_NAMLEN	/* no struct dirent->d_namlen */
+
+#define MAXNAMLEN 255		/* sizeof(struct dirent.d_name)-1 */
+
 
 typedef struct {
     int dd_fd;		/* directory file */
-    struct dirent dd_ent;
+    int dd_loc;		/* position in buffer */
+    int dd_seek;
+    char *dd_buf;	/* buffer */
+    int dd_len;		/* buffer length */
+    int dd_size;	/* amount of data in buffer */
 } DIR;
 
 
@@ -17,7 +30,25 @@ typedef struct {
 
 DIR *opendir(const char *);
 struct dirent *readdir(DIR *);
+int readdir_r(DIR *__restrict, struct dirent *__restrict,
+              struct dirent **__restrict);
 void rewinddir(DIR *);
 int closedir(DIR *);
+
+/* internal prototype */
+void _seekdir(DIR *dir, long offset);
+DIR *_opendir(const char *);
+
+#ifndef _POSIX_SOURCE
+long telldir (DIR *);
+void seekdir (DIR *, off_t loc);
+
+int scandir (const char *__dir,
+             struct dirent ***__namelist,
+             int (*select) (const struct dirent *),
+             int (*compar) (const struct dirent **, const struct dirent **));
+
+int alphasort (const struct dirent **__a, const struct dirent **__b);
+#endif /* _POSIX_SOURCE */
 
 #endif
