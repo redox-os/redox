d44c7b64e935b9a370b24d5d86cb77c517110aa2
Ticki <Ticki@users.noreply.github.com>
2015-10-30 12:21:19 +0100

    Fix bug in d<motion> command

--- filesystem/apps/sodium/selection.rs
+++ filesystem/apps/sodium/selection.rs
@@ -3,9 +3,10 @@ use redox::*;
 
 impl Editor {
     /// Remove from a given motion (row based)
-    pub fn remove_rb<'a>(&mut self, (x, y): (usize, usize)) {
+    pub fn remove_rb<'a>(&mut self, (x, y): (isize, isize)) {
         debugln!("Y: {}, Bounded y: {}", y, self.y());
-        if y == self.y() {
+        if y == self.y() as isize {
+            let (x, y) = self.bound((x as usize, y as usize));
             // Single line mode
             let (a, b) = if self.x() > x {
                 (x, self.x())
@@ -17,6 +18,7 @@ impl Editor {
                 self.text[y].remove(a);
             }
         } else {
+            let (x, y) = self.bound((x as usize, y as usize));
             debugln!("Full line mode!");
             // Full line mode
             let (a, b) = if self.y() < y {
