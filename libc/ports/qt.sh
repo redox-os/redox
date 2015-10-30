c1a5b8aa5afead28d9d461c38adf1e2dde4ebced
Jeremy Soller <jackpot51@gmail.com>
2015-10-30 14:57:47 -0600

    WIP QT

--- libc/ports/qt.sh
+++ libc/ports/qt.sh
@@ -4,5 +4,6 @@ source environ.sh
 SRC=http://download.qt.io/official_releases/qt/5.5/5.5.1/single/qt-everywhere-opensource-src-5.5.1.tar.gz
 DIR=qt-everywhere-opensource-src-5.5.1
 
-CONFIGURE_ARGS="-xplatform redox-g++ -prefix $PREFIX"
+CONFIGURE_ARGS="-xplatform redox-g++ -prefix $PREFIX -arch x86 -opensource -confirm-license -static\
+    -no-largefile -no-accessibility -no-xcb"
 configure_template $*
