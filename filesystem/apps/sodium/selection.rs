05a491154de05a9f76e00c02e52400edcfbe6b18
Ticki <Ticki@users.noreply.github.com>
2015-10-31 08:27:32 +0100

    Remove debug messages

--- filesystem/apps/sodium/selection.rs
+++ filesystem/apps/sodium/selection.rs
@@ -4,7 +4,6 @@ use redox::*;
 impl Editor {
     /// Remove from a given motion (row based)
     pub fn remove_rb<'a>(&mut self, (x, y): (isize, isize)) {
-        debugln!("Y: {}, Bounded y: {}", y, self.y());
         if y == self.y() as isize {
             let (x, y) = self.bound((x as usize, y as usize));
             // Single line mode
@@ -13,13 +12,11 @@ impl Editor {
              } else {
                 (self.x(), x)
             };
-            debugln!("A: {}, B: {}", a, b);
-            for _ in a..b {
+            for _ in a..(b + 1) {
                 self.text[y].remove(a);
             }
         } else {
             let (x, y) = self.bound((x as usize, y as usize));
-            debugln!("Full line mode!");
             // Full line mode
             let (a, b) = if self.y() < y {
                 (self.y(), y)
