7193c003827e79043fc6c7fccc4c1cdb67ee6e90
Jeremy Soller <jackpot51@gmail.com>
2015-11-01 14:14:30 -0700

    Fix read_dir

--- libredox/src/debug.rs
+++ libredox/src/debug.rs
@@ -1,5 +1,22 @@
 use super::syscall::sys_debug;
 
+/// Debug new line to console
+#[macro_export]
+macro_rules! debugln {
+    ($($arg:tt)*) => ({
+        debug!($($arg)*);
+        debug!("\n");
+    });
+}
+
+/// Debug to console
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => ({
+        $crate::debug::debug(format!($($arg)*));
+    });
+}
+
 pub fn debug<T: AsRef<str>>(msg: T) {
     for b in msg.as_ref().bytes() {
         unsafe {
