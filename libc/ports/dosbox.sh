ac2abecf31d6f60941f9a1818bdf4b168004cc49
Jeremy Soller <jackpot51@gmail.com>
2015-11-01 14:51:03 -0700

    Add what is necessary for dosbox, mark dosbox as unstable

--- libc/ports/dosbox.sh
+++ libc/ports/dosbox.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source environ.sh
 
-BROKEN
+UNSTABLE
 
 SRC=http://downloads.sourceforge.net/project/dosbox/dosbox/0.74/dosbox-0.74.tar.gz
 DIR=dosbox-0.74
 
-CONFIGURE_ARGS="--host=${HOST} --prefix=${PREFIX}"
+CONFIGURE_ARGS="--host=${HOST} --prefix=${PREFIX} --with-sdl-prefix=${PREFIX} --disable-opengl --disable-sdltest"
 configure_template $*
