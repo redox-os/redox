2113253625cd82e8633f42b8d17ff51c1e4b7b8d
Theodore DeRego <tderego94@gmail.com>
2015-11-02 12:44:58 -1000

    Documentation for SpaceMapPhys

--- filesystem/apps/zfs/space_map.rs
+++ filesystem/apps/zfs/space_map.rs
@@ -2,19 +2,24 @@ use super::from_bytes::FromBytes;
 
 const SPACE_MAP_HISTOGRAM_SIZE: usize = 32;
 
+/// The `SpaceMapPhys` is the on-disk representation of the space map.
+/// Consumers of space maps should never reference any of the members of this
+/// structure directly. These members may only be updated in syncing context.
+///
+/// Note the smp_object is no longer used but remains in the structure
+/// for backward compatibility.
+///
+/// The smp_histogram maintains a histogram of free regions. Each
+/// bucket, smp_histogram[i], contains the number of free regions
+/// whose size is:
+/// 2^(i+sm_shift) <= size of free region in bytes < 2^(i+sm_shift+1)
+
 #[derive(Debug)]
 pub struct SpaceMapPhys {
     object: u64,   // on-disk space map object
     objsize: u64,  // size of the object
     alloc: u64,    // space allocated from the map
     pad: [u64; 5], // reserved
-
-    /*
-     * The smp_histogram maintains a histogram of free regions. Each
-     * bucket, smp_histogram[i], contains the number of free regions
-     * whose size is:
-     * 2^(i+sm_shift) <= size of free region in bytes < 2^(i+sm_shift+1)
-     */
     histogram: [u64; SPACE_MAP_HISTOGRAM_SIZE],
 }
 
