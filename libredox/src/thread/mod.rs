99bfcb851966b125e9b5900365a1c7b1857dc012
Jeremy Soller <jackpot51@gmail.com>
2015-11-03 16:03:58 -0700

    Very, very basic threads

--- libredox/src/thread/mod.rs
+++ libredox/src/thread/mod.rs
@@ -1,6 +1,10 @@
-use syscall::sys_yield;
+use syscall::{sys_clone, sys_exit};
+use syscall::common::{CLONE_VM, CLONE_FS, CLONE_FILES};
 
 //TODO: JoinHandle
 pub fn spawn<F, T>(f: F) where F: FnOnce() -> T, F: Send + 'static, T: Send + 'static {
-
+    if unsafe { sys_clone(CLONE_VM | CLONE_FS | CLONE_FILES) } == 0 {
+        f();
+        unsafe { sys_exit(0) };
+    }
 }
