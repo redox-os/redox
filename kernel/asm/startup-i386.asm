6f70fadbf8176db9aac5839a73a75161f73f00cd
Jeremy Soller <jackpot51@gmail.com>
2015-11-06 10:12:18 -0700

    Add TSS

--- kernel/asm/startup-i386.asm
+++ kernel/asm/startup-i386.asm
@@ -23,7 +23,7 @@ startup:
   mov cr0, eax
 
   ; far jump to load CS with 32 bit segment
-  jmp 0x08:protected_mode
+  jmp gdt.kernel_code:protected_mode
 
 %include "asm/memory_map.asm"
 %include "asm/vesa.asm"
@@ -31,8 +31,9 @@ startup:
 
 protected_mode:
     use32
+
     ; load all the other segments with 32 bit data segments
-    mov eax, 0x10
+    mov eax, gdt.kernel_data
     mov ds, eax
     mov es, eax
     mov fs, eax
@@ -40,6 +41,9 @@ protected_mode:
     mov ss, eax
     ; set up stack
     mov esp, 0x200000
+
+    mov eax, gdt.tss
+    ltr ax
 
     ;rust init
     mov eax, [kernel_file + 0x18]
@@ -52,26 +56,90 @@ protected_mode:
     jmp .lp
 
 gdtr:
-    dw (gdt_end - gdt) + 1  ; size
+    dw gdt.end + 1  ; size
     dd gdt                  ; offset
 
 gdt:
-    ; null entry
+.null equ $ - gdt
     dq 0
-    ; code entry
+
+.kernel_code equ $ - gdt
     dw 0xffff       ; limit 0:15
     dw 0x0000       ; base 0:15
     db 0x00         ; base 16:23
     db 0b10011010   ; access byte - code
-    db 0xcf         ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0xff         ; flags/(limit 16:19). flag is set to 32 bit protected mode
     db 0x00         ; base 24:31
-    ; data entry
+
+.kernel_data equ $ - gdt
     dw 0xffff       ; limit 0:15
     dw 0x0000       ; base 0:15
     db 0x00         ; base 16:23
     db 0b10010010   ; access byte - data
-    db 0xcf         ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0xff         ; flags/(limit 16:19). flag is set to 32 bit protected mode
     db 0x00         ; base 24:31
-gdt_end:
+
+.user_code equ $ - gdt
+    dw 0xffff       ; limit 0:15
+    dw 0x0000       ; base 0:15
+    db 0x00         ; base 16:23
+    db 0b11111010   ; access byte - code
+    db 0xff         ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0x00         ; base 24:31
+
+.user_data equ $ - gdt
+    dw 0xffff       ; limit 0:15
+    dw 0x0000       ; base 0:15
+    db 0x00         ; base 16:23
+    db 0b11110010   ; access byte - data
+    db 0xff         ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0x00         ; base 24:31
+
+.tss equ $ - gdt
+    dw (tss.end-$$+0x7C00) & 0xFFFF         ; limit 0:15
+    dw (tss-$$+0x7C00) & 0xFFFF             ; base 0:15
+    db ((tss-$$+0x7C00) >> 16) & 0xFF       ; base 16:23
+    db 0b11101001                           ; access byte - data
+    db ((tss.end-$$+0x7C00) >> 16) & 0xF    ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db ((tss-$$+0x7C00) >> 24) & 0xFF       ; base 24:31
+
+.end equ $ - gdt
+
+struc TSS
+    .prev_tss resd 1    ;The previous TSS - if we used hardware task switching this would form a linked list.
+    .esp0 resd 1        ;The stack pointer to load when we change to kernel mode.
+    .ss0 resd 1         ;The stack segment to load when we change to kernel mode.
+    .esp1 resd 1        ;everything below here is unusued now..
+    .ss1 resd 1
+    .esp2 resd 1
+    .ss2 resd 1
+    .cr3 resd 1
+    .eip resd 1
+    .eflags resd 1
+    .eax resd 1
+    .ecx resd 1
+    .edx resd 1
+    .ebx resd 1
+    .esp resd 1
+    .ebp resd 1
+    .esi resd 1
+    .edi resd 1
+    .es resd 1
+    .cs resd 1
+    .ss resd 1
+    .ds resd 1
+    .fs resd 1
+    .gs resd 1
+    .ldt resd 1
+    .trap resw 1
+    .iomap_base resw 1
+endstruc
+
+tss:
+    istruc TSS
+        at TSS.esp0, dd 0x200000
+        at TSS.ss0, dd gdt.kernel_data
+    iend
+.end:
 
 %include "asm/interrupts-i386.asm"
