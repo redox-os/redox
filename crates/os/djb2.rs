95542b921958ad9a8c60241fadf3be86533d051d
Ticki <Ticki@users.noreply.github.com>
2015-11-13 20:02:01 +0100

    Document

--- crates/os/djb2.rs
+++ crates/os/djb2.rs
@@ -1,4 +1,5 @@
 impl Djb2 {
+    /// Create new DJB2 hasher
     pub fn new() -> Self {
         Djb2 {
             state: 5381,
@@ -10,6 +11,7 @@ impl Hasher for Djb2 {
     fn finish(&self) -> u64 {
         self.state
     }
+
     fn write(&mut self, bytes: &[u8]) {
         for &b in bytes {
             self.state = ((self.state << 5) + self.state) + b as u64;
--- crates/os/table.rs
+++ crates/os/table.rs
@@ -1,22 +1,26 @@
 use redox::prelude::v1::*;
 use archive::Archive;
 
+/// A table of nodes, i.e. directories and files
 pub struct NodeTable<'a> {
     pub table: &'a [u8],
 }
 
 impl NodeTable {
+    /// Create a new node table from a slice of bytes
     pub fn new(&self, b: &[u8]) -> Self {
         NodeTable {
             table: b,
         }
     }
 
+    /// Get the n'th entry of the table
     pub fn get_entry(&self, n: usize) -> Option<DataPtr> {
         DataPtr::from_bytes(n, &self.table[n * 16..n * 16 + 16])
     }
 
-    pub fn get<T: Hash>(&self, key: T, probe: &mut usize) -> Option<DataPtr> {
+    /// Get the entry given an key and a probe (in this case the key should is the node name)
+    pub fn get(&self, key: &[u8], probe: &mut usize) -> Option<DataPtr> {
         let hasher = Djb2::new();
         key.hash(hasher);
 
