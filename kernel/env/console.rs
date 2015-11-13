7d0d3f04615d9dd69052f2d1c8e00bc8731a7a51
Jeremy Soller <jackpot51@gmail.com>
2015-11-13 15:35:13 -0700

    WIP: Allow terminal control with ANSI control sequences

--- kernel/env/console.rs
+++ kernel/env/console.rs
@@ -1,18 +1,30 @@
 use alloc::boxed::Box;
 
 use collections::String;
+use collections::Vec;
 
 use graphics::color::Color;
 use graphics::display::Display;
 use graphics::point::Point;
 use graphics::size::Size;
 
+pub enum Escape {
+    None,
+    Char,
+    List
+}
+
 pub struct Console {
     pub display: Box<Display>,
     pub point: Point,
+    pub foreground: Color,
+    pub background: Color,
     pub draw: bool,
     pub redraw: bool,
-    pub command: Option<String>
+    pub command: Option<String>,
+    pub escape: bool,
+    pub escape_sequence: bool,
+    pub sequence: Vec<String>
 }
 
 impl Console {
@@ -20,39 +32,149 @@ impl Console {
         box Console {
             display: unsafe { Display::root() },
             point: Point::new(0, 0),
+            foreground: Color::new(224, 224, 224),
+            background: Color::new(0, 0, 0),
             draw: false,
             redraw: true,
-            command: None
+            command: None,
+            escape: false,
+            escape_sequence: false,
+            sequence: Vec::new()
         }
+    }
+
+    pub fn code(&mut self, c: char){
+        if self.escape_sequence {
+            if c >= '0' && c <= '9' {
+                //Add a number to the sequence list
+                if let Some(mut value) = self.sequence.last_mut() {
+                    value.push(c);
+                }
+            } else if c == ';' {
+                //Split sequence into list
+                self.sequence.push(String::new());
+            } else if c == 'm' {
+                //Display attributes
+                for value in self.sequence.iter() {
+                    if value == "0" {
+                        //Reset all
+                        self.foreground = Color::new(224, 224, 224);
+                        self.background = Color::new(0, 0, 0);
+                    }else if value == "30" {
+                        //Black foreground
+                        self.foreground = Color::new(0, 0, 0);
+                    } else if value == "31" {
+                        //Red foreground
+                        self.foreground = Color::new(255, 127, 127);
+                    } else if value == "32" {
+                        //Green foreground
+                        self.foreground = Color::new(127, 255, 127);
+                    } else if value == "33" {
+                        //Yellow foreground
+                    } else if value == "34" {
+                        //Blue foreground
+                        self.foreground = Color::new(127, 127, 255);
+                    } else if value == "35" {
+                        //Magenta foreground
+                    } else if value == "36" {
+                        //Cyan foreground
+                    } else if value == "37" {
+                        //White foreground
+                        self.foreground = Color::new(224, 224, 224);
+                    }else if value == "40" {
+                        //Black background
+                        self.background = Color::new(0, 0, 0);
+                    } else if value == "41" {
+                        //Red background
+                        self.background = Color::new(255, 127, 127);
+                    } else if value == "42" {
+                        //Green background
+                        self.background = Color::new(127, 255, 127);
+                    } else if value == "43" {
+                        //Yellow background
+                    } else if value == "44" {
+                        //Blue background
+                        self.background = Color::new(127, 127, 255);
+                    } else if value == "45" {
+                        //Magenta background
+                    } else if value == "46" {
+                        //Cyan background
+                    } else if value == "47" {
+                        //White background
+                        self.background = Color::new(224, 224, 224);
+                    }
+                }
+
+                self.escape_sequence = false;
+            } else {
+                self.escape_sequence = false;
+            }
+
+            if ! self.escape_sequence {
+                self.sequence.clear();
+                self.escape = false;
+            }
+        } else if c == '[' {
+            //Control sequence initiator
+
+            self.escape_sequence = true;
+            self.sequence.push(String::new());
+        } else if c == 'c' {
+            //Reset
+            self.point.x = 0;
+            self.point.y = 0;
+            self.foreground = Color::new(224, 224, 224);
+            self.background = Color::new(0, 0, 0);
+            self.display.set(self.background);
+            self.redraw = true;
+
+            self.escape = false;
+        } else {
+            //Unknown escape character
+
+            self.escape = false;
+        }
+    }
+
+    pub fn character(&mut self, c: char){
+        self.display.rect(self.point, Size::new(8, 16), self.background);
+        if c == '\x1B' {
+            self.escape = true;
+        }else if c == '\n' {
+            self.point.x = 0;
+            self.point.y += 16;
+        } else if c == '\x08' {
+            // TODO: Fix up hack for backspace
+            self.point.x -= 8;
+            if self.point.x < 0 {
+                self.point.x = 0
+            }
+            self.display.rect(self.point, Size::new(8, 16), self.background);
+        } else {
+            self.display.char(self.point, c, self.foreground);
+            self.point.x += 8;
+        }
+        if self.point.x >= self.display.width as isize {
+            self.point.x = 0;
+            self.point.y += 16;
+        }
+        while self.point.y + 16 > self.display.height as isize {
+            self.display.scroll(16);
+            self.point.y -= 16;
+        }
+        self.display.rect(self.point, Size::new(8, 16), self.foreground);
+        self.redraw = true;
     }
 
     pub fn write(&mut self, bytes: &[u8]){
         for byte in bytes.iter() {
-            self.display.rect(self.point, Size::new(8, 16), Color::new(0, 0, 0));
-            if *byte == 10 {
-                self.point.x = 0;
-                self.point.y += 16;
-            } else if *byte == 8 {
-                // TODO: Fix up hack for backspace
-                self.point.x -= 8;
-                if self.point.x < 0 {
-                    self.point.x = 0
-                }
-                self.display.rect(self.point, Size::new(8, 16), Color::new(0, 0, 0));
-            } else {
-                self.display.char(self.point, *byte as char, Color::new(255, 255, 255));
-                self.point.x += 8;
+            let c = *byte as char;
+
+            if self.escape {
+                self.code(c);
+            }else{
+                self.character(c);
             }
-            if self.point.x >= self.display.width as isize {
-                self.point.x = 0;
-                self.point.y += 16;
-            }
-            while self.point.y + 16 > self.display.height as isize {
-                self.display.scroll(16);
-                self.point.y -= 16;
-            }
-            self.display.rect(self.point, Size::new(8, 16), Color::new(255, 255, 255));
-            self.redraw = true;
         }
         // If contexts disabled, probably booting up
         if ! unsafe { ::scheduler::context::context_enabled } && self.draw && self.redraw {
