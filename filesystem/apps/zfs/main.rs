dca48e46f14780a9423a73bd2ab74b127dd6d973
Theodore DeRego <tderego94@gmail.com>
2015-11-20 09:05:06 -1000

    playing around

--- filesystem/apps/zfs/main.rs
+++ filesystem/apps/zfs/main.rs
@@ -429,7 +429,7 @@ pub fn main() {
 
                                                     let mut range_tree: avl::Tree<space_map::Entry, u64> =
                                                         avl::Tree::new(Box::new(|x| x.offset()));
-                                                    space_map::load_space_map_avl(&space_map::SpaceMap { size: 15 },
+                                                    space_map::load_space_map_avl(&space_map::SpaceMap { size: 30 },
                                                                                   &mut range_tree,
                                                                                   &space_map.unwrap(),
                                                                                   space_map::MapType::Alloc);
--- filesystem/apps/zfs/spa.rs
+++ filesystem/apps/zfs/spa.rs
@@ -1,4 +1,7 @@
+use super::vdev;
+
 // Storage pool allocator
 pub struct Spa {
     name: String, // Pool name
+    root_vdev: vdev::Vdev,
 }
