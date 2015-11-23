d12ed248c560942389aef271de0daebab82f8e79
Theodore DeRego <tderego94@gmail.com>
2015-11-23 13:29:50 -1000

    Don't use boxes in nvpairs. Flesh out spa and vdev.

--- filesystem/apps/zfs/spa.rs
+++ filesystem/apps/zfs/spa.rs
@@ -1,4 +1,7 @@
+use redox::{Box, String, Vec};
+
 use super::avl;
+use super::nvpair::NvList;
 use super::vdev;
 use super::zfs;
 
@@ -16,20 +19,24 @@ pub struct Spa {
 }
 
 impl Spa {
-    pub fn create(name: String, config: NvList) -> Self {
+    // TODO
+    /*pub fn create(name: String, config: NvList) -> Self {
         let root_vdev = vdev::Vdev::new();
         Self::new(name, root_vdev)
-    }
+    }*/
 
-    pub fn open(&mut self) {
+    pub fn open(&mut self) -> zfs::Result<()> {
         let load_state = zfs::SpaLoadState::Open;
         if self.state == zfs::PoolState::Uninitialized {
             // First time opening
 
             self.activate();
 
-            self.load(load_state);
+            try!(self.load(load_state, ImportType::Existing, false));
+
         }
+
+        Ok(())
     }
 
     fn new(name: String, root_vdev: vdev::Vdev) -> Self {
@@ -41,9 +48,16 @@ impl Spa {
         }
     }
 
-    fn load(&mut self, load_state: zfs::SpaLoadState) {
-        self.load_impl(load_state);
-        self.load_state = zfs::SpaLoadState::Done;
+    fn load(&mut self, load_state: zfs::SpaLoadState,
+            import_type: ImportType, mos_config: bool) -> zfs::Result<()> {
+        let ref config = NvList::new(0); // TODO: this should be replaced by self.config
+
+        let pool_guid = try!(config.get("pool_guid").ok_or(zfs::Error::Invalid));
+
+        self.load_impl(pool_guid, config, load_state, import_type, mos_config);
+        self.load_state = zfs::SpaLoadState::None;
+
+        Ok(())
     }
 
     /// mosconfig: Whether `config` came from on-disk MOS and so is trusted, or was user-made and so
@@ -53,7 +67,7 @@ impl Spa {
         self.load_state = load_state;
 
         // Parse the vdev tree config
-        let nvroot = config.find("vdev_tree").ok_or(zfs::Error::NoEntity);
+        let nvroot = try!(config.get("vdev_tree").ok_or(zfs::Error::Invalid));
         let vdev_alloc_type =
             match import_type {
                 ImportType::Existing => vdev::AllocType::Load,
@@ -65,35 +79,34 @@ impl Spa {
     }
 
     fn parse_vdev_tree(&mut self, nv: &NvList, alloc_type: vdev::AllocType) -> zfs::Result<vdev::Vdev> {
-        let vdev = vdev::Vdev::new(nv, alloc_type);
+        let vdev = vdev::Vdev::load(nv, 0, alloc_type);
 
         // TODO: return here if the vdev is a leaf node
 
         // Get the vdev's children
-        let children = config.find("children").ok_or(zfs::Error::NoEntity);
-        let children =
-            match children {
-                NvList::NvListArray(children) => children,
-                _ => { return Err(zfs::Error::Invalid); },
-            };
+        let children: &Vec<NvList> = try!(nv.get("children").ok_or(zfs::Error::Invalid));
 
         vdev
     }
     
     fn activate(&mut self) {
+        assert!(self.state == zfs::PoolState::Uninitialized);
+
+        self.state = zfs::PoolState::Active;
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub struct SpaNamespace {
-    avl: avl::Tree<Spa, &str>, // AVL tree of Spa sorted by name
+    // TODO: Use &str instead of String as key type. Lifetimes are hard.
+    avl: avl::Tree<Spa, String>, // AVL tree of Spa sorted by name
 }
 
 impl SpaNamespace {
     pub fn new() -> Self {
         SpaNamespace {
-            avl: avl::Tree::new(Box::new(|x| x.name.as_str())),
+            avl: avl::Tree::new(Box::new(|x| x.name.clone())),
         }
     }
 
@@ -101,11 +114,11 @@ impl SpaNamespace {
         self.avl.insert(spa);
     }
 
-    pub fn find<'a>(&'a self, name: &str) -> Option<&'a Spa> {
+    pub fn find(&self, name: String) -> Option<&Spa> {
         self.avl.find(name)
     }
 
-    pub fn find_mut<'a>(&'a mut self, name: &str) -> Option<&'a mut Spa> {
+    pub fn find_mut(&mut self, name: String) -> Option<&mut Spa> {
         self.avl.find_mut(name)
     }
 }
