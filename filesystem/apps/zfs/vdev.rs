9580c2b6d8e552c5797f40ca073cab7b9a2f3db1
Theodore DeRego <tderego94@gmail.com>
2015-11-26 13:30:07 -1000

    Added VdevOps

--- filesystem/apps/zfs/vdev.rs
+++ filesystem/apps/zfs/vdev.rs
@@ -1,9 +1,11 @@
-use redox::{String, ToString, Vec};
+use redox::{Box, String, ToString, Vec};
 
 use super::from_bytes::FromBytes;
 use super::metaslab::{Metaslab, MetaslabGroup};
 use super::nvpair::{NvList, NvValue};
+use super::vdev_file::VdevFile;
 use super::zfs;
+use super::zio;
 
 #[repr(packed)]
 pub struct VdevLabel {
@@ -14,6 +16,58 @@ pub struct VdevLabel {
 }
 
 impl FromBytes for VdevLabel { }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub trait IVdevOps {
+    /// Returns (size, max_size, ashift)
+    fn open(&mut self, vdev: &mut Vdev) -> zfs::Result<(u64, u64, u64)>;
+
+    fn close(&mut self, vdev: &mut Vdev);
+    
+    /// Default asize function: return the MAX of psize with the asize of all children.  This is
+    /// what's used by anything other than RAID-Z.
+    fn asize(&mut self, vdev: &mut Vdev, psize: u64) -> u64;
+
+    fn hold(&mut self, vdev: &mut Vdev);
+
+    fn release(&mut self, vdev: &mut Vdev);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct VdevOps {
+    pub ops: Box<IVdevOps>,
+    //io_start: fn(&zio::Zio),
+    //io_done: fn(&zio::Zio),
+    //state_change: fn(),
+    vdev_type: String,
+    is_leaf: bool,
+}
+
+impl VdevOps {
+    pub fn vdev_type(&self) -> &str {
+        self.vdev_type.as_ref()
+    }
+    pub fn is_leaf(&self) -> bool {
+        self.is_leaf
+    }
+}
+
+fn load_ops(vdev_type: &str, nv: &NvList) -> zfs::Result<VdevOps> {
+    match vdev_type {
+        "disk" => {
+            Ok(VdevOps{
+                ops: Box::new(try!(VdevFile::load(nv))),
+                vdev_type: "disk".to_string(),
+                is_leaf: true,
+            })
+        },
+        _ => {
+            Err(zfs::Error::Invalid)
+        },
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -69,20 +123,12 @@ impl Top {
 
 pub struct Leaf {
     whole_disk: u64,
-    path: String,
-    dev_id: String,
-    phys_path: String,
-    fru: String, // physical fru location
 }
 
 impl Leaf {
     pub fn new() -> Self {
         Leaf {
             whole_disk: 0,
-            path: "".to_string(),
-            dev_id: "".to_string(),
-            phys_path: "".to_string(),
-            fru: "".to_string(),
         }
     }
 }
@@ -101,17 +147,17 @@ pub struct Vdev {
     ashift: u64, // block alignment shift
     state: State,
     prev_state: State,
-    //ops: VdevOps,
+    pub ops: VdevOps,
     parent: Option<TreeIndex>,
     children: Vec<TreeIndex>,
     create_txg: u64, // txg when top-level was added
 
-    top: Option<Top>,
-    leaf: Option<Leaf>,
+    pub top: Option<Top>,
+    pub leaf: Option<Leaf>,
 }
 
 impl Vdev {
-    pub fn new(id: u64, guid: Option<u64>, ashift: u64, create_txg: u64) -> Self {
+    pub fn new(id: u64, guid: Option<u64>, ashift: u64, ops: VdevOps, create_txg: u64) -> Self {
         let guid =
             guid.unwrap_or_else(|| {
                 // TODO: generate a guid
@@ -128,6 +174,7 @@ impl Vdev {
             ashift: ashift,
             state: State::Closed,
             prev_state: State::Unknown,
+            ops: ops,
             parent: None,
             children: Vec::new(),
             create_txg: create_txg,
@@ -139,6 +186,8 @@ impl Vdev {
 
     pub fn load(nv: &NvList, id: u64, parent: Option<TreeIndex>, alloc_type: AllocType) -> zfs::Result<Self> {
         let vdev_type = try!(nv.get::<&String>("type").ok_or(zfs::Error::Invalid)).clone();
+
+        let ops = try!(load_ops(vdev_type.as_ref(), nv));
 
         if alloc_type == AllocType::Load {
             // Verify the provided id matches the id written in the MOS
@@ -159,7 +208,7 @@ impl Vdev {
         let create_txg = try!(nv.get("create_txg").ok_or(zfs::Error::Invalid));
         let ashift = try!(nv.get("ashift").ok_or(zfs::Error::Invalid));
 
-        let mut vdev = Self::new(id, guid, ashift, create_txg);
+        let mut vdev = Self::new(id, guid, ashift, ops, create_txg);
         vdev.parent = parent;
 
         Ok(vdev)
