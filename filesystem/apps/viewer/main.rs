1aa0c3bb5672ddc24a85ffa33c1275264764969b
Jeremy Soller <jackpot51@gmail.com>
2015-12-03 12:18:07 -0700

    Fix env::args to match libstd

--- filesystem/apps/viewer/main.rs
+++ filesystem/apps/viewer/main.rs
@@ -8,9 +8,9 @@ use orbital::*;
 
 #[no_mangle]
 pub fn main() {
-    let url = match args().get(1) {
+    let url = match env::args().nth(1) {
         Some(arg) => arg.clone(),
-        None => "none://",
+        None => "none:",
     };
 
     let bmp = BmpFile::from_path(url);
--- libredox/src/env.rs
+++ libredox/src/env.rs
@@ -3,6 +3,7 @@
 use alloc::boxed::Box;
 
 use fs::File;
+use slice::Iter;
 use string::{String, ToString};
 use vec::Vec;
 
@@ -11,8 +12,8 @@ use syscall::sys_chdir;
 static mut _args: *mut Vec<&'static str> = 0 as *mut Vec<&'static str>;
 
 /// Arguments
-pub fn args<'a>() -> &'a Vec<&'static str> {
-    unsafe { &*_args }
+pub fn args<'a>() -> Iter<'a, &'static str> {
+    unsafe { (*_args).iter() }
 }
 
 /// Initialize arguments
@@ -29,17 +30,19 @@ pub unsafe fn args_destroy() {
 
 /// Method to return the current directory
 /// If the current directory cannot be found, None will be returned
-pub fn current_dir() -> Option<String> {
+pub fn current_dir() -> Result<String, ()> {
     // Return the current path
     if let Some(file) = File::open("") {
-        file.path()
-    } else {
-        None
+        if let Some(path) = file.path() {
+            return Ok(path);
+        }
     }
+
+    Err(())
 }
 
 /// Set the current directory
-pub fn set_current_dir(path: &str) -> bool {
+pub fn set_current_dir(path: &str) -> Result<(), ()> {
     let file_option = if path.is_empty() || path.ends_with('/') {
         File::open(path)
     } else {
@@ -50,10 +53,10 @@ pub fn set_current_dir(path: &str) -> bool {
         if let Some(file_path) = file.path() {
             let path_c = file_path + "\0";
             if unsafe { sys_chdir(path_c.as_ptr()) } == 0 {
-                return true;
+                return Ok(());
             }
         }
     }
 
-    false
+    Err(())
 }
