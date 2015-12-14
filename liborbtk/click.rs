585d83ee2df380ffcb27aa271f02e3e8069fe401
Jeremy Soller <jackpot51@gmail.com>
2015-12-14 12:26:21 -0700

    liborbtk is now a submodule

--- liborbtk/click.rs
+++
@@ -1,6 +0,0 @@
-use super::Point;
-
-pub trait Click {
-    fn click(&mut self, point: Point);
-    fn on_click(&mut self, func: Box<Fn(&mut Self, Point)>) -> &mut Self;
-}
--- liborbtk/widget.rs
+++
@@ -1,8 +0,0 @@
-use super::{Event, Renderer};
-
-use std::any::Any;
-
-pub trait Widget : Any {
-    fn draw(&self, renderer: &mut Renderer);
-    fn event(&mut self, event: &Event);
-}
