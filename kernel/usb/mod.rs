b264f62e47fd6122b577786f6c5163bc5409e0d0
Jeremy Soller <jackpot51@gmail.com>
2016-01-02 17:58:45 -0700

    Cleanup descriptor and set address functions

--- kernel/usb/mod.rs
+++ kernel/usb/mod.rs
@@ -1,6 +1,15 @@
+use self::setup::Setup;
+
 pub mod desc;
 pub mod ehci;
 pub mod ohci;
 pub mod setup;
 pub mod uhci;
 pub mod xhci;
+
+#[derive(Debug)]
+pub enum UsbMsg<'a> {
+    Setup(&'a Setup),
+    In(&'a mut [u8]),
+    Out(&'a [u8]),
+}
