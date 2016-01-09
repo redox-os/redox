2645378cf5f95a7fd63ca420c84c4cbf50100502
polymetric <polymetricofficial@gmail.com>
2016-01-09 12:07:26 -0500

    fix rustfmt on liballoc_system

--- liballoc_system/lib.rs
+++ liballoc_system/lib.rs
@@ -40,10 +40,6 @@ extern "C" {
     fn __rust_allocate(size: usize, align: usize) -> *mut u8;
     fn __rust_deallocate(ptr: *mut u8, old_size: usize, align: usize);
     fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize, align: usize) -> *mut u8;
-    fn __rust_reallocate_inplace(ptr: *mut u8,
-                                 old_size: usize,
-                                 size: usize,
-                                 align: usize)
-                                 -> usize;
+    fn __rust_reallocate_inplace(ptr: *mut u8, old_size: usize, size: usize, align: usize) -> usize;
     fn __rust_usable_size(size: usize, align: usize) -> usize;
 }
