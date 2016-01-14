22181e412cef46cb85cd3877e9eae8329d84b63b
Cyril Plisko <cyril.plisko@mountall.com>
2016-01-14 23:42:02 +0200

    Drop original Pio[8,16,32] API

--- kernel/drivers/pio.rs
+++ kernel/drivers/pio.rs
@@ -97,45 +97,6 @@ impl<T> Pio<T>
     }
 }
 
-/// PIO8
-#[derive(Copy, Clone)]
-pub struct Pio8 {
-    port: u16,
-}
-
-impl Pio8 {
-    /// Create a PIO8 from a given port
-    pub fn new(port: u16) -> Self {
-        return Pio8 { port: port };
-    }
-
-    /// Read
-    pub unsafe fn read(&self) -> u8 {
-        let value: u8;
-        asm!("in $0, $1" : "={al}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
-        return value;
-    }
-
-    /// Write
-    pub unsafe fn write(&mut self, value: u8) {
-        asm!("out $1, $0" : : "{al}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
-    }
-
-    pub unsafe fn readf(&self, flags: u8) -> bool {
-        self.read() & flags == flags
-    }
-
-    pub unsafe fn writef(&mut self, flags: u8, value: bool) {
-        if value {
-            let value = self.read() | flags;
-            self.write(value);
-        } else {
-            let value = self.read() & !flags;
-            self.write(value);
-        }
-    }
-}
-
 // TODO: Remove
 pub unsafe fn inb(port: u16) -> u8 {
     Pio::<u8>::new(port).read()
@@ -146,45 +107,6 @@ pub unsafe fn outb(port: u16, value: u8) {
     Pio::<u8>::new(port).write(value);
 }
 
-/// PIO16
-#[derive(Copy, Clone)]
-pub struct Pio16 {
-    port: u16,
-}
-
-impl Pio16 {
-    /// Create a new PIO16 from a given port
-    pub fn new(port: u16) -> Self {
-        return Pio16 { port: port };
-    }
-
-    /// Read
-    pub unsafe fn read(&self) -> u16 {
-        let value: u16;
-        asm!("in $0, $1" : "={ax}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
-        return value;
-    }
-
-    /// Write
-    pub unsafe fn write(&mut self, value: u16) {
-        asm!("out $1, $0" : : "{ax}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
-    }
-
-    pub unsafe fn readf(&self, flags: u16) -> bool {
-        self.read() & flags == flags
-    }
-
-    pub unsafe fn writef(&mut self, flags: u16, value: bool) {
-        if value {
-            let value = self.read() | flags;
-            self.write(value);
-        } else {
-            let value = self.read() & !flags;
-            self.write(value);
-        }
-    }
-}
-
 // TODO: Remove
 pub unsafe fn inw(port: u16) -> u16 {
     Pio::<u16>::new(port).read()
@@ -193,45 +115,6 @@ pub unsafe fn inw(port: u16) -> u16 {
 // TODO: Remove
 pub unsafe fn outw(port: u16, value: u16) {
     Pio::<u16>::new(port).write(value);
-}
-
-/// PIO32
-#[derive(Copy, Clone)]
-pub struct Pio32 {
-    port: u16,
-}
-
-impl Pio32 {
-    /// Create a new PIO32 from a port
-    pub fn new(port: u16) -> Self {
-        return Pio32 { port: port };
-    }
-
-    /// Read
-    pub unsafe fn read(&self) -> u32 {
-        let value: u32;
-        asm!("in $0, $1" : "={eax}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
-        return value;
-    }
-
-    /// Write
-    pub unsafe fn write(&mut self, value: u32) {
-        asm!("out $1, $0" : : "{eax}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
-    }
-
-    pub unsafe fn readf(&self, flags: u32) -> bool {
-        self.read() & flags == flags
-    }
-
-    pub unsafe fn writef(&mut self, flags: u32, value: bool) {
-        if value {
-            let value = self.read() | flags;
-            self.write(value);
-        } else {
-            let value = self.read() & !flags;
-            self.write(value);
-        }
-    }
 }
 
 // TODO: Remove
