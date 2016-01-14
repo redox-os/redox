66724c32975c2f36d7fcfe29ff691136a9be8d10
Cyril Plisko <cyril.plisko@mountall.com>
2016-01-14 16:00:08 +0200

    Introduce PIO Generics

--- kernel/drivers/pio.rs
+++ kernel/drivers/pio.rs
@@ -1,4 +1,101 @@
-use core::{u8, u16, u32};
+use core::cmp::PartialEq;
+use core::ops::{BitAnd, BitOr, Not};
+use core::marker::PhantomData;
+
+pub trait ReadWrite<T>
+{
+    fn read(&self) -> T;
+    fn write(&self, value: T);
+}
+
+/// Generic PIO
+#[derive(Copy, Clone)]
+pub struct Pio<T> {
+    port: u16,
+    value: PhantomData<T>,
+}
+
+/// Read/Write for byte PIO
+impl ReadWrite<u8> for Pio<u8> {
+    /// Read
+    fn read(&self) -> u8 {
+        let value: u8;
+        unsafe {
+            asm!("in $0, $1" : "={al}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+        return value;
+    }
+
+    /// Write
+    fn write(&self, value: u8) {
+        unsafe {
+            asm!("out $1, $0" : : "{al}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+    }
+}
+
+/// Read/Write for word PIO
+impl ReadWrite<u16> for Pio<u16> {
+    /// Read
+    fn read(&self) -> u16 {
+        let value: u16;
+        unsafe {
+            asm!("in $0, $1" : "={ax}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+        return value;
+    }
+
+    /// Write
+    fn write(&self, value: u16) {
+        unsafe {
+            asm!("out $1, $0" : : "{ax}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+    }
+}
+
+/// Read/Write for doubleword PIO
+impl ReadWrite<u32> for Pio<u32> {
+    /// Read
+    fn read(&self) -> u32 {
+        let value: u32;
+        unsafe {
+            asm!("in $0, $1" : "={eax}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+        return value;
+    }
+
+    /// Write
+    fn write(&self, value: u32) {
+        unsafe {
+            asm!("out $1, $0" : : "{eax}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+    }
+}
+
+impl<T> Pio<T>
+    where Pio<T>: ReadWrite<T>,
+          T: BitAnd<Output = T> + BitOr<Output = T> + PartialEq<T> + Not<Output = T> + Copy
+{
+    /// Create a PIO from a given port
+    pub fn new(port: u16) -> Self {
+        return Pio::<T> {
+            port: port,
+            value: PhantomData,
+        };
+    }
+
+    pub fn readf(&self, flags: T) -> bool {
+        (self.read() & flags) as T == flags
+    }
+
+    pub fn writef(&mut self, flags: T, value: bool) {
+        let tmp: T = match value {
+            true => self.read() | flags,
+            false => self.read() & !flags,
+        };
+        self.write(tmp);
+    }
+}
 
 /// PIO8
 #[derive(Copy, Clone)]
@@ -33,7 +130,7 @@ impl Pio8 {
             let value = self.read() | flags;
             self.write(value);
         } else {
-            let value = self.read() & (u8::MAX - flags);
+            let value = self.read() & !flags;
             self.write(value);
         }
     }
@@ -82,7 +179,7 @@ impl Pio16 {
             let value = self.read() | flags;
             self.write(value);
         } else {
-            let value = self.read() & (u16::MAX - flags);
+            let value = self.read() & !flags;
             self.write(value);
         }
     }
@@ -131,7 +228,7 @@ impl Pio32 {
             let value = self.read() | flags;
             self.write(value);
         } else {
-            let value = self.read() & (u32::MAX - flags);
+            let value = self.read() & !flags;
             self.write(value);
         }
     }
