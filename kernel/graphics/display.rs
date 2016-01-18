4e6793fc2ec74d3aeaa169ead42f029cd12c4393
Jeremy Soller <jackpot51@gmail.com>
2016-01-18 13:31:43 -0700

    Compile fonts into kernel binary

--- kernel/graphics/display.rs
+++ kernel/graphics/display.rs
@@ -51,7 +51,7 @@ pub struct VBEModeInfo {
 
 pub const VBEMODEINFO: *const VBEModeInfo = 0x5200 as *const VBEModeInfo;
 
-pub static mut fonts: usize = 0;
+pub static font: &'static [u8] = include_bytes!("../../filesystem/ui/unifont.font");
 
 /// A display
 pub struct Display {
@@ -243,152 +243,6 @@ impl Display {
         }
     }
 
-    // TODO: Move to orbital?
-
-    /// Draw an line (without antialiasing) with width 1
-    /// (using Bresenham's algorithm)
-    pub fn line(&self, point_a: Point, point_b: Point, color: Color) {
-        // Calculate delta
-        let delta = point_b - point_a;
-
-        if delta.x == 0 {
-            // Handle case where delta x = 0
-            // Set offset
-            let mut y = point_a.y;
-
-            // While the endpoint isn't reached
-            while y != point_b.y {
-                // Set pixel
-                self.pixel(Point::new(point_a.x, y), color);
-                // Increase
-                y += 1;
-            }
-        } else {
-            // Find error and error change
-            let mut error = 0.0;
-            // This is a bit annoying... but libcore does not have .abs() defined on f64 ;-(
-            let delta_error = {
-                let delta_error_signed = (delta.y as f64) / (delta.x as f64);
-                if delta_error_signed < 0.0 {
-                    -delta_error_signed
-                } else {
-                    delta_error_signed
-                }
-            };
-
-            let mut y = 0;
-
-            for x in point_a.x..point_b.x {
-                // Draw pixel
-                self.pixel(Point::new(x, y), color);
-
-                // Update error
-                error += delta_error;
-
-                while error >= 0.5 {
-                    // Draw pixel
-                    self.pixel(Point::new(x, y), color);
-
-                    // Update y
-                    y += if delta.y > 0 {
-                        1
-                    } else {
-                        -1
-                    };
-
-                    // Decrease error
-                    error -= 1.0;
-                }
-            }
-        }
-    }
-    // TODO: Antialiased lines
-    // TODO: Lines with other width
-
-    // Commented because std::f64 is required
-    // fn line_aa(&self, mut point_a: Point, mut point_b: Point, color: RgbColor) {
-    // TODO: Xiaolin Wu line drawing
-    // use core::mem::swap;
-    //
-    // let steep = (point_b.y - point_a.y).abs() > (point_b.x - point_a.x);
-    //
-    // if steep {
-    // Swap the x and y
-    // swap(&mut point_a.x, &mut point_a.y);
-    // swap(&mut point_b.x, &mut point_b.y);
-    // }
-    //
-    // if point_a.x > point_b.x {
-    // Swap point_a and point_b
-    // swap(&mut point_a.x, &mut point_b.x);
-    // swap(&mut point_a.y, &mut point_b.y);
-    // }
-    //
-    // Calculate delta
-    // let dx = point_b.x - point_a.x;
-    // let dy = point_b.y - point_a.y;
-    // Calculate gradient
-    // let gradient = (dy as f64) / (dx as f64);
-    //
-    // First endpoint
-    // let x_end = point_b.x.round();
-    // let y_end = point_b.y + gradient * (x_end - point_b.x);
-    //
-    // let x_pxl2 = x_end;
-    // let y_pxl2 = y_end.floor();
-    //
-    // if steep {
-    // self
-    // } else {
-    // self
-    // };
-    //
-    // }
-    //
-
-    /// Draw an image
-    pub unsafe fn image(&self, point: Point, data: *const u32, size: Size) {
-        let start_y = cmp::max(0, point.y) as usize;
-        let end_y = cmp::min(self.height as isize, point.y + size.height as isize) as usize;
-
-        let start_x = cmp::max(0, point.x) as usize;
-        let len = cmp::min(self.width as isize, point.x + size.width as isize) as usize * 4 -
-                  start_x * 4;
-        let offscreen_offset = self.offscreen + start_x * 4;
-
-        let bytesperrow = size.width * 4;
-        let data_offset = data as usize - start_y * bytesperrow -
-                          (point.x - start_x as isize) as usize * 4;
-
-        for y in start_y..end_y {
-            Display::copy_run(data_offset + y * bytesperrow,
-                              offscreen_offset + y * self.bytesperrow,
-                              len);
-        }
-    }
-    // } Optimized
-
-    /// Draw a image with opacity
-    pub unsafe fn image_alpha(&self, point: Point, data: *const u32, size: Size) {
-        let start_y = cmp::max(0, point.y) as usize;
-        let end_y = cmp::min(self.height as isize, point.y + size.height as isize) as usize;
-
-        let start_x = cmp::max(0, point.x) as usize;
-        let len = cmp::min(self.width as isize, point.x + size.width as isize) as usize * 4 -
-                  start_x * 4;
-        let offscreen_offset = self.offscreen + start_x * 4;
-
-        let bytesperrow = size.width * 4;
-        let data_offset = data as usize - start_y * bytesperrow -
-                          (point.x - start_x as isize) as usize * 4;
-
-        for y in start_y..end_y {
-            Display::copy_run_alpha(data_offset + y * bytesperrow,
-                                    offscreen_offset + y * self.bytesperrow,
-                                    len);
-        }
-    }
-
     // TODO: SIMD to optimize
     pub unsafe fn set_run_alpha(premul: u32, n_alpha: u32, dst: usize, len: usize) {
         let mut i = 0;
@@ -433,9 +287,9 @@ impl Display {
     /// Draw a char
     pub fn char(&self, point: Point, character: char, color: Color) {
         unsafe {
-            let bitmap_location = fonts + 16 * (character as usize);
+            let font_i = 16 * (character as usize);
             for row in 0..16 {
-                let row_data = *((bitmap_location + row) as *const u8);
+                let row_data = font[font_i + row];
                 for col in 0..8 {
                     let pixel = (row_data >> (7 - col)) & 1;
                     if pixel > 0 {
