123d94613103ce0122c3a6094d33d7981037a89c
stratact <stratact1@gmail.com>
2016-01-19 17:50:27 -0800

    Have `OptionSlice` use `RangeArguments` for range syntax instead

--- kernel/common/slice/option_slice.rs
+++ kernel/common/slice/option_slice.rs
@@ -1,11 +1,11 @@
-use core::slice;
-use core::str;
+use collections::range::RangeArgument;
 use core::ops::Range;
 use core::cmp;
 
-fn option_bound(len: usize, a: Option<usize>, b: Option<usize>) -> Range<usize> {
-    let start = cmp::min(a.unwrap_or(0), len);
-    let end = cmp::min(a.unwrap_or(len), len);
+fn bound<T: RangeArgument<Option<usize>>>(len: usize, a: T) -> Range<usize> {
+    // These unwraps at the end will NOT panic
+    let start = cmp::min(a.start().unwrap_or(&Some(0)).unwrap(), len);
+    let end = cmp::min(a.end().unwrap_or(&Some(len)).unwrap(), len);
 
     if start <= end {
         start..end
@@ -15,28 +15,28 @@ fn option_bound(len: usize, a: Option<usize>, b: Option<usize>) -> Range<usize> 
 }
 
 pub trait OptionSlice {
-    fn option_slice(&self, a: Option<usize>, b: Option<usize>) -> &Self;
-    fn option_slice_mut(&mut self, a: Option<usize>, b: Option<usize>) -> &mut Self;
+    fn option_slice<T: RangeArgument<Option<usize>>>(&self, a: T) -> &Self;
+    fn option_slice_mut<T: RangeArgument<Option<usize>>>(&mut self, a: T) -> &mut Self;
 }
 
 impl OptionSlice for str {
-    fn option_slice(&self, a: Option<usize>, b: Option<usize>) -> &Self {
-        &self[option_bound(self.len(), a, b)]
+    fn option_slice<T: RangeArgument<Option<usize>>>(&self, a: T) -> &Self {
+        &self[bound(self.len(), a)]
     }
 
-    fn option_slice_mut(&mut self, a: Option<usize>, b: Option<usize>) -> &mut Self {
+    fn option_slice_mut<T: RangeArgument<Option<usize>>>(&mut self, a: T) -> &mut Self {
         let len = self.len();
-        &mut self[option_bound(len, a, b)]
+        &mut self[bound(len, a)]
     }
 }
 
 impl<T> OptionSlice for [T] {
-    fn option_slice(&self, a: Option<usize>, b: Option<usize>) -> &Self {
-        &self[option_bound(self.len(), a, b)]
+    fn option_slice<U: RangeArgument<Option<usize>>>(&self, a: U) -> &Self {
+        &self[bound(self.len(), a)]
     }
 
-    fn option_slice_mut(&mut self, a: Option<usize>, b: Option<usize>) -> &mut Self {
+    fn option_slice_mut<U: RangeArgument<Option<usize>>>(&mut self, a: U) -> &mut Self {
         let len = self.len();
-        &mut self[option_bound(len, a, b)]
+        &mut self[bound(len, a)]
     }
 }
