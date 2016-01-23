751449837148d80177c43a6bc7075dcb7c8b08eb
Jeremy Soller <jackpot51@gmail.com>
2016-01-23 08:36:54 -0700

    WIP: Implement tests for syscalls in example scheme

--- filesystem/apps/example/main.rs
+++ filesystem/apps/example/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{Read, Write};
 
+use system::error::{Error, Result, ENOENT, EBADF};
 use system::scheme::{Packet, Scheme};
 
 extern crate system;
@@ -8,21 +9,70 @@ extern crate system;
 struct ExampleScheme;
 
 impl Scheme for ExampleScheme {
+    fn open(&mut self, path: &str, flags: usize, mode: usize) -> Result {
+        println!("open {:X} = {}, {:X}, {:X}", path.as_ptr() as usize, path, flags, mode);
+        Ok(0)
+    }
 
+    #[allow(unused_variables)]
+    fn unlink(&mut self, path: &str) -> Result {
+        println!("unlink {}", path);
+        Err(Error::new(ENOENT))
+    }
+
+    #[allow(unused_variables)]
+    fn mkdir(&mut self, path: &str, mode: usize) -> Result {
+        println!("mkdir {}, {:X}", path, mode);
+        Err(Error::new(ENOENT))
+    }
+
+    /* Resource operations */
+
+    #[allow(unused_variables)]
+    fn read(&mut self, id: usize, buf: &mut [u8]) -> Result {
+        println!("read {}, {:X}, {}", id, buf.as_mut_ptr() as usize, buf.len());
+        Err(Error::new(EBADF))
+    }
+
+    #[allow(unused_variables)]
+    fn write(&mut self, id: usize, buf: &[u8]) -> Result {
+        println!("write {}, {:X}, {}", id, buf.as_ptr() as usize, buf.len());
+        Err(Error::new(EBADF))
+    }
+
+    #[allow(unused_variables)]
+    fn seek(&mut self, id: usize, pos: usize, whence: usize) -> Result {
+        println!("seek {}, {}, {}", id, pos, whence);
+        Err(Error::new(EBADF))
+    }
+
+    #[allow(unused_variables)]
+    fn sync(&mut self, id: usize) -> Result {
+        println!("sync {}", id);
+        Err(Error::new(EBADF))
+    }
+
+    #[allow(unused_variables)]
+    fn truncate(&mut self, id: usize, len: usize) -> Result {
+        println!("truncate {}, {}", id, len);
+        Err(Error::new(EBADF))
+    }
 }
 
 fn main() {
    //In order to handle example:, we create :example
-   let mut scheme = File::create(":example").unwrap();
+   let mut scheme = ExampleScheme;
+   let mut socket = File::create(":example").unwrap();
    loop {
        let mut packet = Packet::default();
-       if scheme.read(&mut packet).unwrap() == 0 {
+       if socket.read(&mut packet).unwrap() == 0 {
            panic!("Unexpected EOF");
        }
+       println!("Recv {:?}", packet);
 
-       println!("Received: {:?}", packet);
+       scheme.handle(&mut packet);
 
-       packet.a = 0;
-       scheme.write(&packet).unwrap();
+       socket.write(&packet).unwrap();
+       println!("Sent {:?}", packet);
    }
 }
