bbd567304bf7ffde2ed443be670e97f3f910a8d9
Jeremy Soller <jackpot51@gmail.com>
2016-02-02 09:31:50 -0700

    Convert most things to using .events()

--- filesystem/apps/player/main.rs
+++ filesystem/apps/player/main.rs
@@ -21,7 +21,7 @@ fn main() {
         file.read_to_end(&mut vec);
     }
 
-    let mut window = Window::new(-1, -1, 320, 0, &("Player (".to_string() + &url + ")")).unwrap();
+    let mut window = Window::new(0, 0, 320, 32, &("Player (".to_string() + &url + ")")).unwrap();
     window.sync();
 
     let wav = WavFile::from_data(&vec);
@@ -32,14 +32,16 @@ fn main() {
         }
     }
 
-    while let Some(event) = window.poll() {
-        if let EventOption::Key(key_event) = event.to_option() {
-            if key_event.pressed && key_event.scancode == K_ESC {
-                break;
+    loop {
+        for event in window.events() {
+            if let EventOption::Key(key_event) = event.to_option() {
+                if key_event.pressed && key_event.scancode == K_ESC {
+                    return;
+                }
             }
-        }
-        if let EventOption::Quit(_) = event.to_option() {
-            break;
+            if let EventOption::Quit(_) = event.to_option() {
+                return;
+            }
         }
     }
 }
