14c5aea5ffaf727b5df723894e3a2181c11cdb28
Stefan Nitz <nitz.stefan@googlemail.com>
2016-02-04 20:46:41 +0100

    cleaned up x86_64 interrupt handling, still getting triple faults

--- kernel/asm/interrupts-x86_64.asm
+++ kernel/asm/interrupts-x86_64.asm
@@ -7,11 +7,12 @@ struc IDTEntry
 		.ring1	equ 1 << 5
 		.ring2 equ 1 << 6
 		.ring3 equ 1 << 5 | 1 << 6
-		.task32 equ 0x5
-		.interrupt16 equ 0x6
-		.trap16 equ 0x7
-		.interrupt32 equ 0xE
-		.trap32 equ 0xF
+		.ldt32 equ 0x2
+		.tssAvailabe64 equ 0x9
+		.tssBusy64 equ 0xB
+		.callGate64 equ 0xC
+		.interrupt64 equ 0xE
+		.trap64 equ 0xF
 	.offsetm resw 1
 	.offseth resd 1
 	.zero2 resd 1
@@ -55,7 +56,7 @@ interrupts:
 
 	mov rdi, qword [0x100000]
 	mov rsi, rsp
-	
+
 		;Stack Align
 		mov rbp, rsp
 		and rsp, 0xFFFFFFFFFFFFFFF0
@@ -102,9 +103,9 @@ idt:
 %rep 128
 	istruc IDTEntry
 		at IDTEntry.offsetl, dw interrupts+(interrupts.second-interrupts.first)*i
-		at IDTEntry.selector, dw 0x08
+		at IDTEntry.selector, dw gdt.kernel_code
 		at IDTEntry.zero1, db 0
-		at IDTEntry.attribute, db IDTEntry.present | IDTEntry.interrupt32
+		at IDTEntry.attribute, db IDTEntry.present | IDTEntry.interrupt64
 		at IDTEntry.offsetm, dw 0
 		at IDTEntry.offseth, dd 0
 		at IDTEntry.zero2, dd 0
@@ -115,9 +116,9 @@ idt:
 ;Syscall
 istruc IDTEntry
 	at IDTEntry.offsetl, dw interrupts+(interrupts.second-interrupts.first)*i
-	at IDTEntry.selector, dw 0x08
+	at IDTEntry.selector, dw gdt.kernel_code
 	at IDTEntry.zero1, db 0
-	at IDTEntry.attribute, db IDTEntry.ring3 | IDTEntry.present | IDTEntry.interrupt32
+	at IDTEntry.attribute, db IDTEntry.ring3 | IDTEntry.present | IDTEntry.interrupt64
 	at IDTEntry.offsetm, dw 0
 	at IDTEntry.offseth, dd 0
 	at IDTEntry.zero2, dd 0
@@ -128,9 +129,9 @@ iend
 %rep 127
 	istruc IDTEntry
 		at IDTEntry.offsetl, dw interrupts+(interrupts.second-interrupts.first)*i
-		at IDTEntry.selector, dw 0x08
+		at IDTEntry.selector, dw gdt.kernel_code
 		at IDTEntry.zero1, db 0
-		at IDTEntry.attribute, db IDTEntry.present | IDTEntry.interrupt32
+		at IDTEntry.attribute, db IDTEntry.present | IDTEntry.interrupt64
 		at IDTEntry.offsetm, dw 0
 		at IDTEntry.offseth, dd 0
 		at IDTEntry.zero2, dd 0
