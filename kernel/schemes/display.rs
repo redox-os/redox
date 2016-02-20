d918c6f6698ae5bd7dc1d511a7967a3d0db57bbc
Jeremy Soller <jackpot51@gmail.com>
2016-02-20 08:32:55 -0700

    Fix blocking issue with orbital

--- kernel/schemes/display.rs
+++ kernel/schemes/display.rs
@@ -57,7 +57,7 @@ impl Resource for DisplayResource {
             unsafe { ptr::write(buf.as_mut_ptr().offset(i as isize) as *mut Event, event) };
             i += size_of::<Event>();
 
-            while i <= buf.len() - size_of::<Event>() {
+            while i + size_of::<Event>() <= buf.len() {
                 if let Some(event) = ::env().events.inner.lock().pop_front() {
                     unsafe { ptr::write(buf.as_mut_ptr().offset(i as isize) as *mut Event, event) };
                     i += size_of::<Event>();
