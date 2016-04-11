bd8ce78eee29ae51c88a052141fa40943eaa97de
Wilhem Barbier <nounoursheureux@openmailbox.org>
2016-04-11 20:07:23 +0200

    Use EPERM in the default implementation of Resource methods

--- kernel/fs/resource.rs
+++ kernel/fs/resource.rs
@@ -1,6 +1,6 @@
 use alloc::boxed::Box;
 
-use system::error::{Error, Result, EBADF};
+use system::error::{Error, Result, EPERM, ESPIPE};
 use system::syscall::Stat;
 
 /// Resource seek
@@ -18,41 +18,50 @@ pub enum ResourceSeek {
 #[allow(unused_variables)]
 pub trait Resource {
     /// Duplicate the resource
+    /// Returns `EPERM` if the operation is not supported.
     fn dup(&self) -> Result<Box<Resource>> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 
     /// Return the path of this resource
+    /// Returns `EPERM` if the operation is not supported.
     fn path(&self, buf: &mut [u8]) -> Result<usize> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 
     /// Read data to buffer
+    /// Returns `EPERM` if the operation is not supported.
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 
     /// Write to resource
+    /// Returns `EPERM` if the operation is not supported.
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 
     /// Seek to the given offset
+    /// Returns `ESPIPE` if the operation is not supported.
     fn seek(&mut self, pos: ResourceSeek) -> Result<usize> {
-        Err(Error::new(EBADF))
+        Err(Error::new(ESPIPE))
     }
 
+    /// Get informations about the resource, such as mode and size
+    /// Returns `EPERM` if the operation is not supported.
     fn stat(&self, stat: &mut Stat) -> Result<usize> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 
     /// Sync all buffers
+    /// Returns `EPERM` if the operation is not supported.
     fn sync(&mut self) -> Result<()> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 
     /// Truncate to the given length
+    /// Returns `EPERM` if the operation is not supported.
     fn truncate(&mut self, len: usize) -> Result<()> {
-        Err(Error::new(EBADF))
+        Err(Error::new(EPERM))
     }
 }
