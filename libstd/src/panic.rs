90fb053eb58de0c25ff4d1d61a0637095b050933
Jeremy Soller <jackpot51@gmail.com>
2016-06-10 09:45:21 -0600

    Error checking in panic

--- libstd/src/panic.rs
+++ libstd/src/panic.rs
@@ -7,9 +7,11 @@ pub struct DebugStream;
 
 impl Write for DebugStream {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        let _ = sys_write(2, s.as_bytes());
-
-        result::Result::Ok(())
+        if let Err(_err) = sys_write(2, s.as_bytes()) {
+            result::Result::Err(fmt::Error)
+        } else {
+            result::Result::Ok(())
+        }
     }
 }
 
@@ -17,10 +19,7 @@ impl Write for DebugStream {
 #[allow(unused_must_use)]
 pub extern "C" fn panic_impl(args: &fmt::Arguments, file: &'static str, line: u32) -> ! {
     let mut stream = DebugStream;
-    stream.write_str(file);
-    stream.write_fmt(format_args!(":{}: ", line));
-    stream.write_fmt(*args);
-    stream.write_str("\n");
+    stream.write_fmt(format_args!("Panic in {}:{}: {}\n", file, line, *args));
 
     loop {
         let _ = sys_exit(128);
