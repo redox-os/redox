4c244135d5e7ccd0cd19902b507098719fb64426
Jeremy Soller <jackpot51@gmail.com>
2016-06-12 14:37:08 -0600

    Formatting

--- drivers/pcid/main.rs
+++ drivers/pcid/main.rs
@@ -8,20 +8,30 @@ use pci::{Pci, PciClass};
 
 mod pci;
 
+fn enumerate_pci() {
+    println!("PCI BS/DV/FN VEND:DEVI CL.SC.IN.RV");
+
+    let pci = Pci::new();
+    for bus in pci.buses() {
+        for dev in bus.devs() {
+            for func in dev.funcs() {
+                if let Some(header) = func.header() {
+                    println!("PCI {:>02X}/{:>02X}/{:>02X} {:>04X}:{:>04X} {:>02X}.{:>02X}.{:>02X}.{:>02X} {:?}",
+                            bus.num, dev.num, func.num,
+                            header.vendor_id, header.device_id,
+                            header.class, header.subclass, header.interface, header.revision,
+                            PciClass::from(header.class));
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     thread::spawn(|| {
         unsafe { sys_iopl(3).unwrap() };
 
-        let pci = Pci::new();
-        for bus in pci.buses() {
-            for dev in bus.devs() {
-                for func in dev.funcs() {
-                    if let Some(header) = func.header() {
-                        println!("PCI {:X} {:X} {:X}: {:?} {:#?}", bus.num, dev.num, func.num, PciClass::from(header.class), header);
-                    }
-                }
-            }
-        }
+        enumerate_pci();
 
         thread::sleep_ms(10000);
     });
