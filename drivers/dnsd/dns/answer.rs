351b23fd18347e6c61171cf0191984128145459a
Jeremy Soller <jackpot51@gmail.com>
2016-07-25 14:36:13 -0600

    WIP: dns query support

---
+++ drivers/dnsd/dns/answer.rs
@@ -0,0 +1,9 @@
+#[derive(Clone, Debug)]
+pub struct DnsAnswer {
+    pub name: String,
+    pub a_type: u16,
+    pub a_class: u16,
+    pub ttl_a: u16,
+    pub ttl_b: u16,
+    pub data: Vec<u8>
+}
