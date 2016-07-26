49910a54b2c6cd50f0a2d480de9e3f78a4476b59
Jeremy Soller <jackpot51@gmail.com>
2016-07-25 20:40:40 -0600

    Allow completion of DHCP transaction

--- drivers/dhcpd/main.rs
+++ drivers/dhcpd/main.rs
@@ -7,6 +7,9 @@ use dhcp::Dhcp;
 mod dhcp;
 
 fn main(){
+    let mut current_mac = [0; 6];
+    File::open("netcfg:mac").unwrap().read(&mut current_mac).unwrap();
+
     {
         let mut current_ip = [0; 4];
         File::open("netcfg:ip").unwrap().read(&mut current_ip).unwrap();
@@ -16,45 +19,79 @@ fn main(){
 
     let tid = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().subsec_nanos();
 
-    let packet = Dhcp {
-        op: 1,
-        htype: 1,
-        hlen: 6,
-        hops: 0,
-        tid: tid,
-        secs: 0,
-        flags: 0x8000u16.to_be(),
-        ciaddr: [0, 0, 0, 0],
-        yiaddr: [0, 0, 0, 0],
-        siaddr: [0, 0, 0, 0],
-        giaddr: [0, 0, 0, 0],
-        chaddr: [0x52, 0x54, 0x00, 0x12, 0x34, 0x56, 0x00, 0x00,
-                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-        sname: [0; 64],
-        file: [0; 128],
-        magic: 0x63825363u32.to_be(),
-        options: [53, 1, 1, 255]
-    };
-
-    let packet_data = unsafe { std::slice::from_raw_parts((&packet as *const Dhcp) as *const u8, std::mem::size_of::<Dhcp>()) };
-
     let mut socket = File::open("udp:255.255.255.255:67/68").unwrap();
-    let _sent = socket.write(packet_data).unwrap();
-
-    socket.flush().unwrap();
-
-    let mut buf = [0; 65536];
-    socket.read(&mut buf).unwrap();
-
-    let response = unsafe { &* (buf.as_ptr() as *const Dhcp) };
-
-    println!("DHCP: Suggested IP: {:?}, Server IP: {:?}", response.yiaddr, response.siaddr);
 
     {
-        File::open("netcfg:ip").unwrap().write(&response.yiaddr).unwrap();
+        let discover = Dhcp {
+            op: 1,
+            htype: 1,
+            hlen: 6,
+            hops: 0,
+            tid: tid,
+            secs: 0,
+            flags: 0x8000u16.to_be(),
+            ciaddr: [0, 0, 0, 0],
+            yiaddr: [0, 0, 0, 0],
+            siaddr: [0, 0, 0, 0],
+            giaddr: [0, 0, 0, 0],
+            chaddr: [current_mac[0], current_mac[1], current_mac[2], current_mac[3], current_mac[4], current_mac[5],
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            sname: [0; 64],
+            file: [0; 128],
+            magic: 0x63825363u32.to_be(),
+            options: [53, 1, 1, 255, 0, 0, 0, 0, 0, 0]
+        };
+
+        let discover_data = unsafe { std::slice::from_raw_parts((&discover as *const Dhcp) as *const u8, std::mem::size_of::<Dhcp>()) };
+
+        let _sent = socket.write(discover_data).unwrap();
+        socket.flush().unwrap();
+
+        println!("DHCP: Sent Discover");
     }
 
+    let mut offer_data = [0; 65536];
+    socket.read(&mut offer_data).unwrap();
+    let offer = unsafe { &* (offer_data.as_ptr() as *const Dhcp) };
+    println!("DHCP: Offer IP: {:?}, Server IP: {:?}", offer.yiaddr, offer.siaddr);
+
     {
+        let request = Dhcp {
+            op: 1,
+            htype: 1,
+            hlen: 6,
+            hops: 0,
+            tid: tid,
+            secs: 0,
+            flags: 0,
+            ciaddr: [0; 4],
+            yiaddr: [0; 4],
+            siaddr: offer.siaddr,
+            giaddr: [0; 4],
+            chaddr: [current_mac[0], current_mac[1], current_mac[2], current_mac[3], current_mac[4], current_mac[5],
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            sname: [0; 64],
+            file: [0; 128],
+            magic: 0x63825363u32.to_be(),
+            options: [53, 1, 3, 50, 4, offer.yiaddr[0], offer.yiaddr[1], offer.yiaddr[2], offer.yiaddr[3], 255]
+        };
+
+        let request_data = unsafe { std::slice::from_raw_parts((&request as *const Dhcp) as *const u8, std::mem::size_of::<Dhcp>()) };
+
+        let _sent = socket.write(request_data).unwrap();
+        socket.flush().unwrap();
+
+        println!("DHCP: Sent Request");
+    }
+
+    let mut ack_data = [0; 65536];
+    socket.read(&mut ack_data).unwrap();
+    let ack = unsafe { &* (ack_data.as_ptr() as *const Dhcp) };
+    println!("DHCP: Ack IP: {:?}, Server IP: {:?}", ack.yiaddr, ack.siaddr);
+
+    {
+        File::open("netcfg:ip").unwrap().write(&ack.yiaddr).unwrap();
+
         let mut new_ip = [0; 4];
         File::open("netcfg:ip").unwrap().read(&mut new_ip).unwrap();
 
