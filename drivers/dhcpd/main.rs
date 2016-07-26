0f27e9fc80a052168ba4f1f268ade4bcdf6b6948
Jeremy Soller <jackpot51@gmail.com>
2016-07-26 17:35:30 -0600

    Fix for virtualbox DHCP by setting address after offer Enable virtualbox networking

--- drivers/dhcpd/main.rs
+++ drivers/dhcpd/main.rs
@@ -49,7 +49,6 @@ fn main(){
         let discover_data = unsafe { std::slice::from_raw_parts((&discover as *const Dhcp) as *const u8, std::mem::size_of::<Dhcp>()) };
 
         let _sent = socket.write(discover_data).unwrap();
-        socket.flush().unwrap();
 
         println!("DHCP: Sent Discover");
     }
@@ -58,6 +57,88 @@ fn main(){
     socket.read(&mut offer_data).unwrap();
     let offer = unsafe { &* (offer_data.as_ptr() as *const Dhcp) };
     println!("DHCP: Offer IP: {:?}, Server IP: {:?}", offer.yiaddr, offer.siaddr);
+
+    {
+        let mut subnet_option = None;
+        let mut router_option = None;
+        let mut dns_option = None;
+
+        let mut options = offer.options.iter();
+        while let Some(option) = options.next() {
+            match *option {
+                0 => (),
+                255 => break,
+                _ => if let Some(len) = options.next() {
+                    if *len as usize <= options.as_slice().len() {
+                        let data = &options.as_slice()[.. *len as usize];
+                        for _data_i in 0..*len {
+                            options.next();
+                        }
+                        match *option {
+                            1 => {
+                                println!("DHCP: Subnet Mask: {:?}", data);
+                                if data.len() == 4 && subnet_option.is_none() {
+                                    subnet_option = Some(Vec::from(data));
+                                }
+                            },
+                            3 => {
+                                println!("DHCP: Router: {:?}", data);
+                                if data.len() == 4  && router_option.is_none() {
+                                    router_option = Some(Vec::from(data));
+                                }
+                            },
+                            6 => {
+                                println!("DHCP: Domain Name Server: {:?}", data);
+                                if data.len() == 4 && dns_option.is_none() {
+                                    dns_option = Some(Vec::from(data));
+                                }
+                            },
+                            51 => println!("DHCP: Lease Time: {:?}", data),
+                            53 => println!("DHCP: Message Type: {:?}", data),
+                            54 => println!("DHCP: Server ID: {:?}", data),
+                            _ => println!("DHCP: {}: {:?}", option, data)
+                        }
+                    }
+                },
+            }
+        }
+
+        {
+            File::open("netcfg:ip").unwrap().write(&offer.yiaddr).unwrap();
+
+            let mut new_ip = [0; 4];
+            File::open("netcfg:ip").unwrap().read(&mut new_ip).unwrap();
+
+            println!("DHCP: New IP: {:?}", new_ip);
+        }
+
+        if let Some(subnet) = subnet_option {
+            File::open("netcfg:ip_subnet").unwrap().write(&subnet).unwrap();
+
+            let mut new_subnet = [0; 4];
+            File::open("netcfg:ip_subnet").unwrap().read(&mut new_subnet).unwrap();
+
+            println!("DHCP: New Subnet: {:?}", new_subnet);
+        }
+
+        if let Some(router) = router_option {
+            File::open("netcfg:ip_router").unwrap().write(&router).unwrap();
+
+            let mut new_router = [0; 4];
+            File::open("netcfg:ip_router").unwrap().read(&mut new_router).unwrap();
+
+            println!("DHCP: New Router: {:?}", new_router);
+        }
+
+        if let Some(dns) = dns_option {
+            File::open("netcfg:dns").unwrap().write(&dns).unwrap();
+
+            let mut new_dns = [0; 4];
+            File::open("netcfg:dns").unwrap().read(&mut new_dns).unwrap();
+
+            println!("DHCP: New DNS: {:?}", new_dns);
+        }
+    }
 
     {
         let mut request = Dhcp {
@@ -87,93 +168,14 @@ fn main(){
         let request_data = unsafe { std::slice::from_raw_parts((&request as *const Dhcp) as *const u8, std::mem::size_of::<Dhcp>()) };
 
         let _sent = socket.write(request_data).unwrap();
-        socket.flush().unwrap();
 
         println!("DHCP: Sent Request");
     }
 
-    let mut ack_data = [0; 65536];
-    socket.read(&mut ack_data).unwrap();
-    let ack = unsafe { &* (ack_data.as_ptr() as *const Dhcp) };
-    println!("DHCP: Ack IP: {:?}, Server IP: {:?}", ack.yiaddr, ack.siaddr);
-
-    let mut subnet_option = None;
-    let mut router_option = None;
-    let mut dns_option = None;
-
-    let mut options = ack.options.iter();
-    while let Some(option) = options.next() {
-        match *option {
-            0 => (),
-            255 => break,
-            _ => if let Some(len) = options.next() {
-                if *len as usize <= options.as_slice().len() {
-                    let data = &options.as_slice()[.. *len as usize];
-                    for _data_i in 0..*len {
-                        options.next();
-                    }
-                    match *option {
-                        1 => {
-                            println!("DHCP: Subnet Mask: {:?}", data);
-                            if data.len() == 4 && subnet_option.is_none() {
-                                subnet_option = Some(Vec::from(data));
-                            }
-                        },
-                        3 => {
-                            println!("DHCP: Router: {:?}", data);
-                            if data.len() == 4  && router_option.is_none() {
-                                router_option = Some(Vec::from(data));
-                            }
-                        },
-                        6 => {
-                            println!("DHCP: Domain Name Server: {:?}", data);
-                            if data.len() == 4 && dns_option.is_none() {
-                                dns_option = Some(Vec::from(data));
-                            }
-                        },
-                        51 => println!("DHCP: Lease Time: {:?}", data),
-                        53 => println!("DHCP: Message Type: {:?}", data),
-                        54 => println!("DHCP: Server ID: {:?}", data),
-                        _ => println!("DHCP: {}: {:?}", option, data)
-                    }
-                }
-            },
-        }
-    }
-
     {
-        File::open("netcfg:ip").unwrap().write(&ack.yiaddr).unwrap();
-
-        let mut new_ip = [0; 4];
-        File::open("netcfg:ip").unwrap().read(&mut new_ip).unwrap();
-
-        println!("DHCP: New IP: {:?}", new_ip);
-    }
-
-    if let Some(subnet) = subnet_option {
-        File::open("netcfg:ip_subnet").unwrap().write(&subnet).unwrap();
-
-        let mut new_subnet = [0; 4];
-        File::open("netcfg:ip_subnet").unwrap().read(&mut new_subnet).unwrap();
-
-        println!("DHCP: New Subnet: {:?}", new_subnet);
-    }
-
-    if let Some(router) = router_option {
-        File::open("netcfg:ip_router").unwrap().write(&router).unwrap();
-
-        let mut new_router = [0; 4];
-        File::open("netcfg:ip_router").unwrap().read(&mut new_router).unwrap();
-
-        println!("DHCP: New Router: {:?}", new_router);
-    }
-
-    if let Some(dns) = dns_option {
-        File::open("netcfg:dns").unwrap().write(&dns).unwrap();
-
-        let mut new_dns = [0; 4];
-        File::open("netcfg:dns").unwrap().read(&mut new_dns).unwrap();
-
-        println!("DHCP: New DNS: {:?}", new_dns);
+        let mut ack_data = [0; 65536];
+        socket.read(&mut ack_data).unwrap();
+        let ack = unsafe { &* (ack_data.as_ptr() as *const Dhcp) };
+        println!("DHCP: Ack IP: {:?}, Server IP: {:?}", ack.yiaddr, ack.siaddr);
     }
 }
