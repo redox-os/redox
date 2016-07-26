1566a3efc315dff6e0ea7cbd987786df8855475d
Jeremy Soller <jackpot51@gmail.com>
2016-07-25 21:15:49 -0600

    Add timeout capability to locks

--- kernel/sync/wait_condition.rs
+++ kernel/sync/wait_condition.rs
@@ -6,6 +6,8 @@ use core::cell::UnsafeCell;
 use core::mem;
 use core::ops::DerefMut;
 
+use common::time::Duration;
+
 pub struct WaitCondition {
     contexts: UnsafeCell<Vec<*mut Context>>
 }
@@ -45,6 +47,32 @@ impl WaitCondition {
             }
         }
         unsafe { context_switch(); }
+    }
+
+    pub fn wait_for(&self, reason: &str, time: Duration) -> bool {
+        let mut ret = true;
+        {
+            // debugln!("  WaitCondition::wait_until {:X} {}", self as *const _ as usize, reason);
+            if let Ok(mut context) = unsafe { &mut *::env().contexts.get() }.current_mut() {
+                let mut contexts = unsafe { &mut *self.contexts.get() };
+                contexts.push(context.deref_mut() as *mut Context);
+                (*context).wake = Some(Duration::monotonic() + time);
+                (*context).block(reason);
+            } else {
+                // debugln!("    NOT FOUND {}/{}", unsafe { & *::env().contexts.get() }.i, unsafe { & *::env().contexts.get() }.len());
+            }
+        }
+        unsafe { context_switch(); }
+        {
+            if let Ok(mut context) = unsafe { &mut *::env().contexts.get() }.current_mut() {
+                if (*context).wake.is_none() {
+                    ret = false;
+                } else {
+                    (*context).wake = None;
+                }
+            }
+        }
+        ret
     }
 }
 
