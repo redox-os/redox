d8e67d6c0610ef83797a8e22221aaca4068ebbbc
Jeremy Soller <jackpot51@gmail.com>
2016-07-26 18:34:03 -0600

    Add netutils repo

--- drivers/dnsd/dns/answer.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Clone, Debug)]
-pub struct DnsAnswer {
-    pub name: String,
-    pub a_type: u16,
-    pub a_class: u16,
-    pub ttl_a: u16,
-    pub ttl_b: u16,
-    pub data: Vec<u8>
-}
--- drivers/dnsd/dns/query.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Clone, Debug)]
-pub struct DnsQuery {
-    pub name: String,
-    pub q_type: u16,
-    pub q_class: u16
-}
