2e840a2ef319ea857e4079d46ffafa9ceacf56a8
Jeremy Soller <jackpot51@gmail.com>
2016-08-10 10:29:20 -0600

    Update libstd to use futex

--- libstd/src/sync/mod.rs
+++ libstd/src/sync/mod.rs
@@ -1,11 +1,10 @@
 pub use alloc::arc::{Arc, Weak};
 pub use core::sync::atomic;
-pub use self::mutex::{Mutex, MutexGuard, StaticMutex};
+pub use self::mutex::{Condvar, Mutex, MutexGuard, StaticMutex};
 pub use self::rwlock::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 pub use self::once::Once;
 
 pub mod mpsc;
-mod futex;
 mod mutex;
 mod once;
 mod rwlock;
--- libstd/src/sync/mutex.rs
+++ libstd/src/sync/mutex.rs
@@ -1,12 +1,116 @@
-use core::cell::UnsafeCell;
-use core::ops::{Deref, DerefMut, Drop};
-use core::sync::atomic::{AtomicBool, Ordering};
+use cell::UnsafeCell;
+use intrinsics::{atomic_cxchg, atomic_xadd, atomic_xchg};
+use ops::{Deref, DerefMut, Drop};
+use ptr;
 
-use thread;
+use system::syscall::{sys_futex, FUTEX_WAIT, FUTEX_WAKE, FUTEX_REQUEUE};
+
+unsafe fn mutex_lock(m: *mut i32) {
+    let mut c = 0;
+    //Set to larger value for longer spin test
+    for _i in 0..1 {
+        c = atomic_cxchg(m, 0, 1).0;
+        if c == 0 {
+            break;
+        }
+        //cpu_relax()
+    }
+    if c == 1 {
+        c = atomic_xchg(m, 2);
+    }
+    while c != 0 {
+        let _ = sys_futex(m, FUTEX_WAIT, 2, 0, ptr::null_mut());
+        c = atomic_xchg(m, 2);
+    }
+}
+
+unsafe fn mutex_unlock(m: *mut i32) {
+    if *m == 2 {
+        *m = 0;
+    } else if atomic_xchg(m, 0) == 1 {
+        return;
+    }
+    //Set to larger value for longer spin test
+    for _i in 0..1 {
+        if *m != 0 {
+            if atomic_cxchg(m, 1, 2).0 != 0 {
+                return;
+            }
+        }
+        //cpu_relax()
+    }
+    let _ = sys_futex(m, FUTEX_WAKE, 1, 0, ptr::null_mut());
+}
+
+pub struct Condvar {
+    lock: UnsafeCell<*mut i32>,
+    seq: UnsafeCell<i32>
+}
+
+impl Condvar {
+    pub fn new() -> Condvar {
+        Condvar {
+            lock: UnsafeCell::new(ptr::null_mut()),
+            seq: UnsafeCell::new(0)
+        }
+    }
+
+    pub fn notify_one(&self) {
+        unsafe {
+            let seq = self.seq.get();
+
+            atomic_xadd(seq, 1);
+
+            let _ = sys_futex(seq, FUTEX_WAKE, 1, 0, ptr::null_mut());
+        }
+    }
+
+    pub fn notify_all(&self) {
+        unsafe {
+            let lock = self.lock.get();
+            let seq = self.seq.get();
+
+            if *lock == ptr::null_mut() {
+                return;
+            }
+
+            atomic_xadd(seq, 1);
+
+            let _ = sys_futex(seq, FUTEX_REQUEUE, 1, ::usize::MAX, *lock);
+        }
+    }
+
+    pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> Result<MutexGuard<'a, T>, ()> {
+        unsafe {
+            let lock = self.lock.get();
+            let seq = self.seq.get();
+
+            if *lock != guard.lock.get() {
+                if *lock != ptr::null_mut() {
+                    panic!("Condvar used with more than one Mutex");
+                }
+
+                atomic_cxchg(lock as *mut usize, 0, guard.lock.get() as usize);
+            }
+
+            mutex_unlock(*lock);
+
+            let _ = sys_futex(seq, FUTEX_WAIT, *seq, 0, ptr::null_mut());
+
+            while atomic_xchg(*lock, 2) != 0 {
+                let _ = sys_futex(*lock, FUTEX_WAIT, 2, 0, ptr::null_mut());
+            }
+
+            mutex_lock(*lock);
+        }
+
+        Ok(guard)
+    }
+}
 
 /// A mutex, i.e. a form of safe shared memory between threads. See rust std's Mutex.
 pub struct Mutex<T: ?Sized> {
-    lock: AtomicBool,
+    lock: UnsafeCell<i32>,
     value: UnsafeCell<T>,
 }
 
@@ -14,7 +118,7 @@ impl<T> Mutex<T> {
     /// Create a new mutex with value `value`.
     pub fn new(value: T) -> Self {
         Mutex {
-            lock: AtomicBool::new(false),
+            lock: UnsafeCell::new(0),
             value: UnsafeCell::new(value),
         }
     }
@@ -23,9 +127,7 @@ impl<T> Mutex<T> {
 impl<T: ?Sized> Mutex<T> {
     /// Lock the mutex
     pub fn lock(&self) -> Result<MutexGuard<T>, ()> {
-        while self.lock.compare_and_swap(false, true, Ordering::SeqCst) {
-            thread::yield_now();
-        }
+        unsafe { mutex_lock(self.lock.get()) };
         Ok(MutexGuard::new(&self.lock, &self.value))
     }
 }
@@ -35,29 +137,24 @@ unsafe impl Sync for Dummy {}
 static DUMMY: Dummy = Dummy(UnsafeCell::new(()));
 
 pub struct StaticMutex {
-    lock: AtomicBool,
+    lock: UnsafeCell<i32>,
 }
 
 impl StaticMutex {
     /// Create a new mutex with value `value`.
     pub const fn new() -> Self {
         StaticMutex {
-            lock: AtomicBool::new(false),
+            lock: UnsafeCell::new(0),
         }
     }
 
     /// Lock the mutex
     pub fn lock(&'static self) -> Result<MutexGuard<()>, ()> {
-        while self.lock.compare_and_swap(false, true, Ordering::SeqCst) {
-            thread::yield_now();
-        }
+        unsafe { mutex_lock(self.lock.get()) };
         Ok(MutexGuard::new(&self.lock, &DUMMY.0)) // TODO catch panics
     }
 
     pub unsafe fn destroy(&'static self) {
-        if !self.lock.compare_and_swap(true, false, Ordering::SeqCst) {
-            // Mutex was already unlocked!
-        }
     }
 }
 
@@ -67,12 +164,12 @@ unsafe impl<T: ?Sized + Send> Sync for Mutex<T> {}
 
 /// A mutex guard (returned by .lock())
 pub struct MutexGuard<'a, T: ?Sized + 'a> {
-    lock: &'a AtomicBool,
+    lock: &'a UnsafeCell<i32>,
     data: &'a UnsafeCell<T>,
 }
 
 impl<'mutex, T: ?Sized> MutexGuard<'mutex, T> {
-    fn new(lock: &'mutex AtomicBool, data: &'mutex UnsafeCell<T>) -> Self {
+    fn new(lock: &'mutex UnsafeCell<i32>, data: &'mutex UnsafeCell<T>) -> Self {
         MutexGuard {
             lock: lock,
             data: data,
@@ -96,8 +193,6 @@ impl<'mutex, T: ?Sized> DerefMut for MutexGuard<'mutex, T> {
 
 impl<'a, T: ?Sized> Drop for MutexGuard<'a, T> {
     fn drop(&mut self) {
-        if !self.lock.compare_and_swap(true, false, Ordering::SeqCst) {
-            // Mutex was already unlocked!
-        }
+        unsafe { mutex_unlock(self.lock.get()) };
     }
 }
