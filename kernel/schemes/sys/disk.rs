29a3847d24e23f309b55843a9e3625ba9a4323c7
Jeremy Soller <jackpot51@gmail.com>
2016-08-11 11:38:09 -0600

    Disk size

--- kernel/schemes/sys/disk.rs
+++ kernel/schemes/sys/disk.rs
@@ -8,7 +8,7 @@ use system::error::Result;
 use system::syscall::MODE_FILE;
 
 pub fn resource() -> Result<Box<Resource>> {
-    let mut string = format!("{:<6}{:<10}{}", "PATH", "SIZE (MB)", "NAME");
+    let mut string = format!("{:<6}{:<10}{}\n", "PATH", "SIZE (MB)", "NAME");
 
     for (i, disk) in unsafe { &mut *::env().disks.get() }.iter().enumerate() {
         string.push_str(&format!("{:<6}{:<10}{}\n", i, unsafe { & *disk.get() }.size()/1024/1024, unsafe { & *disk.get() }.name()));
