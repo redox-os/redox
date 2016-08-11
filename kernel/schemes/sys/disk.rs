cf62e8f1a4af152ccb079f68cb3afb0a8baf33a8
Jeremy Soller <jackpot51@gmail.com>
2016-08-11 11:37:34 -0600

    Disk size

--- kernel/schemes/sys/disk.rs
+++ kernel/schemes/sys/disk.rs
@@ -1,6 +1,6 @@
 use alloc::boxed::Box;
 
-use collections::string::{String, ToString};
+use collections::string::ToString;
 
 use fs::{Resource, VecResource};
 
@@ -8,10 +8,10 @@ use system::error::Result;
 use system::syscall::MODE_FILE;
 
 pub fn resource() -> Result<Box<Resource>> {
-    let mut string = String::new();
+    let mut string = format!("{:<6}{:<10}{}", "PATH", "SIZE (MB)", "NAME");
 
     for (i, disk) in unsafe { &mut *::env().disks.get() }.iter().enumerate() {
-        string.push_str(&format!("{:<6}{}\n", i, unsafe { & *disk.get() }.name()));
+        string.push_str(&format!("{:<6}{:<10}{}\n", i, unsafe { & *disk.get() }.size()/1024/1024, unsafe { & *disk.get() }.name()));
     }
 
     Ok(box VecResource::new("sys:/disk".to_string(), string.into_bytes(), MODE_FILE))
