01926b30cad52ff99a743ea9d156995115f1ab64
Thomas Levy <tychomonger@gmail.com>
2016-08-13 10:05:15 -0400

    Use weak linkage for libc symbols (#693)

--- libstd/src/externs.rs
+++ libstd/src/externs.rs
@@ -6,6 +6,7 @@ pub static mut __errno: isize = 0;
 ///
 /// Copy N bytes of memory from one location to another.
 #[no_mangle]
+#[linkage = "weak"]
 pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
                             n: usize) -> *mut u8 {
     let mut i = 0;
@@ -21,6 +22,7 @@ pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
 ///
 /// Copy N bytes of memory from src to dest. The memory areas may overlap.
 #[no_mangle]
+#[linkage = "weak"]
 pub unsafe extern fn memmove(dest: *mut u8, src: *const u8,
                              n: usize) -> *mut u8 {
     if src < dest as *const u8 {
@@ -44,6 +46,7 @@ pub unsafe extern fn memmove(dest: *mut u8, src: *const u8,
 ///
 /// Fill a block of memory with a specified value.
 #[no_mangle]
+#[linkage = "weak"]
 pub unsafe extern fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
     let mut i = 0;
     while i < n {
@@ -58,6 +61,7 @@ pub unsafe extern fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
 ///
 /// Compare two blocks of memory.
 #[no_mangle]
+#[linkage = "weak"]
 pub unsafe extern fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
     let mut i = 0;
 
