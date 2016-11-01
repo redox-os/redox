047281e30327313eba6e19ed4c4bc3dd5d4483a0
Jeremy Soller <jackpot51@gmail.com>
2016-11-01 16:43:15 -0600

    Install pixelcannon manifest

--- recipes/pixelcannon/recipe.sh
+++ recipes/pixelcannon/recipe.sh
@@ -3,4 +3,6 @@ GIT=https://github.com/jackpot51/pixelcannon.git
 function recipe_stage {
     mkdir -pv "$1/apps/pixelcannon"
     cp -Rv assets "$1/apps/pixelcannon"
+    mkdir -pv "$1/ui/apps"
+    cp -v manifest "$1/ui/apps/pixelcannon"
 }
