13b3eb2869197d906daf8d4a6911feb339910847
Jeremy Soller <jackpot51@gmail.com>
2016-12-15 13:40:26 -0700

    Update rust

--- libstd/libc/src/lib.rs
+++ libstd/libc/src/lib.rs
@@ -2,7 +2,6 @@
 #![allow(non_camel_case_types)]
 #![feature(asm)]
 #![feature(const_fn)]
-#![feature(naked_functions)]
 #![feature(thread_local)]
 
 #![cfg_attr(stdbuild, feature(no_std, core, core_slice_ext, staged_api, custom_attribute, cfg_target_vendor))]
@@ -15,14 +14,11 @@
 
 pub use types::*;
 pub use funcs::*;
-pub use start::*;
 pub use syscall::*;
 
 /// Basic types (not usually system specific)
 mod types;
 /// Basic functions (not system specific)
 mod funcs;
-/// Start function and call in to libstd
-mod start;
 /// Conversion for syscall library (specific to Redox)
 mod syscall;
