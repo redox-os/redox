2d25d5848243d9fce29fd1f3862c61eec185f394
Jeremy Soller <jackpot51@gmail.com>
2017-02-07 19:48:50 -0700

    Fix kernel output

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -4,10 +4,10 @@ $(KBUILD)/libcollections.rlib: rust/src/libcollections/Cargo.toml rust/src/libco
 	cp rust/src/target/$(KTARGET)/release/deps/*.rlib $(KBUILD)
 
 $(KBUILD)/libkernel.a: kernel/Cargo.toml kernel/arch/** kernel/src/** $(KBUILD)/libcollections.rlib $(BUILD)/initfs.rs
-	$(KCARGO) rustc --manifest-path $< --lib $(KCARGOFLAGS) -C lto --emit obj=$@
+	$(KCARGO) rustc --manifest-path $< --lib $(KCARGOFLAGS) -C lto -o $@
 
 $(KBUILD)/libkernel_live.a: kernel/Cargo.toml kernel/arch/** kernel/src/** $(KBUILD)/libcollections.rlib $(BUILD)/initfs.rs build/filesystem.bin
-	$(KCARGO) rustc --manifest-path $< --lib --features live $(KCARGOFLAGS) -C lto --emit obj=$@
+	$(KCARGO) rustc --manifest-path $< --lib --features live $(KCARGOFLAGS) -C lto -o $@
 
 $(KBUILD)/kernel: $(KBUILD)/libkernel.a
 	$(LD) $(LDFLAGS) -z max-page-size=0x1000 -T kernel/arch/$(ARCH)/src/linker.ld -o $@ $<
