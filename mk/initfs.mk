bd131b838cae82cdcc76f7cf31cf1112041efa61
Jeremy Soller <jackpot51@gmail.com>
2017-03-19 16:46:15 -0600

    Fix detection of initfs changes

--- mk/initfs.mk
+++ mk/initfs.mk
@@ -1,13 +1,15 @@
-$(BUILD)/initfs.rs: \
-		initfs/bin/init \
-		initfs/bin/ahcid \
-		initfs/bin/bgad \
-		initfs/bin/nvmed \
-		initfs/bin/pcid \
-		initfs/bin/ps2d \
-		initfs/bin/redoxfs \
-		initfs/bin/vesad \
-		initfs/etc/**
+$(KBUILD)/initfs.tag: initfs/bin/init \
+	initfs/bin/ahcid \
+	initfs/bin/bgad \
+	initfs/bin/nvmed \
+	initfs/bin/pcid \
+	initfs/bin/ps2d \
+	initfs/bin/redoxfs \
+	initfs/bin/vboxd \
+	initfs/bin/vesad \
+	initfs/etc/**
+		$(KCARGO) clean --manifest-path kernel/Cargo.toml
+		touch $@
 
 initfs/bin/%: programs/%/Cargo.toml programs/%/src/** $(BUILD)/libstd.rlib
 	mkdir -p initfs/bin
