7d426b552c98aa8e3281570b8271ba39b1219b07
Jeremy Soller <jackpot51@gmail.com>
2017-03-27 20:31:50 -0600

    Fixes for setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
 set -e
+
+echo "Downloading latest libc-artifacts"
+git submodule update --init --remote libc-artifacts
 
 echo "Defaulting to rust nightly"
 rustup override set nightly
+echo "Update rust nightly"
+rustup update nightly
 echo "Downloading rust source"
 rustup component add rust-src
 if [ -z "$(which xargo)" ]
