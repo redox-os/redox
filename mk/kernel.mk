0e1b9c631550cbf48ba3fa6b421a0dfcdde2331f
Jeremy Soller <jackpot51@gmail.com>
2017-05-11 21:19:31 -0600

    Update recipes

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -1,8 +1,8 @@
-build/libkernel.a: kernel/Cargo.toml kernel/src/** build/initfs.tag
+build/libkernel.a: kernel/Cargo.toml kernel/src/* kernel/src/*/* kernel/src/*/*/* build/initfs.tag
 	cd kernel && xargo rustc --lib --target $(KTARGET) --release -- -C soft-float --emit link=../$@
 
-build/libkernel_live.a: kernel/Cargo.toml kernel/src/** build/initfs.tag build/filesystem.bin
-	cd kernel && xargo rustc --lib --features live --target $(KTARGET) --release -- -C soft-float --emit link=../$@
+build/libkernel_live.a: kernel/Cargo.toml kernel/src/* kernel/src/*/* kernel/src/*/*/* build/initfs.tag build/filesystem.bin
+	cd kernel && FILESYSTEM="$(PWD)/build/filesystem.bin" xargo rustc --lib --features live --target $(KTARGET) --release -- -C soft-float --emit link=../$@
 
 build/kernel: build/libkernel.a
 	$(LD) --gc-sections -z max-page-size=0x1000 -T kernel/linkers/$(ARCH).ld -o $@ $<
