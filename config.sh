3e18b7108a5004c81c14d8ccc3124227d3eea06e
Jeremy Soller <jackpot51@gmail.com>
2017-06-04 20:09:42 -0600

    Use cross compiler instead of libc-artifacts

--- config.sh
+++ config.sh
@@ -7,4 +7,4 @@ export TARGET=x86_64-unknown-redox
 # Automatic variables
 ROOT="$(cd `dirname "$0"` && pwd)"
 REPO="$ROOT/repo/$TARGET"
-export CC="$ROOT/libc-artifacts/gcc.sh"
+export CC="x86_64-elf-redox-gcc"
