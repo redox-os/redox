6528b4bc2166b98b8e05dfe71023d58a9b721189
Ian Douglas Scott <ian@iandouglasscott.com>
2017-06-11 21:07:19 -0700

    Strip binaries in gcc and gnu-binutils, for much smaller file size

--- recipes/gcc/recipe.sh
+++ recipes/gcc/recipe.sh
@@ -52,5 +52,6 @@ function recipe_clean {
 function recipe_stage {
     dest="$(realpath $1)"
     make DESTDIR="$dest" install-gcc install-target-libgcc
+    find "$dest" -exec x86_64-elf-redox-strip {} ';' 2> /dev/null
     skip=1
 }
