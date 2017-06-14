2158be3aee93b6628bc2337fbdfa5c70bf75bf32
Jeremy Soller <jackpot51@gmail.com>
2017-06-13 21:43:57 -0600

    Update kernel and bootloader

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -4,8 +4,8 @@ build/libkernel.a: kernel/Cargo.toml kernel/src/* kernel/src/*/* kernel/src/*/*/
 build/libkernel_live.a: kernel/Cargo.toml kernel/src/* kernel/src/*/* kernel/src/*/*/* build/initfs.tag build/filesystem.bin
 	cd kernel && FILESYSTEM="$(PWD)/build/filesystem.bin" xargo rustc --lib --features live --target $(KTARGET) --release -- -C soft-float --emit link=../$@
 
-build/kernel: build/libkernel.a
+build/kernel: build/libkernel.a kernel/linkers/$(ARCH).ld
 	$(LD) --gc-sections -z max-page-size=0x1000 -T kernel/linkers/$(ARCH).ld -o $@ $<
 
-build/kernel_live: build/libkernel_live.a
+build/kernel_live: build/libkernel_live.a kernel/linkers/$(ARCH).ld
 	$(LD) --gc-sections -z max-page-size=0x1000 -T kernel/linkers/$(ARCH).ld -o $@ $<
