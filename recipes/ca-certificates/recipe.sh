c9c2b864fa265b44aa662716c12193da7c957754
Ian Douglas Scott <ian@iandouglasscott.com>
2017-06-23 12:32:02 -0700

    ca-certificates package; needed by cargo

---
+++ recipes/ca-certificates/recipe.sh
@@ -0,0 +1,39 @@
+function recipe_fetch {
+    mkdir source
+    pushd source
+        curl -o make-ca.sh http://anduin.linuxfromscratch.org/BLFS/other/make-ca.sh-20170514
+        curl -o certdata.txt http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt
+    popd
+    skip=1
+}
+
+function recipe_update {
+    curl -o make-ca.sh --time-cond make-ca.sh http://anduin.linuxfromscratch.org/BLFS/other/make-ca.sh-20170514
+    curl -o certdata.txt --time-cond certdata.txt http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt
+    skip=1
+}
+
+function recipe_build {
+    rm -rf build
+    mkdir build
+    chmod +x ./make-ca.sh
+    ./make-ca.sh -D "$PWD/build"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    rm -rf build
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    mkdir -p "$1/ssl"
+    cp -rL build/etc/ssl/certs "$1/ssl"
+    skip=1
+}
