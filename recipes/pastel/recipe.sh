b658dcb96311c270f4bf2cf6ed806dc364fb8522
Jeremy Soller <jackpot51@gmail.com>
2017-07-19 19:32:28 -0600

    Add manifest for Pastel, place in /ui/bin

--- recipes/pastel/recipe.sh
+++ recipes/pastel/recipe.sh
@@ -1,7 +1,15 @@
 GIT=https://github.com/robbycerantola/pastel.git
-
+BINDIR=/ui/bin
 
 function recipe_stage {
-    mkdir "$1/ui"
-    cp -rv res "$1/ui/pastel"
+	mkdir "$1/ui"
+	cp -rv res "$1/ui/pastel"
+	mkdir "$1/ui/apps"
+	cat > "$1/ui/apps/pastel" <<-EOF
+	name=Pastel
+	binary=/ui/bin/pastel
+	icon=/ui/pastel/accessories-bitmap-editor.png
+	author=Robby Cerantola
+	description=Bitmap Editor
+	EOF
 }
