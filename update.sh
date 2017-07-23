53c7ce83ceba4bd888a761e631f01abdade807d3
Jeremy Soller <jackpot51@gmail.com>
2017-07-23 11:58:04 -0600

    Add update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+source config.sh
+
+if [ $# = 0 ]
+then
+    recipes="$(ls -1 recipes)"
+else
+    recipes="$@"
+fi
+
+for recipe in $recipes
+do
+    if [ -d "recipes/$recipe/build" ]
+    then
+        ./cook.sh "$recipe" update
+    fi
+done
