aeff91c18deb708d651a3c955127caae867bd93e
Ian Douglas Scott <ian@iandouglasscott.com>
2017-08-08 13:30:42 -0700

    Add recipe for xz

---
+++ recipes/xz/recipe.sh
@@ -0,0 +1,36 @@
+VERSION=5.2.3
+TAR=https://tukaani.org/xz/xz-$VERSION.tar.gz
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    wget -O build-aux/config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+    ./configure --host=${HOST} --prefix=/ --enable-threads=no
+    make
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" install
+    rm -rf "$dest/share"
+    skip=1
+}
