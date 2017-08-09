f3e4c37ebf7cca0a6071fcbe8ac14829d3f59a53
Ian Douglas Scott <ian@iandouglasscott.com>
2017-08-08 17:47:48 -0700

    Make extrautils have xz as a build depend

--- recipes/extrautils/recipe.sh
+++ recipes/extrautils/recipe.sh
@@ -1,1 +1,6 @@
 GIT=https://github.com/redox-os/extrautils.git
+BUILD_DEPENDS=xz
+export PKG_CONFIG_ALLOW_CROSS=1
+export PKG_CONFIG_PATH=
+export PKG_CONFIG_LIBDIR="$PWD/sysroot/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$PWD/sysroot"
