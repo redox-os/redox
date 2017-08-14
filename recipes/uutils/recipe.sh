12082e265e11f3fd34e8095962e7daf74d270d10
Ian Douglas Scott <ian@iandouglasscott.com>
2017-08-14 11:21:57 -0700

    Add symlinks to uutils package

--- recipes/uutils/recipe.sh
+++ recipes/uutils/recipe.sh
@@ -1,2 +1,12 @@
 GIT=https://github.com/redox-os/uutils.git
 CARGOFLAGS="--no-default-features --features redox"
+
+function recipe_stage {
+    mkdir -p "$1/bin"
+    ln -s uutils "$1/bin/chmod"
+    ln -s uutils "$1/bin/env"
+    ln -s uutils "$1/bin/expr"
+    ln -s uutils "$1/bin/install"
+    ln -s uutils "$1/bin/ls"
+    ln -s uutils "$1/bin/mktemp"
+}
