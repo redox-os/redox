348dd4a8fe8bc92614a60a77d31b416029993d89
Jeremy Soller <jackpot51@gmail.com>
2017-10-11 20:19:43 -0600

    Update status script

--- status.sh
+++ status.sh
@@ -12,15 +12,13 @@ fi
 
 for recipe in $recipes
 do
-    echo -e "\e[1m$recipe\e[0m"
-    if [ -d "recipes/$recipe/source/.git" ]
+    if [ -d "recipes/$recipe/source" ]
     then
-        git -C "recipes/$recipe/source" status
-    elif [ -e "recipes/$recipe/source.tar" ]
-    then
-        echo "Using source tarball"
-        tar --compare --file="recipes/$recipe/source.tar" -C "recipes/$recipe/source" --strip-components=1 2>&1| grep -v "tar: :" | grep -v '\(Mode\|Gid\|Uid\) differs' || true
-    else
-        echo "No original source found"
+        status="$(COOK_QUIET=1 ./cook.sh "$recipe" status)"
+
+        if [ -n "$status" ]
+        then
+            echo -e "\e[1m$recipe\e[0m\n$status"
+        fi
     fi
 done
