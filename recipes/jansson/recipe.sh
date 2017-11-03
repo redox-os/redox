e62a6b8922e6decc1adb0c4531ed3ef37753685c
Paul Sajna <paulsajna@gmail.com>
2017-11-02 19:26:24 -0700

    recipe for jansson

---
+++ recipes/jansson/recipe.sh
@@ -0,0 +1,49 @@
+VERSION=2.10
+TAR=http://www.digip.org/jansson/releases/jansson-$VERSION.tar.gz
+
+export AR="${HOST}-ar"
+export AS="${HOST}-as"
+export CC="${HOST}-gcc"
+export CXX="${HOST}-g++"
+export LD="${HOST}-ld"
+export NM="${HOST}-nm"
+export OBJCOPY="${HOST}-objcopy"
+export OBJDUMP="${HOST}-objdump"
+export RANLIB="${HOST}-ranlib"
+export READELF="${HOST}-readelf"
+export STRIP="${HOST}-strip"
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="${PWD}/../sysroot"
+    export LDFLAGS="-L$sysroot/lib"
+    export CPPFLAGS="-I$sysroot/include"
+    ./configure --host=${HOST} --prefix=/ 
+    make 
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" ${MAKEFLAGS} install
+    skip=1
+}
