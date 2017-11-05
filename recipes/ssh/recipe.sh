7f5f69ed66b3e413fa8bc9cc0a6240e525ae164e
Paul <sajattack@gmail.com>
2017-11-04 17:37:30 -0700

    change ssh recipe to use custom branch of newlib

--- recipes/ssh/recipe.sh
+++ recipes/ssh/recipe.sh
@@ -1,6 +1,6 @@
 VERSION=7.6p1
 TAR=http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$VERSION.tar.gz
-BUILD_DEPENDS=(zlib openssl newlib)
+BUILD_DEPENDS=(zlib openssl)
 
 export AR="${HOST}-ar"
 export AS="${HOST}-as"
@@ -24,7 +24,36 @@ function recipe_update {
     skip=1
 }
 
+function newlib_build {
+    rm -rf ../newlib
+    sysroot="${PWD}/../sysroot"
+    cd ..
+    git clone --recursive https://github.com/sajattack/newlib -b ssh-deps
+    cd newlib
+    pushd newlib/libc/sys
+        aclocal-1.11 -I ../..
+        autoconf
+        automake-1.11 --cygnus Makefile
+    popd
+
+    pushd newlib/libc/sys/redox
+        aclocal-1.11 -I ../../..
+        autoconf
+        automake-1.11 --cygnus Makefile
+    popd
+
+    CC= ./configure --target="${HOST}" --prefix=/
+    make all
+    make DESTDIR="$sysroot" install
+    cd ..
+    cp -r $sysroot/x86_64-unknown-redox/* $sysroot
+    rm -rf $sysroot/x86_64-unknown-redox
+    rm -rf newlib
+    cd build
+}
+
 function recipe_build {
+    newlib_build 
     sysroot="${PWD}/../sysroot"
     export LDFLAGS="-L$sysroot/lib"
     export CPPFLAGS="-I$sysroot/include"
