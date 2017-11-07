91338a697ab17bfa7009a79c3bd12cf735ec413b
Tibor Nagy <xnagytibor@gmail.com>
2017-11-07 19:51:31 +0100

    Add periodictable recipe

---
+++ recipes/periodictable/recipe.sh
@@ -0,0 +1,10 @@
+GIT=https://github.com/xtibor/periodictable.git
+BINDIR="/ui/bin"
+
+function recipe_stage {
+    mkdir -pv "$1/ui/apps"
+    cp -v pkg/manifest "$1/ui/apps/periodictable"
+    mkdir -pv "$1/ui/icons"
+    cp -v pkg/icon.png "$1/ui/icons/periodictable.png"
+}
+
