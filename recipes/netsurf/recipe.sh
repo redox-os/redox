2ff556bce142909c8a66fb474a0154552aa767b4
Jeremy Soller <jackpot51@gmail.com>
2018-02-22 19:27:10 -0700

    Update netsurf recipe

--- recipes/netsurf/recipe.sh
+++ recipes/netsurf/recipe.sh
@@ -2,6 +2,23 @@ VERSION=3.7
 TAR=http://download.netsurf-browser.org/netsurf/releases/source-full/netsurf-all-$VERSION.tar.gz
 BUILD_DEPENDS=(expat curl sdl openssl zlib)
 
+export AR="${HOST}-ar"
+export AS="${HOST}-as"
+export CC="${HOST}-gcc"
+export CXX="${HOST}-g++"
+export LD="${HOST}-ld"
+export NM="${HOST}-nm"
+export OBJCOPY="${HOST}-objcopy"
+export OBJDUMP="${HOST}-objdump"
+export RANLIB="${HOST}-ranlib"
+export READELF="${HOST}-readelf"
+export STRIP="${HOST}-strip"
+
+export PKG_CONFIG_ALLOW_CROSS=1
+export PKG_CONFIG_PATH=
+export PKG_CONFIG_LIBDIR="$PWD/sysroot/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$PWD/sysroot"
+
 function recipe_version {
     echo "$VERSION"
     skip=1
@@ -14,11 +31,9 @@ function recipe_update {
 
 function recipe_build {
     sysroot="${PWD}/../sysroot"
-    export AR="${HOST}-ar"
     export CFLAGS="-I$sysroot/include"
     export LDFLAGS="-L$sysroot/lib"
     export TARGET="framebuffer"
-    export PKG_CONFIG_PATH="$PWD/../sysroot/lib/pkgconfig"
 
     make
     skip=1
@@ -39,4 +54,3 @@ function recipe_stage {
     make DESTDIR="$dest" install
     skip=1
 }
-
