238414801d0e90806dfd4c180e2eed2b0ff03310
Jeremy Soller <jackpot51@gmail.com>
2018-03-24 12:29:11 -0600

    Add drivers-041 recipe

---
+++ recipes/drivers-041/recipe.sh
@@ -0,0 +1,14 @@
+GIT=https://github.com/redox-os/drivers.git
+GIT_BRANCH=0.4.1
+CARGOFLAGS=--all
+
+function recipe_version {
+    echo "0.1.1"
+    skip=1
+}
+
+function recipe_stage {
+    mkdir -pv "$1/etc/pcid"
+    cp -v initfs.toml "$1/etc/pcid/initfs.toml"
+    cp -v filesystem.toml "$1/etc/pcid/filesystem.toml"
+}
