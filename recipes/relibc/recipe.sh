0fa182d351db86871f662727117b8d25a2f1a4f3
Jeremy Soller <jackpot51@gmail.com>
2018-03-26 20:15:19 -0600

    Use install target for relibc

--- recipes/relibc/recipe.sh
+++ recipes/relibc/recipe.sh
@@ -7,11 +7,6 @@ function recipe_build {
 
 function recipe_stage {
     dest="$(realpath $1)"
-    mkdir -pv "$dest/lib"
-    mkdir -pv "$dest/include"
-    cp -rv "include"/* "$dest/include"
-    cp -rv "target/include"/* "$dest/include"
-    cp -v "target/$TARGET/debug/libc.a" "$dest/lib"
-    cp -v "target/$TARGET/debug/crt0.o" "$dest/lib"
+    make DESTDIR="$dest" install
     skip=1
 }
