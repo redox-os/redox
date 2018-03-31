93ae4513bcc046116aaabc10d60c77abb4ca0109
Jeremy Soller <jackpot51@gmail.com>
2018-03-31 08:16:17 -0600

    Add keyboard-sfx recipe

---
+++ recipes/keyboard-sfx/recipe.sh
@@ -0,0 +1,32 @@
+GIT=https://github.com/redox-os/keyboard-sfx.git
+
+function recipe_version {
+    echo "0.0.1"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    echo "skipping build"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    echo "skipping clean"
+    skip=1
+}
+
+function recipe_stage {
+    mkdir -pv "$1/sfx"
+    cp -Rv ./* "$1/sfx"
+    skip=1
+}
