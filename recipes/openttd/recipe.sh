f65c07cbc45678d848b316ef4a0affc3be55c419
Tibor Nagy <xnagytibor@gmail.com>
2018-05-02 03:15:54 +0200

    Add openttd recipe

---
+++ recipes/openttd/recipe.sh
@@ -0,0 +1,39 @@
+VERSION=1.8
+GIT=https://github.com/OpenTTD/OpenTTD.git
+BRANCH=release/$VERSION
+BUILD_DEPENDS=(sdl liborbital zlib)
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    ./configure --build=`gcc -dumpmachine` --host=${HOST} --prefix='' --enable-static --without-lzma --without-liblzo2 --disable-network --without-threads
+    make VERBOSE=1
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    bundledir="$dest/bundle"
+
+    make VERBOSE=1 ROOT_DIR="$dest/../build/" BUNDLE_DIR="$bundledir" INSTALL_DIR="$dest" install
+    rm -rf "$bundledir"
+    skip=1
+}
