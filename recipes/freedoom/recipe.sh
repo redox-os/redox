853723e8af16a3f990af448aa3179177307ed3d5
Jeremy Soller <jackpot51@gmail.com>
2018-05-02 19:18:39 -0600

    Add binaries to run freedoom

--- recipes/freedoom/recipe.sh
+++ recipes/freedoom/recipe.sh
@@ -1,4 +1,5 @@
 GIT=https://github.com/redox-os/freedoom.git
+DEPENDS=(ion prboom)
 
 function recipe_version {
     echo "0.11.3"
@@ -26,8 +27,18 @@ function recipe_clean {
 }
 
 function recipe_stage {
-    mkdir -pv "$1/share/games/doom"
-    cp -Rv ./*.wad "$1/share/games/doom"
+    mkdir -pv "$1/games" "$1/share/games/doom"
+    for file in ./*.wad
+    do
+        game="$(basename "$file" .wad)"
+
+        wad="/share/games/doom/$game.wad"
+        cp -v "$file" "$1$wad"
+
+        bin="/games/$game"
+        echo "#!/bin/ion" > "$1$bin"
+        echo "/games/prboom -geom 800x600 -vidmode 32 -iwad $wad" >> "$1$bin"
+        chmod +x "$1$bin"
+    done
     skip=1
 }
-
