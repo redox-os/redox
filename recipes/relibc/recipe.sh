01f7b16dff00320897dd75c36a7a054cf1eacba3
Jeremy Soller <jeremy@system76.com>
2018-11-13 21:24:49 -0700

    Build tests and install

--- recipes/relibc/recipe.sh
+++ recipes/relibc/recipe.sh
@@ -2,11 +2,14 @@ GIT=https://gitlab.redox-os.org/redox-os/relibc.git
 
 function recipe_build {
     make
+    make -C tests
     skip=1
 }
 
 function recipe_stage {
     dest="$(realpath $1)"
     make DESTDIR="$dest" install
+    mkdir -pv "$dest/share/relibc"
+    cp -rv "tests" "$dest/share/relibc/tests"
     skip=1
 }
