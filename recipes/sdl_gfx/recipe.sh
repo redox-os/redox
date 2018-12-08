79da2471027b30e40d3c80c330dab4f12eee82ca
Tibor Nagy <xnagytibor@gmail.com>
2018-12-09 00:16:28 +0100

    Add sdl_gfx recipe

---
+++ recipes/sdl_gfx/recipe.sh
@@ -0,0 +1,39 @@
+VERSION=2.0.25
+TAR=https://sourceforge.net/projects/sdlgfx/files/SDL_gfx-$VERSION.tar.gz
+BUILD_DEPENDS=(sdl liborbital libiconv)
+
+export CFLAGS="-I$PWD/sysroot/include/"
+export LDFLAGS="-L$PWD/sysroot/lib/"
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    ./autogen.sh
+    ./configure --prefix=/ --host=${HOST} --disable-shared --disable-sdltest
+    make -j"$(nproc)"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" install
+    skip=1
+}
