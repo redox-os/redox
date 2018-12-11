0c89f485065284a6f25b36404a4be38abac53f46
Jeremy Soller <jeremy@system76.com>
2018-12-10 18:58:33 -0700

    Fix glutin and winit compilation

--- recipes/glutin/recipe.sh
+++ recipes/glutin/recipe.sh
@@ -1,4 +1,18 @@
 GIT=https://gitlab.redox-os.org/redox-os/glutin.git
 GIT_UPSTREAM=https://github.com/tomaka/glutin.git
-GIT_BRANCH=redox
+BUILD_DEPENDS=(mesa)
+BRANCH=redox
 CARGOFLAGS="--example window"
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    cp -p "$ROOT/Xargo.toml" "Xargo.toml"
+    xargo rustc --target "$TARGET" --release ${CARGOFLAGS} \
+        -- \
+        -L "${sysroot}/lib" \
+        -l OSMesa \
+        -l glapi \
+        -l pthread \
+        -l stdc++
+    skip=1
+}
--- recipes/winit/recipe.sh
+++ recipes/winit/recipe.sh
@@ -1,4 +1,4 @@
 GIT=https://gitlab.redox-os.org/redox-os/winit.git
 GIT_UPSTREAM=https://github.com/tomaka/winit.git
-GIT_BRANCH=redox
+BRANCH=redox
 CARGOFLAGS="--example window"
