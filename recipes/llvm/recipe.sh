edd15269ebd8822f30741ce6946588265584dad0
Jeremy Soller <jeremy@system76.com>
2018-12-14 17:49:43 -0700

    Fix missing path in strip

--- recipes/llvm/recipe.sh
+++ recipes/llvm/recipe.sh
@@ -75,6 +75,6 @@ function recipe_clean {
 function recipe_stage {
     dest="$(realpath $1)"
     make DESTDIR="$dest" install
-    find "$dest"/{bin,libexec} -exec $STRIP {} ';' 2> /dev/null
+    find "$dest"/bin -exec $STRIP {} ';' 2> /dev/null
     skip=1
 }
