efef3eab261d766e8d5f760af8301b0914bac92c
Jeremy Soller <jeremy@system76.com>
2018-12-14 17:30:50 -0700

    Fix compilation of native tablegen

---
+++ recipes/llvm/native.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_COMPILER cc)
+set(CMAKE_CXX_COMPILER c++)
--- recipes/llvm/recipe.sh
+++ recipes/llvm/recipe.sh
@@ -18,18 +18,20 @@ function recipe_prepare {
 }
 
 function recipe_build {
+    native="$(realpath ../native.cmake)"
     source="$(realpath ../source)"
     sysroot="$(realpath ../sysroot)"
     CMAKE_ARGS=(
         -Wno-dev
         -DCMAKE_CROSSCOMPILING=True
+        -DCROSS_TOOLCHAIN_FLAGS_NATIVE="-DCMAKE_TOOLCHAIN_FILE=$native"
         -DCMAKE_INSTALL_PREFIX="/"
         -DLLVM_DEFAULT_TARGET_TRIPLE="$HOST"
         -DLLVM_TARGET_ARCH="$ARCH"
         -DLLVM_TARGETS_TO_BUILD=X86
         -DCMAKE_SYSTEM_NAME=Generic
         -DPYTHON_EXECUTABLE="/usr/bin/python2"
-        -DLLVM_TABLEGEN="/usr/bin/llvm-tblgen-8"
+        #-DLLVM_TABLEGEN="/usr/bin/llvm-tblgen-8"
         -DUNIX=1
         -DLLVM_ENABLE_THREADS=Off
         -DLLVM_BUILD_BENCHMARKS=Off
@@ -56,7 +58,7 @@ function recipe_build {
         -DLLVM_TOOL_LLVM_RTDYLD_BUILD=Off
     )
     cmake "${CMAKE_ARGS[@]}" "$source"
-    make VERBOSE=1 -j$(nproc)
+    make -j$(nproc)
     skip=1
 }
 
