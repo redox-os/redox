032a361aa7a390f258ab9f2658ae23f1307d0325
Michael Hölzl <FlowPX2@googlemail.com>
2018-12-21 08:45:04 +0100

    Cairo Graphics support with C-Example
    Also i removed "./autogen.sh" from libpng.

---
+++ recipes/cairodemo/recipe.sh
@@ -0,0 +1,44 @@
+BUILD_DEPENDS=(liborbital cairo pixman zlib libpng freetype)
+
+function recipe_version {
+    printf "1.0.0"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_prepare {
+    rm -rf source
+    mkdir source
+    cp cairodemo.c source
+}
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    export LDFLAGS="-L$sysroot/lib"
+    export CPPFLAGS="-I$sysroot/include"
+    set -x
+    "${CXX}" -I "$sysroot/include" -L "$sysroot/lib" cairodemo.c -o cairodemo -lorbital -lcairo -lpixman-1 -lfreetype -lpng -lz -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -lm
+    set +x
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    mkdir -pv "$dest/bin"
+    cp -v "cairodemo" "$dest/bin/cairodemo"
+    skip=1
+}
