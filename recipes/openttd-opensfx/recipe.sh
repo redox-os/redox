95ce8a6a719d201100703d354f1be87ab473553b
Jeremy Soller <jeremy@system76.com>
2018-12-29 20:00:06 -0700

    Add opensfx recipe

---
+++ recipes/openttd-opensfx/recipe.sh
@@ -0,0 +1,34 @@
+GIT=https://gitlab.redox-os.org/redox-os/openttd-opensfx.git
+
+function recipe_version {
+    echo "0.2.3"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    echo "skipping build"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    echo "skipping clean"
+    skip=1
+}
+
+function recipe_stage {
+    mkdir -pv "$1/share/games/openttd/baseset/opensfx"
+    cp -Rv ./* "$1/share/games/openttd/baseset/opensfx"
+    skip=1
+}
+
+
