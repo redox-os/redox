649e1c70f849f3979c61e894bcc454a3af2732e9
Jeremy Soller <jeremy@system76.com>
2018-12-30 11:03:44 -0700

    Add sdl-player

---
+++ recipes/sdl-player/recipe.sh
@@ -0,0 +1,37 @@
+GIT=https://gitlab.redox-os.org/redox-os/sdl-player.git
+BUILD_DEPENDS=(ffmpeg liborbital sdl zlib)
+
+function recipe_version {
+    echo "1.0.0"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    export CPPFLAGS="-I$sysroot/include"
+    export LDFLAGS="-L$sysroot/lib"
+    make -j"$(nproc)"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    mkdir -pv "$dest/bin"
+    cp -v "player" "$dest/bin/sdl-player"
+    skip=1
+}
