0dbd4fd3519668472847ed15056ac43712d7173b
Jeremy Soller <jeremy@system76.com>
2019-01-05 11:06:12 -0700

    Add pcre

---
+++ recipes/pcre/recipe.sh
@@ -0,0 +1,44 @@
+VERSION=8.42
+TAR=https://ftp.pcre.org/pub/pcre/pcre-$VERSION.tar.bz2
+BUILD_DEPENDS=()
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    export CFLAGS="-I$sysroot/include"
+    export LDFLAGS="-L$sysroot/lib"
+    export LIBS="-Wl,--whole-archive -lpthread -Wl,--no-whole-archive"
+    ./configure \
+        --host=${HOST} \
+        --prefix=/ \
+        --disable-shared \
+        --enable-static
+    make -j"$(nproc)"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" install
+    rm -f "$dest/lib/"*.la
+    skip=1
+}
