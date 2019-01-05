bc611cf36e54f2abd4a666c38bab12c0a9c29f1a
Jeremy Soller <jeremy@system76.com>
2019-01-05 10:26:23 -0700

    Prepare for use of llvmpipe

--- recipes/hematite/recipe.sh
+++ recipes/hematite/recipe.sh
@@ -1,18 +1,16 @@
 GIT=https://gitlab.redox-os.org/redox-os/hematite.git
 GIT_UPSTREAM=https://github.com/PistonDevelopers/hematite.git
-BUILD_DEPENDS=(mesa zlib)
+BUILD_DEPENDS=(mesa llvm zlib)
 BRANCH=redox
 
 function recipe_build {
     sysroot="$(realpath ../sysroot)"
     cp -p "$ROOT/Xargo.toml" "Xargo.toml"
+    set -x
     xargo rustc --target "$TARGET" --release ${CARGOFLAGS} \
         -- \
         -L "${sysroot}/lib" \
-        -l OSMesa \
-        -l glapi \
-        -l z \
-        -l stdc++ \
-        -C link-args="-Wl,--whole-archive -lpthread -Wl,--no-whole-archive"
+        -C link-args="$("${PKG_CONFIG}" --libs osmesa) -lglapi -lz -lstdc++ -lc -lgcc"
+    set +x
     skip=1
 }
