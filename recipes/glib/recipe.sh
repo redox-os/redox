cb4f573134accab6e68b42c37a1b7b2c61cc289f
Jeremy Soller <jeremy@system76.com>
2019-01-05 14:33:50 -0700

    Add glib

---
+++ recipes/glib/recipe.sh
@@ -0,0 +1,44 @@
+VERSION=2.59.0
+TAR=https://download.gnome.org/sources/glib/${VERSION%.*}/glib-$VERSION.tar.xz
+BUILD_DEPENDS=(gettext libffi libiconv pcre zlib)
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    export CFLAGS="-I$sysroot/include"
+    export LDFLAGS="-L$sysroot/lib"
+    glib_cv_stack_grows=no ./autogen.sh \
+        --host=${HOST} \
+        --prefix=/ \
+        --disable-shared \
+        --enable-static
+    sed -i 's/#define HAVE_SYS_RESOURCE_H 1/#undef HAVE_SYS_RESOURCE_H/' config.h
+    make -j"$(nproc)"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" install
+    rm -f "$dest/lib/"*.la
+    skip=1
+}
