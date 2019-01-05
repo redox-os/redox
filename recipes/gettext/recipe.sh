c44ee8ef19e7b4a725a58259290c9b8473d2f236
Jeremy Soller <jeremy@system76.com>
2019-01-05 09:39:36 -0700

    Add gettext

---
+++ recipes/gettext/recipe.sh
@@ -0,0 +1,44 @@
+VERSION=0.19.8.1
+TAR=http://ftp.gnu.org/pub/gnu/gettext/gettext-${VERSION}.tar.xz
+BUILD_DEPENDS=(libiconv)
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    export CFLAGS="-I$sysroot/include"
+    export LDFLAGS="-L$sysroot/lib"
+    wget -O build-aux/config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+    ./configure \
+        --host=${HOST} \
+        --prefix=/ \
+        --disable-shared \
+        --enable-static
+    make -j"$(nproc)"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" install
+    rm -f "$dest/lib/"*.la
+    skip=1
+}
