1b994ae338f939ee95cae975baaca294b05d37d5
Jeremy Soller <jeremy@system76.com>
2019-01-05 08:43:53 -0700

    Add libffi

---
+++ recipes/libffi/recipe.sh
@@ -0,0 +1,44 @@
+VERSION=3.2.1
+TAR=ftp://sourceware.org/pub/libffi/libffi-$VERSION.tar.gz
+BUILD_DEPENDS=()
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="$(realpath ../sysroot)"
+    export CFLAGS="-I$sysroot/include"
+    export LDFLAGS="-L$sysroot/lib"
+    wget -O config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+    ./configure \
+        --host=${HOST} \
+        --prefix=/ \
+        --disable-shared \
+        --enable-static
+    make -j"$(nproc)"
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make DESTDIR="$dest" install
+    rm -f "$dest/lib/"*.la
+    skip=1
+}
