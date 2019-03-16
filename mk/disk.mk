2161c9586a0206b1bf29e276d573ad54abfac739
Jeremy Soller <jeremy@system76.com>
2019-03-16 14:55:11 -0600

    Update ci-img target to build all possible disk formats

--- mk/disk.mk
+++ mk/disk.mk
@@ -5,14 +5,8 @@ build/bootloader: bootloader/$(ARCH)/**
 build/harddrive.bin: build/filesystem.bin bootloader/$(ARCH)/**
 	nasm -f bin -o $@ -D ARCH_$(ARCH) -D FILESYSTEM=$< -ibootloader/$(ARCH)/ bootloader/$(ARCH)/disk.asm
 
-build/harddrive.bin.gz: build/harddrive.bin
-	gzip -k -f $<
-
 build/livedisk.bin: build/kernel_live bootloader/$(ARCH)/**
 	nasm -f bin -o $@ -D ARCH_$(ARCH) -D KERNEL=$< -ibootloader/$(ARCH)/ bootloader/$(ARCH)/disk.asm
-
-build/livedisk.bin.gz: build/livedisk.bin
-	gzip -k -f $<
 
 build/livedisk.iso: build/livedisk.bin.gz
 	rm -rf build/iso/
@@ -28,24 +22,23 @@ bootloader-coreboot/build/bootloader: build/kernel_coreboot
 	$(MAKE) -C bootloader-coreboot clean build/bootloader KERNEL="$(ROOT)/$<"
 
 build/coreboot.elf: bootloader-coreboot/build/bootloader
-	cp $< $@
+	cp -v $< $@
 
-bootloader-efi/build/redox_bootloader/boot.efi:
-	$(MAKE) -C bootloader-efi build/redox_bootloader/boot.efi
+bootloader-efi/build/$(EFI_TARGET)/boot.efi: FORCE
+	$(MAKE) -C bootloader-efi build/$(EFI_TARGET)/boot.efi TARGET=$(EFI_TARGET)
 
-build/harddrive-efi.bin: bootloader-efi/build/x86_64-efi-pe/boot.efi build/filesystem.bin
+build/bootloader.efi: bootloader-efi/build/$(EFI_TARGET)/boot.efi
+	cp -v $< $@
+
+build/harddrive-efi.bin: build/bootloader.efi build/filesystem.bin
 	dd if=/dev/zero of=$@.partial bs=1048576 count=4
 	mkfs.vfat $@.partial
 	mmd -i $@.partial efi
 	mmd -i $@.partial efi/boot
 	mcopy -i $@.partial $< ::efi/boot/bootx64.efi
-	mmd -i $@.partial redox_bootloader
 	cat $@.partial build/filesystem.bin > $@
 
-build/harddrive-efi.bin.gz: build/harddrive-efi.bin
-	gzip -k -f $<
-
-build/livedisk-efi.iso: bootloader-efi/build/x86_64-efi-pe/boot.efi build/kernel_live
+build/livedisk-efi.iso: build/bootloader.efi build/kernel_live
 	dd if=/dev/zero of=$@.partial bs=1048576 count=272
 	mkfs.vfat $@.partial
 	mmd -i $@.partial efi
@@ -54,6 +47,3 @@ build/livedisk-efi.iso: bootloader-efi/build/x86_64-efi-pe/boot.efi build/kernel
 	mmd -i $@.partial redox_bootloader
 	mcopy -i $@.partial -s build/kernel_live ::redox_bootloader/kernel
 	mv $@.partial $@
-
-build/livedisk-efi.bin.gz: build/livedisk-efi.bin
-	gzip -k -f $<
