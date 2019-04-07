f31064caa177d3691911ef74850b561efb405b7f
Jeremy Soller <jeremy@system76.com>
2019-04-07 10:47:52 -0600

    Update pkgutils

--- recipes/git/recipe.sh
+++ recipes/git/recipe.sh
@@ -3,7 +3,14 @@ TAR=https://www.kernel.org/pub/software/scm/git/git-$VERSION.tar.xz
 BUILD_DEPENDS=(zlib curl openssl expat)
 DEPENDS="ca-certificates"
 
-MAKEFLAGS="NO_MMAP=1 NEEDS_SSL_WITH_CURL=1 NEEDS_CRYPTO_WITH_SSL=1 NO_UNIX_SOCKETS=1 NEEDS_LIBICONV= NEEDS_LIBRT= BLK_SHA1=1"
+MAKEFLAGS=(
+    NEEDS_SSL_WITH_CURL=1
+    NEEDS_CRYPTO_WITH_SSL=1
+    NO_UNIX_SOCKETS=1
+    NEEDS_LIBICONV=
+    NEEDS_LIBRT=
+    BLK_SHA1=1
+)
 
 function recipe_version {
     echo "$VERSION"
@@ -19,8 +26,15 @@ function recipe_build {
     sysroot="$(realpath ../sysroot)"
     export LDFLAGS="-L$sysroot/lib"
     export CPPFLAGS="-I$sysroot/include"
-    ./configure --build=${BUILD} --host=${HOST} --prefix=/ ac_cv_fread_reads_directories=yes ac_cv_snprintf_returns_bogus=yes ac_cv_lib_curl_curl_global_init=yes CURL_CONFIG=no
-    make ${MAKEFLAGS} -j"$(nproc)"
+    ./configure \
+        --build="${BUILD}" \
+        --host="${HOST}" \
+        --prefix=/ \
+        ac_cv_fread_reads_directories=yes \
+        ac_cv_snprintf_returns_bogus=yes \
+        ac_cv_lib_curl_curl_global_init=yes \
+        CURL_CONFIG=no
+    make "${MAKEFLAGS[@]}" -j"$(nproc)"
     skip=1
 }
 
@@ -36,7 +50,7 @@ function recipe_clean {
 
 function recipe_stage {
     dest="$(realpath $1)"
-    make DESTDIR="$dest" ${MAKEFLAGS} install
+    make DESTDIR="$dest" "${MAKEFLAGS[@]}" install
     ${STRIP} $1/bin/* || true
     ${STRIP} $1/libexec/git-core/* || true
     rm -rf $1/share/man
