c7d62aa831b56e7867675c14d60575bd6071a9ca
Jeremy Soller <jeremy@system76.com>
2019-04-10 20:39:06 -0600

    Fix relibc recipe

--- recipes/relibc/recipe.sh
+++ recipes/relibc/recipe.sh
@@ -1,14 +1,14 @@
 GIT=https://gitlab.redox-os.org/redox-os/relibc.git
 
 function recipe_build {
-    make -j"$(nproc)"
-    make -C tests -j"$(nproc)"
+    make CARGO=xargo -j"$(nproc)"
+    make CARGO=xargo -C tests -j"$(nproc)"
     skip=1
 }
 
 function recipe_stage {
     dest="$(realpath $1)"
-    make DESTDIR="$dest" install
+    make CARGO=xargo DESTDIR="$dest" install
     mkdir -pv "$dest/share/relibc"
     cp -rv "tests" "$dest/share/relibc/tests"
     skip=1
