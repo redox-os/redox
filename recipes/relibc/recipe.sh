ccefd458f7ff2eeb4e21464e1629584c54ebab3a
Jeremy Soller <jeremy@system76.com>
2019-04-15 21:32:30 -0600

    Do not include tests in relibc package

--- recipes/relibc/recipe.sh
+++ recipes/relibc/recipe.sh
@@ -2,14 +2,11 @@ GIT=https://gitlab.redox-os.org/redox-os/relibc.git
 
 function recipe_build {
     make CARGO=xargo -j"$(nproc)"
-    make CARGO=xargo -C tests -j"$(nproc)"
     skip=1
 }
 
 function recipe_stage {
     dest="$(realpath $1)"
     make CARGO=xargo DESTDIR="$dest" install
-    mkdir -pv "$dest/share/relibc"
-    cp -rv "tests" "$dest/share/relibc/tests"
     skip=1
 }
