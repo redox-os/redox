6ad99c593e955627ef9c914b35b7685b562cbe3e
Jeremy Soller <jeremy@system76.com>
2019-04-24 10:34:31 -0600

    Fix netsurf recipe

--- recipes/netsurf/recipe.sh
+++ recipes/netsurf/recipe.sh
@@ -1,6 +1,6 @@
 VERSION=3.7
 TAR=http://download.netsurf-browser.org/netsurf/releases/source-full/netsurf-all-$VERSION.tar.gz
-BUILD_DEPENDS=(curl expat libjpeg libpng openssl sdl zlib freetype liborbital libiconv)
+BUILD_DEPENDS=(curl expat libjpeg libpng nghttp2 openssl sdl zlib freetype liborbital libiconv)
 DEPENDS="ca-certificates orbital"
 
 function recipe_version {
@@ -18,6 +18,8 @@ function recipe_build {
     export TARGET="framebuffer"
     export CFLAGS="-I$sysroot/include -I${PWD}/inst-${TARGET}/include"
     export LDFLAGS="-L$sysroot/lib -L${PWD}/inst-${TARGET}/lib -Wl,--allow-multiple-definition"
+    # nghttp2 is not linked for some reason
+    export LDFLAGS="${LDFLAGS} -lcurl -lnghttp2"
     make V=1 -j"$(nproc)"
     skip=1
 }
