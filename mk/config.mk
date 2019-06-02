e236d55f7f38cd7447a838f61796b9ff0e3a29f6
Jeremy Soller <jeremy@system76.com>
2019-06-02 10:50:41 -0600

    Fix two compile issues

--- mk/config.mk
+++ mk/config.mk
@@ -27,6 +27,7 @@ endif
 
 # Automatic variables
 ROOT=$(CURDIR)
+export RUST_COMPILER_RT_ROOT=$(ROOT)/rust/src/llvm-project/compiler-rt
 export RUST_TARGET_PATH=$(ROOT)/kernel/targets
 export XARGO_HOME=$(ROOT)/build/xargo
 export XARGO_RUST_SRC=$(ROOT)/rust/src
