6d7f1352b61b8b5123f03a564f7c2c49ac86b562
Jeremy Soller <jeremy@system76.com>
2019-07-16 19:22:30 -0600

    Add pango

---
+++ recipes/pango/recipe.sh
@@ -0,0 +1,46 @@
+VERSION="1.38.1"
+TAR="ftp.gnome.org/pub/GNOME/sources/pango/${VERSION%.*}/pango-${VERSION}.tar.xz"
+BUILD_DEPENDS=(cairo expat fontconfig freetype gettext glib harfbuzz libffi libiconv libpng pcre pixman zlib)
+
+function recipe_version {
+	echo "$VERSION"
+	skip=1
+}
+
+function recipe_update {
+	echo "skipping update"
+	skip=1
+}
+
+function recipe_build {
+	wget -O config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+	sysroot="$(realpath ../sysroot)"
+	export CFLAGS="-I$sysroot/include"
+	export LDFLAGS="-L$sysroot/lib --static"
+	export GLIB_MKENUMS="$sysroot/bin/glib-mkenums"
+	./configure \
+	    --build=${BUILD} \
+	    --host=${HOST} \
+	    --prefix=/ \
+	    --disable-shared \
+	    --enable-static
+	make -j"$(nproc)"
+    	skip=1
+}
+
+function recipe_test {
+	echo "skipping test"
+	skip=1
+}
+
+function recipe_clean {
+	make clean
+	skip=1
+}
+
+function recipe_stage {
+	dest="$(realpath $1)"
+	make DESTDIR="$dest" install
+	rm -f "$dest/lib/"*.la
+	skip=1
+}
