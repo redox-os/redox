ef5675278c6614e59cc069b05fd0c6b3f10fb69c
Jeremy Soller <jeremy@system76.com>
2019-10-05 10:04:42 -0600

    Add extra space for livedisk-efi.iso target

--- mk/disk.mk
+++ mk/disk.mk
@@ -43,7 +43,7 @@ build/harddrive-efi.bin: build/bootloader.efi build/filesystem.bin
 	cat $@.partial build/filesystem.bin > $@
 
 build/livedisk-efi.iso: build/bootloader.efi build/kernel_live
-	dd if=/dev/zero of=$@.partial bs=1048576 count=$$(du -mc $^ | grep 'total$$' | cut -f1)
+	dd if=/dev/zero of=$@.partial bs=1048576 count=$$(expr $$(du -mc $^ | grep 'total$$' | cut -f1) + 1)
 	mkfs.vfat $@.partial
 	mmd -i $@.partial efi
 	mmd -i $@.partial efi/boot
