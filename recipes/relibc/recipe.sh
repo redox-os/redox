2150535f64716eb139d020a741b214bdbc8f4b4d
Jeremy Soller <jeremy@system76.com>
2020-03-26 20:48:25 -0600

    Fix xargo deadlock in relibc recipe

--- recipes/relibc/recipe.sh
+++ recipes/relibc/recipe.sh
@@ -1,12 +1,12 @@
 GIT=https://gitlab.redox-os.org/redox-os/relibc.git
 
 function recipe_build {
-    make CARGO=xargo -j"$(nproc)"
+    make CARGO="env -u CARGO xargo" -j"$(nproc)"
     skip=1
 }
 
 function recipe_stage {
     dest="$(realpath $1)"
-    make CARGO=xargo DESTDIR="$dest" install
+    make CARGO="env -u CARGO xargo" DESTDIR="$dest" install
     skip=1
 }
