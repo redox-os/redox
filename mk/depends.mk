8557fe7f2f282809bb4c22e9d8c3b911c033991f
Jeremy Soller <jeremy@system76.com>
2020-06-06 21:35:35 -0600

    Require rustup and nasm

--- mk/depends.mk
+++ mk/depends.mk
@@ -1,5 +1,13 @@
 # Dependencies
 
+ifeq ($(shell which rustup),)
+$(error rustup not found, install from "https://rustup.rs/")
+endif
+
+ifeq ($(shell which nasm),)
+$(error nasm not found, install from your package manager)
+endif
+
 CARGO_CONFIG_VERSION=0.1.1
 ifeq ($(shell cargo install --list | grep '^cargo-config v$(CARGO_CONFIG_VERSION):$$'),)
 $(error cargo-config $(CARGO_CONFIG_VERSION) not found, run "cargo install --force --version $(CARGO_CONFIG_VERSION) cargo-config")
