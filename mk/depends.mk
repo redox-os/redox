14b048c34e858744eaea7889df5ba06f2aa53c1a
Jeremy Soller <jeremy@system76.com>
2020-06-18 20:14:38 -0600

    Unset RUSTUP_TOOLCHAIN when checking for Rust depends

--- mk/depends.mk
+++ mk/depends.mk
@@ -9,11 +9,11 @@ $(error nasm not found, install from your package manager)
 endif
 
 CARGO_CONFIG_VERSION=0.1.1
-ifeq ($(shell cargo install --list | grep '^cargo-config v$(CARGO_CONFIG_VERSION):$$'),)
+ifeq ($(shell env -u RUSTUP_TOOLCHAIN cargo install --list | grep '^cargo-config v$(CARGO_CONFIG_VERSION):$$'),)
 $(error cargo-config $(CARGO_CONFIG_VERSION) not found, run "cargo install --force --version $(CARGO_CONFIG_VERSION) cargo-config")
 endif
 
 XARGO_VERSION=0.3.20
-ifeq ($(shell cargo install --list | grep '^xargo v$(XARGO_VERSION):$$'),)
+ifeq ($(shell env -u RUSTUP_TOOLCHAIN cargo install --list | grep '^xargo v$(XARGO_VERSION):$$'),)
 $(error xargo $(XARGO_VERSION) not found, run "cargo install --force --version $(XARGO_VERSION) xargo")
 endif
