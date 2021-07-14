cf861a663a921a0e7fcd0c28e7f2900075fb8981
Jeremy Soller <jeremy@system76.com>
2021-07-14 12:24:18 -0600

    Use cargo instead of xargo for rust-cairo and rust-cairo-demo

--- recipes/rust-cairo/recipe.sh
+++ recipes/rust-cairo/recipe.sh
@@ -5,7 +5,7 @@ CARGOFLAGS="--example gui"
 function recipe_build {
     sysroot="$(realpath ../sysroot)"
     cp -p "$ROOT/Xargo.toml" "Xargo.toml"
-    xargo rustc --target "$TARGET" --release ${CARGOFLAGS} \
+    cargo rustc --target "$TARGET" --release ${CARGOFLAGS} \
         -- \
         -L "${sysroot}/lib" \
         -l cairo \
