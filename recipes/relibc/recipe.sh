f848f9f87fba663cab86f994f5912d8d8c4cf0c6
Jeremy Soller <jeremy@system76.com>
2021-07-14 12:18:51 -0600

    Use cargo instead of xargo for relibc recipe

--- recipes/relibc/recipe.sh
+++ recipes/relibc/recipe.sh
@@ -1,12 +1,12 @@
 GIT=https://gitlab.redox-os.org/redox-os/relibc.git
 
 function recipe_build {
-    "$REDOX_MAKE" CARGO="env -u CARGO xargo" -j"$($NPROC)"
+    "$REDOX_MAKE" CARGO="env -u CARGO cargo" -j"$($NPROC)"
     skip=1
 }
 
 function recipe_stage {
     dest="$(realpath $1)"
-    "$REDOX_MAKE" CARGO="env -u CARGO xargo" DESTDIR="$dest" install
+    "$REDOX_MAKE" CARGO="env -u CARGO cargo" DESTDIR="$dest" install
     skip=1
 }
