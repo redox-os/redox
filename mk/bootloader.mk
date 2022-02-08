537915de3b9353de26be48007dcc1404a3286a02
Jeremy Soller <jackpot51@gmail.com>
2022-02-08 14:17:39 -0700

    Update bootloader, remove bootloader-efi

--- mk/bootloader.mk
+++ mk/bootloader.mk
@@ -6,18 +6,18 @@ build/bootloader.bin: bootloader/build/$(BOOTLOADER_TARGET)/bootloader.bin
 	mkdir -p build
 	cp -v $< $@
 
+bootloader/build/$(EFI_TARGET)/bootloader.efi: FORCE
+	env --unset=RUST_TARGET_PATH --unset=RUSTUP_TOOLCHAIN --unset=XARGO_RUST_SRC \
+	$(MAKE) -C bootloader build/$(EFI_TARGET)/bootloader.efi TARGET=$(EFI_TARGET)
+
+build/bootloader.efi: bootloader/build/$(EFI_TARGET)/bootloader.efi
+	mkdir -p build
+	cp -v $< $@
+
 bootloader-coreboot/build/bootloader: build/kernel_coreboot
 	env --unset=RUST_TARGET_PATH --unset=RUSTUP_TOOLCHAIN --unset=XARGO_RUST_SRC \
 	$(MAKE) -C bootloader-coreboot clean build/bootloader KERNEL="$(ROOT)/$<"
 
 build/coreboot.elf: bootloader-coreboot/build/bootloader
-	mkdir -p build
-	cp -v $< $@
-
-bootloader-efi/build/$(EFI_TARGET)/boot.efi: FORCE
-	env --unset=RUST_TARGET_PATH --unset=RUSTUP_TOOLCHAIN --unset=XARGO_RUST_SRC \
-	$(MAKE) -C bootloader-efi build/$(EFI_TARGET)/boot.efi TARGET=$(EFI_TARGET)
-
-build/bootloader.efi: bootloader-efi/build/$(EFI_TARGET)/boot.efi
 	mkdir -p build
 	cp -v $< $@
