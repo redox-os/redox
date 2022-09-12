939b650125fc8885f9c5300f2f829f62f6b9652f
Jeremy Soller <jackpot51@gmail.com>
2022-09-12 07:51:13 -0600

    Build repository outside of installer

---
+++ mk/repo.mk
@@ -0,0 +1,16 @@
+build/fetch.tag: cookbook installer prefix $(FILESYSTEM_CONFIG)
+	$(HOST_CARGO) build --manifest-path cookbook/Cargo.toml --release
+	$(HOST_CARGO) build --manifest-path installer/Cargo.toml --release
+	PACKAGES="$$($(INSTALLER) --list-packages -c $(FILESYSTEM_CONFIG))" && \
+	cd cookbook && \
+	./fetch.sh "$${PACKAGES}"
+
+build/repo.tag: build/fetch.tag
+	$(HOST_CARGO) build --manifest-path cookbook/Cargo.toml --release
+	$(HOST_CARGO) build --manifest-path installer/Cargo.toml --release
+	export PATH="$(PREFIX_PATH):$$PATH" && \
+	PACKAGES="$$($(INSTALLER) --list-packages -c $(FILESYSTEM_CONFIG))" && \
+	cd cookbook && \
+	./repo.sh "$${PACKAGES}"
+	mkdir -p build
+	touch $@
