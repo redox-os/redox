1772f78aa973e92653f20c2b445be962c3a8e54d
Ron Williams <ron.williams@englishbluffcg.com>
2022-11-11 15:23:08 -0800

    Add podman build

--- mk/depends.mk
+++ mk/depends.mk
@@ -1,5 +1,8 @@
 # Dependencies
 
+# Don't check for Rust/Cargo if you will be using Podman
+ifneq ($(PODMAN_BUILD),1)
+
 ifeq ($(shell which rustup),)
 $(error rustup not found, install from "https://rustup.rs/")
 endif
@@ -11,4 +14,6 @@ endif
 CARGO_CONFIG_VERSION=0.1.1
 ifeq ($(shell env -u RUSTUP_TOOLCHAIN cargo install --list | grep '^cargo-config v$(CARGO_CONFIG_VERSION):$$'),)
 $(error cargo-config $(CARGO_CONFIG_VERSION) not found, run "cargo install --force --version $(CARGO_CONFIG_VERSION) cargo-config")
+endif
+
 endif
