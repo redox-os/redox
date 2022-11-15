2e42fbb6c8aa515a5399de7d2149c88544ec3827
Ron Williams <ron.williams@englishbluffcg.com>
2022-11-15 12:35:44 -0800

    move Rust install to a persistent container dir

---
+++ podman/rustinstall.sh
@@ -0,0 +1,7 @@
+#/usr/bin/env bash
+
+# Install Rust in Podman, after the image has been built
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+cargo install --force --version 0.1.1 cargo-config
+cargo install --force --version 0.3.20 xargo
