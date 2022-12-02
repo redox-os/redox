24e63127e3e444b02549ef1f761a8f4cc1e25889
Jeremy Soller <jackpot51@gmail.com>
2022-12-01 18:00:15 -0700

    Improvements to freeciv recipe, still not working

--- recipes/freeciv/recipe.sh
+++ recipes/freeciv/recipe.sh
@@ -1,4 +1,4 @@
-VERSION=2.6.0
+VERSION=2.6.6
 TAR=http://files.freeciv.org/stable/freeciv-$VERSION.tar.bz2
 BUILD_DEPENDS=(curl freetype libiconv liborbital libpng openssl nghttp2 sdl sdl_gfx sdl_image sdl_mixer sdl_ttf zlib)
 
@@ -11,7 +11,8 @@ function recipe_build {
     wget -O bootstrap/config.sub "https://gitlab.redox-os.org/redox-os/gnu-config/-/raw/master/config.sub?inline=false"
     sysroot="$(realpath ../sysroot)"
     export CFLAGS="-I$sysroot/include"
-    export LDFLAGS="-L$sysroot/lib"
+    export LDFLAGS="-L$sysroot/lib --static"
+    export SDL_CONFIG="$sysroot/bin/sdl-config"
     ./configure \
         --build=${BUILD} \
         --host="$HOST" \
@@ -24,7 +25,9 @@ function recipe_build {
         --with-sdl-prefix="$sysroot" \
         ac_cv_lib_SDL_image_IMG_Load=yes \
         ac_cv_lib_SDL_ttf_TTF_OpenFont=yes \
-        ac_cv_lib_SDL_gfx_rotozoomSurface=yes
+        ac_cv_lib_SDL_gfx_rotozoomSurface=yes \
+        gui_sdl_cflags="${CFLAGS}" \
+        gui_sdl_ldflags="${LDFLAGS}"
     "$REDOX_MAKE" -j"$($NPROC)" V=1
     skip=1
 }
