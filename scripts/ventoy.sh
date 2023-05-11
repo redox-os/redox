7cbd57a0ecd1e0be14f303ccc73426e8eb037030
Jeremy Soller <jackpot51@gmail.com>
2023-05-10 20:25:07 -0600

    Add ventoy script

---
+++ scripts/ventoy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+ARCHS=(
+    i686
+    x86_64
+)
+CONFIGS=(
+    desktop
+)
+
+VENTOY="/media/${USER}/Ventoy"
+if [ ! -d "${VENTOY}" ]
+then
+    echo "Ventoy not mounted" >&2
+    exit 1
+fi
+
+for ARCH in "${ARCHS[@]}"
+do
+    for CONFIG_NAME in "${CONFIGS[@]}"
+    do
+        IMAGE="build/${ARCH}/${CONFIG_NAME}/livedisk.iso"
+        make ARCH="${ARCH}" CONFIG_NAME="${CONFIG_NAME}" "${IMAGE}"
+        cp -v "${IMAGE}" "${VENTOY}/redox-${CONFIG_NAME}-${ARCH}.iso"
+    done
+done
+
+echo "Finished copying configs (${CONFIGS[@]}) for archs (${ARCHS[@]})"
