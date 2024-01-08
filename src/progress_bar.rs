64ea29f63bbb2b67ac2ae03497cdd331548c2a71
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-01-08 19:42:50 +0100

    Remove ProgressBarWrite
    It is unused.

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -23,29 +23,3 @@ impl<'p, 'r, P: Write, R: Read> Read for ProgressBarRead<'p, 'r, P, R> {
         Ok(count)
     }
 }
-
-pub struct ProgressBarWrite<'p, 'w, P: Write + 'p, W: Write + 'w> {
-    pb: &'p mut ProgressBar<P>,
-    w: &'w mut W,
-}
-
-impl<'p, 'w, P: Write, W: Write> ProgressBarWrite<'p, 'w, P, W> {
-    pub fn _new(pb: &'p mut ProgressBar<P>, w: &'w mut W) -> ProgressBarWrite<'p, 'w, P, W> {
-        ProgressBarWrite {
-            pb,
-            w
-        }
-    }
-}
-
-impl<'p, 'w, P: Write, W: Write> Write for ProgressBarWrite<'p, 'w, P, W> {
-    fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        let count = self.w.write(buf)?;
-        self.pb.add(count as u64);
-        Ok(count)
-    }
-
-    fn flush(&mut self) -> Result<()> {
-        self.w.flush()
-    }
-}
