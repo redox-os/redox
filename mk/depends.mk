33769cd851c0429dbed2b84d0cd2d102bad9a5f8
Jeremy Soller <jackpot51@gmail.com>
2024-01-12 13:04:29 -0700

    Allow any just version

--- mk/depends.mk
+++ mk/depends.mk
@@ -15,14 +15,13 @@ ifeq ($(shell which nasm),)
 $(error nasm not found, install from your package manager)
 endif
 
+ifeq ($(shell which just),)
+$(error just not found, install from crates.io or from your package manager)
+endif
+
 CARGO_CONFIG_VERSION=0.1.1
 ifeq ($(shell env -u RUSTUP_TOOLCHAIN cargo install --list | grep '^cargo-config v$(CARGO_CONFIG_VERSION):$$'),)
 $(error cargo-config $(CARGO_CONFIG_VERSION) not found, run "cargo install --force --version $(CARGO_CONFIG_VERSION) cargo-config")
-endif
-
-JUST_VERSION=1.16.0
-ifeq ($(shell env -u RUSTUP_TOOLCHAIN cargo install --list | grep '^just v$(JUST_VERSION):$$'),)
-$(error just $(JUST_VERSION) not found, run "cargo install --force --version $(JUST_VERSION) just")
 endif
 
 endif
