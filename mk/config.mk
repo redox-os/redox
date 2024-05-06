81670ea23e3316b2321037e41d846cdec771104b
Jeremy Soller <jackpot51@gmail.com>
2024-05-06 11:35:23 -0600

    Disable binary prefix when not building on x86_64 Linux

--- mk/config.mk
+++ mk/config.mk
@@ -36,6 +36,10 @@ CONTAINERFILE?=podman/redox-base-containerfile
 export NPROC=nproc
 export REDOX_MAKE=make
 HOST_TARGET := $(shell env -u RUSTUP_TOOLCHAIN rustc -vV | grep host | cut -d: -f2 | tr -d " ")
+ifneq ($(HOST_TARGET),x86_64-unknown-linux-gnu)
+	# The binary prefix is only built for x86_64 Linux hosts
+	PREFIX_BINARY=0
+endif
 UNAME := $(shell uname)
 ifeq ($(UNAME),Darwin)
 	FUMOUNT=umount
