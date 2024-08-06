e0ca1633ff1721c5165df1b8588230cd15f8df08
Ribbon <ribbon_45@proton.me>
2024-08-06 11:37:34 +0000

    Finish more scripts

--- scripts/print-recipe.sh
+++ scripts/print-recipe.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 
+# This script print the recipe configuration
+
+cat "$(find cookbook/recipes -name $1)"/recipe.*
--- scripts/recipe-path.sh
+++ scripts/recipe-path.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This script print the location of recipes
+
 FIND_RECIPE="find cookbook/recipes -maxdepth 4 -name"
 
 for recipe in $*
