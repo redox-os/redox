508e773fcaa5e95f1195a2e8398a497077049c22
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-09-28 06:14:14 +0200

    bump cbindgen version

--- podman/rustinstall.sh
+++ podman/rustinstall.sh
@@ -6,4 +6,4 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
 cargo +stable install --force --version 0.1.1 cargo-config
 cargo +stable install --force --version 1.16.0 just
-cargo +stable install --force --version 0.26.0 cbindgen
+cargo +stable install --force --version 0.27.0 cbindgen
