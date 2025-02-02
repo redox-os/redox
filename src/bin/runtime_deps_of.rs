2fa33aae81925283cd5d5cc6ab71a744c66abb1f
Anhad Singh <andypython@protonmail.com>
2025-02-02 16:55:14 +1100

    fix(runtime_deps_of): do not error out if no packages are specified
    This may be the case if repo.sh did not build any TOML recipes. In that
    case, we can just exit cleanly.
    
    Signed-off-by: Anhad Singh <andypython@protonmail.com>

--- src/bin/runtime_deps_of.rs
+++ src/bin/runtime_deps_of.rs
@@ -5,15 +5,10 @@ use std::{env::args, process::ExitCode};
 const DEP_DEPTH: usize = 16;
 
 fn usage() {
-    eprintln!("Usage: pkg_deps_of package1 [package2 ...]");
+    eprintln!("Usage: pkg_deps_of [package1 package2 ...]");
 }
 
 fn main() -> ExitCode {
-    if args().len() < 2 {
-        usage();
-        return ExitCode::FAILURE;
-    }
-
     let names = args().skip(1).collect::<Vec<String>>();
     let recipes = CookRecipe::new_recursive(&names, DEP_DEPTH, true).expect("recipe not found");
 
