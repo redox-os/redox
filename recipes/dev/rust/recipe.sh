55fd58171da9bacf1aae5dd824da47c778394728
Anhad Singh <andypython@protonmail.com>
2025-02-10 23:22:28 +1100

    feat(rustc): dynamic
    Signed-off-by: Anhad Singh <andypython@protonmail.com>

--- recipes/dev/rust/recipe.sh
+++ recipes/dev/rust/recipe.sh
@@ -1,6 +1,6 @@
 GIT=https://gitlab.redox-os.org/redox-os/rust.git
 BRANCH=redox-2024-05-11
-BUILD_DEPENDS=(llvm18)
+BUILD_DEPENDS=(llvm18-shared zlib libgcc)
 DEPENDS="gcc13 cargo"
 PREPARE_COPY=0
 
@@ -11,6 +11,7 @@ function recipe_version {
 
 function recipe_build {
     unset AR AS CC CXX LD NM OBJCOPY OBJDUMP RANLIB READELF STRIP
+    export MAGIC_EXTRA_RUSTFLAGS="-C link-args=-lz"
     python3 "${COOKBOOK_SOURCE}/x.py" install --config "${COOKBOOK_RECIPE}/config.toml" --jobs $(nproc) --incremental
     skip=1
 }
