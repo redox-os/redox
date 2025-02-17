a138fa63ca156739f5bdd8e661aa198efc9150a3
Vincent Berthier <vincent.berthier@posteo.org>
2025-02-17 17:28:14 +0100

    Add dev env for NixOS

--- mk/depends.mk
+++ mk/depends.mk
@@ -3,8 +3,13 @@
 # Don't check for Rust/Cargo if you will be using Podman
 ifneq ($(PODMAN_BUILD),1)
 
+# don’t check for Rust and Cargo if building on a Nix system
+ifneq ($(NIX_SHELL_BUILD),1)
+
 ifeq ($(shell which rustup),)
 $(error rustup not found, install from "https://rustup.rs/")
+endif
+
 endif
 
 ifeq ($(shell which cbindgen),)
@@ -19,9 +24,11 @@ ifeq ($(shell which just),)
 $(error 'just' not found, install from crates.io or from your package manager)
 endif
 
+ifneq ($(NIX_SHELL_BUILD),1)
 CARGO_CONFIG_VERSION=0.1.1
 ifeq ($(shell env -u RUSTUP_TOOLCHAIN cargo install --list | grep '^cargo-config v$(CARGO_CONFIG_VERSION):$$'),)
 $(error cargo-config $(CARGO_CONFIG_VERSION) not found, run "cargo install --force --version $(CARGO_CONFIG_VERSION) cargo-config")
+endif
 endif
 
 endif
