7d65ef505724f23cd0729a40bcd31d5d7fc5239f
Jeremy Soller <jackpot51@gmail.com>
2025-04-18 10:44:49 -0600

    Drop PREFER_STATIC from makefiles

--- mk/repo.mk
+++ mk/repo.mk
@@ -1,15 +1,12 @@
 # Configuration file for recipe commands
 
-PREFER_STATIC?=
-
 $(BUILD)/fetch.tag: prefix $(FSTOOLS_TAG) $(FILESYSTEM_CONFIG) $(CONTAINER_TAG)
 ifeq ($(PODMAN_BUILD),1)
-	$(PODMAN_RUN) $(MAKE) $@ PREFER_STATIC=$(PREFER_STATIC)
+	$(PODMAN_RUN) $(MAKE) $@
 else
 	export PATH="$(PREFIX_PATH):$$PATH" && \
 	PACKAGES="$$($(LIST_PACKAGES) $(LIST_PACKAGES_OPTS) -c $(FILESYSTEM_CONFIG))" && \
 	export COOKBOOK_HOST_SYSROOT="$(ROOT)/$(PREFIX_INSTALL)" && \
-	export COOKBOOK_PREFER_STATIC="$(PREFER_STATIC)" && \
 	cd cookbook && \
 	./fetch.sh "$${PACKAGES}"
 	mkdir -p $(BUILD)
@@ -18,11 +15,10 @@ endif
 
 $(REPO_TAG): $(BUILD)/fetch.tag $(FSTOOLS_TAG) $(CONTAINER_TAG)
 ifeq ($(PODMAN_BUILD),1)
-	$(PODMAN_RUN) $(MAKE) $@ PREFER_STATIC=$(PREFER_STATIC)
+	$(PODMAN_RUN) $(MAKE)
 else
 	export PATH="$(PREFIX_PATH):$$PATH" && \
 	export COOKBOOK_HOST_SYSROOT="$(ROOT)/$(PREFIX_INSTALL)" && \
-	export COOKBOOK_PREFER_STATIC="$(PREFER_STATIC)" && \
 	PACKAGES="$$($(LIST_PACKAGES) $(LIST_PACKAGES_OPTS) -c $(FILESYSTEM_CONFIG))" && \
 	cd cookbook && \
 	./repo.sh $(REPO_NONSTOP) "$${PACKAGES}"
@@ -36,11 +32,10 @@ endif
 # Invoke clean.sh for a single target
 c.%: $(FSTOOLS_TAG) FORCE
 ifeq ($(PODMAN_BUILD),1)
-	$(PODMAN_RUN) $(MAKE) $@ PREFER_STATIC=$(PREFER_STATIC)
+	$(PODMAN_RUN) $(MAKE) $@
 else
 	export PATH="$(PREFIX_PATH):$$PATH" && \
 	export COOKBOOK_HOST_SYSROOT="$(ROOT)/$(PREFIX_INSTALL)" && \
-	export COOKBOOK_PREFER_STATIC="$(PREFER_STATIC)" && \
 	cd cookbook && \
 	./clean.sh $*
 endif
@@ -48,11 +43,10 @@ endif
 # Invoke fetch.sh for a single target
 f.%: $(FSTOOLS_TAG) FORCE
 ifeq ($(PODMAN_BUILD),1)
-	$(PODMAN_RUN) $(MAKE) $@ PREFER_STATIC=$(PREFER_STATIC)
+	$(PODMAN_RUN) $(MAKE) $@
 else
 	export PATH="$(PREFIX_PATH):$$PATH" && \
 	export COOKBOOK_HOST_SYSROOT="$(ROOT)/$(PREFIX_INSTALL)" && \
-	export COOKBOOK_PREFER_STATIC="$(PREFER_STATIC)" && \
 	cd cookbook && \
 	./fetch.sh $*
 endif
@@ -60,11 +54,10 @@ endif
 # Invoke repo.sh for a single target
 r.%: $(FSTOOLS_TAG) FORCE
 ifeq ($(PODMAN_BUILD),1)
-	$(PODMAN_RUN) $(MAKE) $@ PREFER_STATIC=$(PREFER_STATIC)
+	$(PODMAN_RUN) $(MAKE) $@
 else
 	export PATH="$(PREFIX_PATH):$$PATH" && \
 	export COOKBOOK_HOST_SYSROOT="$(ROOT)/$(PREFIX_INSTALL)" && \
-	export COOKBOOK_PREFER_STATIC="$(PREFER_STATIC)" && \
 	cd cookbook && \
 	./repo.sh $*
 endif
@@ -72,11 +65,10 @@ endif
 # Invoke unfetch.sh for a single target
 u.%: $(FSTOOLS_TAG) FORCE
 ifeq ($(PODMAN_BUILD),1)
-	$(PODMAN_RUN) $(MAKE) $@ PREFER_STATIC=$(PREFER_STATIC)
+	$(PODMAN_RUN) $(MAKE) $@
 else
 	export PATH="$(PREFIX_PATH):$$PATH" && \
 	export COOKBOOK_HOST_SYSROOT="$(ROOT)/$(PREFIX_INSTALL)" && \
-	export COOKBOOK_PREFER_STATIC="$(PREFER_STATIC)" && \
 	cd cookbook && \
 	./unfetch.sh $*
 endif
