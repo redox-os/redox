0137ba71b61d56eae63929d46632dadc8bf0e93e
Maltsev andrey <maltsev.a2012@gamil.com>
2025-05-23 00:10:50 +0200

    small fix building reliases on aarch64

--- podman/rustinstall.sh
+++ podman/rustinstall.sh
@@ -7,3 +7,4 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
 cargo +stable install --force --version 0.1.1 cargo-config
 cargo +stable install --force --version 1.16.0 just
 cargo +stable install --force --version 0.27.0 cbindgen
+
