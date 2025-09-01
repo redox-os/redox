f4d3630ef8d24b7ef1cf59f4d6a53f7f6a3bd1b6
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-09-01 21:38:14 +0200

    Remove all code that handles recipe.sh

--- repo.sh
+++ repo.sh
@@ -34,9 +34,6 @@ then
     recipes="$(target/release/list_recipes)"
 fi
 
-# All $recipes that are in the new TOML format.
-toml_recipes=""
-
 for recipe in $recipes
 do
     recipe_path=`target/release/find_recipe $recipe`
@@ -47,65 +44,18 @@ do
     COOKBOOK_SOURCE="${COOKBOOK_RECIPE}/source"
     COOKBOOK_SYSROOT="${TARGET_DIR}/sysroot"
 
-    if [ -e "${COOKBOOK_RECIPE}/recipe.toml" ]
-    then
-        toml_recipes+=" $recipe"
-        target/release/cook $COOK_OPT "$recipe"
-        continue
-    fi
-
-    if [ ! -d "${COOKBOOK_SOURCE}" ]
-    then
-        echo -e "\033[01;38;5;155mrepo - fetching $recipe\033[0m" >&2
-        ./cook.sh "$recipe" fetch
-    fi
-
-    if [ ! -d "${COOKBOOK_BUILD}" ]
-    then
-        echo -e "\033[01;38;5;155mrepo - preparing $recipe\033[0m" >&2
-        ./cook.sh "$recipe" prepare
-    elif [ ! -d "${COOKBOOK_SYSROOT}" ]
-    then
-        echo -e "\033[01;38;5;155mrepo - repreparing $recipe\033[0m" >&2
-        ./cook.sh "$recipe" unprepare prepare
-    else
-        TIME_SOURCE="$($FIND "${COOKBOOK_SOURCE}" -type f -not -path '*/.git*' -printf "%Ts\n" | sort -nr | head -n 1)"
-        TIME_BUILD="$($FIND "${COOKBOOK_BUILD}" -type f -not -path '*/.git*' -printf "%Ts\n" | sort -nr | head -n 1)"
-        if [ "$TIME_SOURCE" -gt "$TIME_BUILD" ]
-        then
-            echo -e "\033[01;38;5;155mrepo - repreparing $recipe\033[0m" >&2
-            ./cook.sh "$recipe" unprepare prepare
-        fi
-    fi
-
-    if [ ! -f "${COOKBOOK_STAGE}.pkgar" ]
-    then
-        echo -e "\033[01;38;5;155mrepo - building $recipe\033[0m" >&2
-        ./cook.sh "$recipe" build stage pkg $DEBUG
-    else
-        TIME_BUILD="$($FIND "${COOKBOOK_BUILD}" -type f -not -path '*/.git*' -printf "%Ts\n" | sort -nr | head -n 1)"
-        TIME_STAGE="$($STAT -c "%Y" "${COOKBOOK_STAGE}.pkgar")"
-        TIME_RECIPE="$($FIND "${COOKBOOK_RECIPE}"/{recipe.sh,*.patch} -printf '%Ts\n' | sort -nr | head -n 1)"
-        if [ "$TIME_BUILD" -gt "$TIME_STAGE" -o "$TIME_RECIPE" -gt "$TIME_STAGE" ]
-        then
-            echo -e "\033[01;38;5;155mrepo - rebuilding $recipe\033[0m" >&2
-            ./cook.sh "$recipe" untar unstage build stage pkg $DEBUG
-        else
-            echo -e "\033[01;38;5;155mrepo - $recipe up to date\033[0m" >&2
-        fi
-    fi
+    target/release/cook $COOK_OPT "$recipe"
 done
 
 mkdir -p "$REPO"
 
 declare -A APPSTREAM_SOURCES
 
-# Currently, we only support runtime dependencies for recipes in the new TOML
-# format. Runtime dependencies include both `[package.dependencies]` and
-# dynamically linked packages discovered by auto_deps.
-# 
+# Runtime dependencies include both `[package.dependencies]` and dynamically
+# linked packages discovered by auto_deps.
+#
 # The following adds the package dependencies of the recipes to the repo as
 # well.
-recipes="$recipes $(target/release/pkg_deps $toml_recipes)"
+recipes="$recipes $(target/release/pkg_deps $recipes)"
 
 target/release/repo_builder "$REPO" $recipes
