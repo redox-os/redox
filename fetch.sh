10e189306f93da37d2bd324702bb01aff14d8618
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-09-02 20:43:39 +0200

    Only accept recipe name in shell scripts
    Rather than recipe path.

--- fetch.sh
+++ fetch.sh
@@ -22,15 +22,7 @@ then
     recipes="$(target/release/list_recipes)"
 fi
 
-for recipe_path in $recipes
+for recipe_name in $recipes
 do
-    if (echo "$recipe_path" | grep '.*/.*' >/dev/null); then
-        recipe_name=$(basename "$recipe_path")
-        recipe_path="$recipe_path"
-    else
-        recipe_name="$recipe_path"
-        recipe_path=`target/release/find_recipe $recipe_name`
-    fi
-
     target/release/cook --fetch-only "$recipe_name"
 done
