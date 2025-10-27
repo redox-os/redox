e7c4d5aff38dca6b49f7b9ea6e4b49ab343231ea
Wildan M <willnode@wellosoft.net>
2025-10-28 00:29:54 +0700

    Update to use the new repo bin

--- scripts/category.sh
+++ scripts/category.sh
@@ -6,26 +6,9 @@ if [ -z "$1" ] || [ -z "$2" ]
 then
     echo "Build or clean all recipe directories in a category" >&2
     echo Usage: $0 "<action>" "<recipe-category>" >&2
-    echo "<action>" can be f, r, c, u, or combinations that \"make\" understands >&2
+    echo "<action>" can be f, r, c, u, p, or combinations that \"make\" understands >&2
+    echo "<category>" can be path of category you want to run e.g. \"core\", \"wip\", \"wip/dev\" >&2
     exit 1
 fi
 
-action="${1#-}"
-
-recipe_list=""
-first=1
-
-for recipe in `find cookbook/recipes/"$2" -name "recipe.*"`
-do
-    recipe_folder=`dirname "$recipe"`
-    recipe_name=`basename "$recipe_folder"`
-    if [ "$first" -eq 1 ]; then
-        recipe_list="$recipe_name"
-        first=0
-    else
-        recipe_list="$recipe_list,$recipe_name"
-    fi
-done
-
-set -x
-make "$action"."$recipe_list"
+make "${1#-}"."--category-$2"
