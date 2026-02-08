b85e5fa2d8d99129ea1087bcce7e85aecc4428dc
Wildan M <willnode@wellosoft.net>
2026-02-08 12:49:34 +0700

    Accurate push with deps, separate tree command

--- mk/repo.mk
+++ mk/repo.mk
@@ -122,7 +122,11 @@ pp.%: $(FSTOOLS_TAG) FORCE
 	$(MAKE) p.$*,--with-package-deps
 
 # Show what to push
+<<<<<<< HEAD
 pt.%: $(FSTOOLS_TAG) FORCE
+=======
+pt.%: prefix $(FSTOOLS_TAG) FORCE
+>>>>>>> e940c2cd (Accurate push with deps, separate tree command)
 ifeq ($(PODMAN_BUILD),1)
 	$(PODMAN_RUN) make $@
 else
