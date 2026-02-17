b6d441b53617d80ba43579f8f8cce804707cc9b8
Jeremy Soller <jackpot51@gmail.com>
2026-02-17 10:16:17 -0700

    Fix static_clean

--- mk/prefix.mk
+++ mk/prefix.mk
@@ -32,7 +32,7 @@ static_clean: | $(FSTOOLS_TAG)
 	$(MAKE) c.relibc
 	$(MAKE) c.base,base-initfs,extrautils,kernel,ion,pkgutils,redoxfs
 	$(MAKE) c.bash,luajit,gettext,openssl1,pcre2,sdl1,zstd,zlib,bzip2,xz
-	$(MAKE) c.expat,freetype,libffi,libiconv,libjpeg,liborbital,libpng,libxml2,ncurses,ncursesw
+	$(MAKE) c.expat,freetype2,libffi,libiconv,libjpeg,liborbital,libpng,libxml2,ncurses,ncursesw
 
 $(PREFIX)/relibc-install: $(PREFIX)/rust-install $(PREFIX)/gcc-install | $(FSTOOLS_TAG) $(CONTAINER_TAG)
 ifeq ($(PODMAN_BUILD),1)
