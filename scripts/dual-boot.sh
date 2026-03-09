a0d3bccf5fc2a1219035f8c056ded90f835cb044
Jeremy Soller <jackpot51@gmail.com>
2026-03-08 19:24:04 -0600

    Fix bootloader path in dual-boot script

--- scripts/dual-boot.sh
+++ scripts/dual-boot.sh
@@ -34,7 +34,7 @@ then
     exit 1
 fi
 
-BOOTLOADER="recipes/core/bootloader/target/${ARCH}-unknown-redox/stage/boot/bootloader.efi"
+BOOTLOADER="recipes/core/bootloader/target/${ARCH}-unknown-redox/stage/usr/lib/boot/bootloader.efi"
 set -x
 sudo mkdir -pv "${ESP}/EFI" "${ESP}/loader/entries"
 sudo cp -v "${BOOTLOADER}" "${ESP}/EFI/redox.efi"
