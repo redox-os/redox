df5e823c3da5ec9d699be48b9a6a54707463e21c
Wildan M <willnode@wellosoft.net>
2026-04-14 18:53:24 +0700

    Fix compiler

--- mk/prefix.mk
+++ mk/prefix.mk
@@ -391,7 +391,7 @@ else
 	@echo "\033[1;36;49mBuilding rust-install\033[0m"
 	rm -rf "$@.partial" "$@"
 	export PATH="$(ROOT)/$(PREFIX)/libtool-install/bin:$$PATH" \
-		$(PREFIX_CONFIG) COOKBOOK_HOST_SYSROOT=/usr COOKBOOK_CROSS_TARGET=$(TARGET) && \
+		$(PREFIX_CONFIG) COOKBOOK_HOST_SYSROOT=/usr COOKBOOK_CROSS_TARGET=$(TARGET) COOKBOOK_CROSS_GNU_TARGET=$(GNU_TARGET) && \
 		$(REPO_BIN) cook host:llvm21 host:rust
 	cp -r "$(RUST_TARGET)/stage/usr/". "$@.partial"
 	cp -r "$(LLVM_TARGET)/stage/usr/". "$@.partial"
