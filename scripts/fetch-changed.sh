726940949f394f4369eb9bd52b8bcbbe45aa92e0
Jeremy Soller <jackpot51@gmail.com>
2026-06-04 10:16:53 -0600

    Add script to test fetching changed recipes

---
+++ scripts/fetch-changed.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+git fetch origin master
+packages=""
+for toml in $(git diff --name-only origin/master | grep '/recipe.toml$' | sort | uniq)
+do
+    package="$(basename "$(dirname "${toml}")")"
+    if [ -n "${packages}" ]
+    then
+        packages="${packages},"
+    fi
+    packages="${packages}${package}"
+done
+if [ -n "${packages}" ]
+then
+    make f."${packages}"
+else
+    echo "No recipe.toml changes found"
+fi
+
+
