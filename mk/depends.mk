3374d5a95e684856eea4f00210ee6693723f861d
Konstantin Shabanov <mail@etehtsea.me>
2026-08-07 23:23:44 +0700

    mk: Explicitly set SKIP_CHECK_TOOLS
    Rather than checking for different env variables, explicitly set
    SKIP_CHECK_TOOLS for podman build and hosted redox.

--- mk/depends.mk
+++ mk/depends.mk
@@ -1,10 +1,5 @@
 # Configuration file for the build system dependencies
 
-# Don't check for dependencies if you will be using Podman
-ifneq ($(PODMAN_BUILD),1)
-# Don't check for dependencies if you will be using Hosted Redox
-ifneq ($(HOSTED_REDOX),1)
-
 # don't check for compile tools, used when tools are managed externally, like in nix or CI
 ifneq ($(SKIP_CHECK_TOOLS),1)
 ifeq ($(shell which rustup),)
@@ -18,8 +13,5 @@ $(error nasm not found, install from your package manager)
 endif
 ifeq ($(shell which just),)
 $(error 'just' not found, install from crates.io or from your package manager)
-endif
-endif
-
 endif
 endif
